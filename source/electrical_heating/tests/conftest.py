9f63e9a7f2f3ec1ecf91dd430ad01a9edc27d421
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2024-12-09 13:14:49 +0100

    add conftest.py

---
+++ source/electrical_heating/tests/conftest.py
@@ -0,0 +1,65 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import pytest
+from typing import Callable
+
+@pytest.fixture(scope="session")
+def file_path_finder() -> Callable[[str], str]:
+    """
+    Returns the path of the file.
+    Please note that this only works if current folder haven't been changed prior using
+    `os.chdir()`. The correctness also relies on the prerequisite that this function is
+    actually located in a file located directly in the tests folder.
+    """
+
+    def finder(file: str) -> str:
+        return os.path.dirname(os.path.normpath(file))
+
+    return finder
+
+
+@pytest.fixture(scope="session")
+def source_path(file_path_finder: Callable[[str], str]) -> str:
+    """
+    Returns the <repo-root>/source folder path.
+    Please note that this only works if current folder haven't been changed prior using
+    `os.chdir()`. The correctness also relies on the prerequisite that this function is
+    actually located in a file located directly in the tests folder.
+    """
+    return file_path_finder(f"{__file__}/../../..")
+
+
+@pytest.fixture(scope="session")
+def electrical_heating_path(source_path: str) -> str:
+    """
+    Returns the source/electrical_heating/ folder path.
+    Please note that this only works if current folder haven't been changed prior using
+    `os.chdir()`. The correctness also relies on the prerequisite that this function is
+    actually located in a file located directly in the tests folder.
+    """
+    return f"{source_path}/electrical_heating"
+
+
+@pytest.fixture(scope="session")
+def contracts_path(electrical_heating_path: str) -> str:
+    """
+    Returns the source/contract folder path.
+    Please note that this only works if current folder haven't been changed prior using
+    `os.chdir()`. The correctness also relies on the prerequisite that this function is
+    actually located in a file located directly in the tests folder.
+    """
+    return f"{electrical_heating_path}/contracts"
+
+
