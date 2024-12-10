215bd5cd2abf1c037714ab98633371d8e82d6fc5
Alexander <alexanderharrow@hotmail.com>
2024-12-10 15:10:09 +0100

    Updates

--- source/electrical_heating/tests/entry_points/job_args/test_electrical_heating_job_args.py
+++ source/electrical_heating/tests/entry_points/job_args/test_electrical_heating_job_args.py
@@ -2,11 +2,13 @@
 
 import pytest
 
-from electrical_heating_job import parse_job_arguments, parse_command_line_arguments
-from electrical_heating_job import (
+from electrical_heating_job.entry_points.job_args.electrical_heating_job_args import (
+    parse_command_line_arguments,
+    parse_job_arguments,
+)
+from electrical_heating_job.entry_points.job_args.environment_variables import (
     EnvironmentVariable,
 )
-
 
 DEFAULT_ID = "12345678-9fc8-409a-a169-fbd49479d718"
 
