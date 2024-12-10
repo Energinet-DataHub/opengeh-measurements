38f8e62d34a7ccec2ecf1ca2709b07ed3b400146
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-12-10 15:34:50 +0100

    feat: Add contract for time series points (#17)

---
+++ source/electrical_heating/contracts/measurements__electrical_heating/time_series_points_v1.py
@@ -0,0 +1,24 @@
+import pyspark.sql.types as t
+
+nullable = True
+
+
+time_series_points_v1 = t.StructType(
+    [
+        #
+        # GSRN number
+        t.StructField("metering_point_id", t.StringType(), not nullable),
+        #
+        t.StructField("quantity", t.DecimalType(18, 3), not nullable),
+        #
+        # UTC time
+        t.StructField(
+            "observation_time",
+            t.TimestampType(),
+            not nullable,
+        ),
+        #
+        # 'supply_to_grid' 'consumption_from_grid' | 'electrical_heating' | 'net_consumption'
+        t.StructField("metering_point_type", t.StringType(), not nullable),
+    ]
+)
