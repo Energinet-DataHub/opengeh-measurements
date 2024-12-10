aa8958a9b7a5a3ef8b6b2307af5fd731b2de3b3f
Bjarke Meier <hej@bjarkemeier.dk>
2024-12-10 21:49:22 +0100

    feat: Create contract for measurements input table for electrical heating

---
+++ source/electrical_heating/contracts/measurements__electrical_heating/measurements_bronze.py
@@ -0,0 +1,70 @@
+import pyspark.sql.types as t
+
+nullable = True
+
+
+_point = t.StructType(
+    [
+        #
+        # A sequential numbering.
+        # Combined with start and resolution it can be used to determine the observation time
+        # of the individual points in the transaction.
+        t.StructField("position", t.StringType(), not nullable),
+        #
+        # The quantity calculated by the electrical heating job
+        t.StructField("quantity", t.StringType(), not nullable),
+        #
+        # Must be: "calculated"
+        t.StructField("quality", t.StringType(), not nullable),
+    ]
+)
+
+# Electrical heating result is delivered to Measurements
+# as one transaction per metering point id.
+measurements_bronze = t.StructType(
+    [
+        #
+        # Must be: "electrical_heating"
+        t.StructField("orchestration_type", t.StringType(), not nullable),
+        #
+        # The GUIDvalue of the electrical heating calculation job
+        t.StructField("orchestration_instance_id", t.StringType(), not nullable),
+        #
+        # Metering Point ID
+        # GSRN number
+        t.StructField("metering_point_id", t.StringType(), not nullable),
+        #
+        # Transaction ID. Created by the electrical heating calculation job.
+        t.StructField("transaction_id", t.StringType(), not nullable),
+        #
+        # The start DateTime of the transaction (the first position’s observation time)
+        t.StructField("transaction_start_datetime", t.TimestampType(), not nullable),
+        #
+        # The end DateTime of the transaction
+        t.StructField("transaction_end_datetime", t.TimestampType(), not nullable),
+        #
+        # A DateTime value indicating when the transaction was created
+        # by the electrical heating calculation job.
+        t.StructField("transaction_creation_datetime", t.StringType(), not nullable),
+        #
+        # Must be "electrical_heating"
+        t.StructField("metering_point_type", t.StringType(), not nullable),
+        #
+        # Must be "Active Energy"
+        t.StructField("product", t.StringType(), not nullable),
+        #
+        # Metering Unit
+        # Must be: "kWh"
+        t.StructField("unit", t.StringType(), not nullable),
+        #
+        # Must be: "PT1H"
+        t.StructField("resolution", t.StringType(), not nullable),
+        #
+        # Points
+        t.StructField(
+            "points",
+             t.ArrayType(_point, containsNull=False),
+             not nullable,
+        ),
+    ]
+)
