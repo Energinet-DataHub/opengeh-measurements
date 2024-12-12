0e1ca181cb60eafb8fb7b5d4e95877f6a4f06786
Alexander <alexanderharrow@hotmail.com>
2024-12-12 11:00:21 +0100

    Update test_entry_points.py

--- source/electrical_heating/tests/entry_points/test_entry_points.py
+++ source/electrical_heating/tests/entry_points/test_entry_points.py
@@ -1,7 +1,6 @@
 ﻿import pytest
 
 from electrical_heating_job import entry_points as module
-from source.test_common import assert_entry_point_exists
 
 
 @pytest.mark.parametrize(
@@ -14,4 +13,6 @@ def test__entry_point_exists(
     installed_package: None,
     entry_point_name: str,
 ) -> None:
+    from source.test_common import assert_entry_point_exists
+
     assert_entry_point_exists(entry_point_name, module)
