c0af89192324058136dcb1a3c93ac58eb6c939df
Bjarke Meier <hej@bjarkemeier.dk>
2024-12-12 12:28:39 +0100

    add needed columns

--- source/electrical_heating/contracts/electricity_market__electrical_heating/child_metering_point_periods_v1.py
+++ source/electrical_heating/contracts/electricity_market__electrical_heating/child_metering_point_periods_v1.py
@@ -2,7 +2,7 @@ import pyspark.sql.types as t
 
 nullable = True
 
-# All child metering points related to electrical heating.
+# Child metering points related to electrical heating.
 #
 # The included metering point types are:
 # 'supply_to_grid' 'consumption_from_grid' | 'electrical_heating' | 'net_consumption'
@@ -15,6 +15,12 @@ child_metering_point_periods_v1 = t.StructType(
         # 'supply_to_grid' | 'consumption_from_grid' |
         # 'electrical_heating' | 'net_consumption'
         t.StructField("metering_point_type", t.StringType(), not nullable),
+        #
+        # 'calculated' | 'virtual' | 'physical'
+        t.StructField("metering_point_sub_type", t.StringType(), not nullable),
+        #
+        # "PT15M" | "PT1H"
+        t.StructField("resolution", t.StringType(), not nullable),
         #
         # GSRN number
         t.StructField("parent_metering_point_id", t.StringType(), not nullable),
