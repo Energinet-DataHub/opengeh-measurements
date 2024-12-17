41599bf6e8f492925e1e78ea906425a57d49722d
Bjarke Meier <hej@bjarkemeier.dk>
2024-12-17 11:33:42 +0100

    etl refacs

--- source/electrical_heating/tests/scenarios/given_foo/test_it.py
+++ source/electrical_heating/tests/scenarios/given_foo/test_it.py
@@ -1,30 +1,28 @@
-﻿from typing import Any, Tuple
+﻿from typing import Tuple
 
 import pytest
 from pyspark.sql import DataFrame
 
-from scenarios.test_common import assert_output, get_output_names
-
 
 # get_test_case_names() -> ["output_table_1", "output_table_2"]
 # test_cases: All actual and expected dataframes
-@pytest.mark.parametrize("test_case_name", get_test_case_names())
+@pytest.mark.parametrize("test_case_name", get_then_names())
 def test__equals_expected(
-    test_case_name: str,
+    then_name: str,
     test_cases: list[Tuple[str, DataFrame, DataFrame]],
 ) -> None:
-    test_case = next(tc for tc in test_cases if tc[0] == test_case_name)
+    test_case = next(tc for tc in test_cases if tc[0] == then_name)
     assert_data_frames(
         test_case[1], test_case[2]
     )
 
 
-@pytest.mark.parametrize("test_case_name", get_test_case_names())
+@pytest.mark.parametrize("test_case_name", get_then_names())
 def test__equals_expected(
-    test_case_name: str,
+    then_name: str,
     test_cases: TestCases,
 ) -> None:
-    test_case = test_cases[test_case_name]
+    test_case = test_cases[then_name]
     assert_data_frames(
         test_case.actual, test_case.expected
     )
---
+++ source/electrical_heating/tests/scenarios/testcommon/etl.py
@@ -0,0 +1,24 @@
+﻿from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType
+
+"""
+README:
+- This is the framework (module `etl` of package `opengeh-testcommon`) for writing tests for ETL processes.
+- The API is enlisted below.
+- Other functionality is:
+    - A tool to update all test modules in a directory including subdirectories.
+    - Documentation on how to write tests (test file, folders, fixture, ...)
+"""
+
+
+# Consider extracting actual and expected in the test itself and then simplify to:
+# Was: assert_output(actual_and_expected, output_name)
+def assert_dataframes(actual: DataFrame, expected: DataFrame):
+    pass
+
+# Was get_output_names
+def get_then_names() -> list[str]:
+    return []
+
+def create_dataframe_from_csv(csv_path: str, dataframe_schema: StructType) -> DataFrame:
+    pass
