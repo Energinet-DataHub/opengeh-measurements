1b98789089a6573d0d47a6091e1ce7b17084410a
Alexander <alexanderharrow@hotmail.com>
2024-12-17 08:45:25 +0100

    Update entry_point.py

--- source/capacity_settlement/capacity_settlement_job/entry_points/entry_point.py
+++ source/capacity_settlement/capacity_settlement_job/entry_points/entry_point.py
@@ -10,7 +10,10 @@ from telemetry_logging.span_recording import span_record_exception
 from capacity_settlement_job.entry_points.job_args.capacity_settlement_args import (
     CapacitySettlementArgs,
 )
-from capacity_settlement_job.entry_points.job_args.capacity_settlement_job_args import parse_command_line_arguments, parse_job_arguments
+from capacity_settlement_job.entry_points.job_args.capacity_settlement_job_args import (
+    parse_command_line_arguments,
+    parse_job_arguments,
+)
 
 
 def execute() -> None:
