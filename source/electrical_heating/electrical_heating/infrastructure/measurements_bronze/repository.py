62c1fe1af353cf3dc5ab92ff7519f14f3e5d969a
Bjarke Meier <hej@bjarkemeier.dk>
2024-12-18 11:46:07 +0100

    bronze has a write method

--- source/electrical_heating/electrical_heating/infrastructure/measurements_bronze/repository.py
+++ source/electrical_heating/electrical_heating/infrastructure/measurements_bronze/repository.py
@@ -1,4 +1,4 @@
-﻿from pyspark.sql import SparkSession, DataFrame
+﻿from pyspark.sql import DataFrame
 
 from electrical_heating.infrastructure.measurements_bronze.database_definitions import (
     MeasurementsBronzeDatabase,
@@ -8,20 +8,11 @@ from electrical_heating.infrastructure.measurements_bronze.database_definitions 
 class MeasurementsBronzeRepository:
     def __init__(
         self,
-        spark: SparkSession,
         catalog_name: str,
     ) -> None:
-        self._spark = spark
         self._catalog_name = catalog_name
 
-    def read_measurements(self) -> DataFrame:
-        return self._read_view_or_table(
-            MeasurementsBronzeDatabase.MEASUREMENTS_NAME,
+    def write_measurements(self, df: DataFrame) -> None:
+        df.writeTo(
+            f"{self._catalog_name}.{MeasurementsBronzeDatabase.DATABASE_NAME}.{MeasurementsBronzeDatabase.MEASUREMENTS_NAME}"
         )
-
-    def _read_view_or_table(
-        self,
-        table_name: str,
-    ) -> DataFrame:
-        name = f"{self._catalog_name}.{MeasurementsBronzeDatabase.DATABASE_NAME}.{table_name}"
-        return self._spark.read.format("delta").table(name)
