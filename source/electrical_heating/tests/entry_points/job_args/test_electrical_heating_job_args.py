7579be73b3fd79b7272248748d01930497613213
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-12-18 10:12:14 +0100

    refac: Remove EH job suffix from python library (#46)

--- source/electrical_heating/tests/entry_points/job_args/test_electrical_heating_job_args.py
+++ source/electrical_heating/tests/entry_points/job_args/test_electrical_heating_job_args.py
@@ -2,11 +2,11 @@
 
 import pytest
 
-from electrical_heating_job.entry_points.job_args.electrical_heating_job_args import (
+from electrical_heating.entry_points.job_args.electrical_heating_job_args import (
     parse_command_line_arguments,
     parse_job_arguments,
 )
-from electrical_heating_job.entry_points.job_args.environment_variables import (
+from electrical_heating.entry_points.job_args.environment_variables import (
     EnvironmentVariable,
 )
 
