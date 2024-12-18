ab95d76df5007805853ec803a60688f462119a03
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-12-18 13:39:24 +0100

    feat: Add measurements repositories to EH (#49)

---
+++ source/electrical_heating/electrical_heating/infrastructure/measurements_bronze/data_values/orchestration_type.py
@@ -0,0 +1,5 @@
+﻿from enum import Enum
+
+
+class OrchestrationType(Enum):
+    ELECTRICAL_HEATING = "electrical_heating"
---
+++ source/electrical_heating/electrical_heating/infrastructure/measurements_bronze/repository.py
@@ -0,0 +1,18 @@
+﻿from pyspark.sql import DataFrame
+
+from electrical_heating.infrastructure.measurements_bronze.database_definitions import (
+    MeasurementsBronzeDatabase,
+)
+
+
+class Repository:
+    def __init__(
+        self,
+        catalog_name: str,
+    ) -> None:
+        self._catalog_name = catalog_name
+
+    def write_measurements(self, df: DataFrame) -> None:
+        df.writeTo(
+            f"{self._catalog_name}.{MeasurementsBronzeDatabase.DATABASE_NAME}.{MeasurementsBronzeDatabase.MEASUREMENTS_NAME}"
+        )
