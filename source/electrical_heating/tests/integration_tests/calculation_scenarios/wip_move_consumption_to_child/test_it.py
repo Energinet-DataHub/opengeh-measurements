4e2484623765db4f208d1d6b5e54f24bc03c1fe3
Bjarke Meier <hej@bjarkemeier.dk>
2024-12-19 19:36:40 +0100

    wip

--- source/electrical_heating/tests/integration_tests/calculation_scenarios/wip_move_consumption_to_child/test_it.py
+++ source/electrical_heating/tests/integration_tests/calculation_scenarios/wip_move_consumption_to_child/test_it.py
@@ -1,12 +1,10 @@
-﻿from typing import Tuple
-
-import pytest
-from pyspark.sql import DataFrame
-
+﻿import pytest
 from testcommon.etl import assert_dataframes, get_then_names
 
+from integration_tests.calculation_scenarios.temp_testcommon import TestCases
 
-@pytest.mark.parametrize("test_case_name", get_then_names())
-def test_get_then_names(test_case_name: str, test_cases: list[Tuple[str, DataFrame, DataFrame]]) -> None:
-    test_case = next(test_case for test_case in test_cases if test_case[0] == test_case_name)
-    assert_dataframes(test_case[1], test_cases[2])
+
+@pytest.mark.parametrize("name", get_then_names())
+def test_get_then_names(name: str, test_cases: TestCases) -> None:
+    test_case = test_cases[name]
+    assert_dataframes(test_case.actual, test_case.expected)
