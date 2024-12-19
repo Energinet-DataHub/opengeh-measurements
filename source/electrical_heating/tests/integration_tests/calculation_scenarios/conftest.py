bbf1c681cefd407062c62849e28f77c4dd0a35fd
Bjarke Meier <hej@bjarkemeier.dk>
2024-12-19 15:30:59 +0100

    add initial code

---
+++ source/electrical_heating/tests/integration_tests/calculation_scenarios/conftest.py
@@ -0,0 +1,13 @@
+﻿from typing import Tuple
+
+import pytest
+from pyspark.sql import SparkSession, DataFrame
+from testcommon.etl import read_csv
+
+from electrical_heating.domain.calculation import _execute
+
+
+@pytest.fixture(scope="module")
+def test_cases(spark: SparkSession) -> list[Tuple[str, DataFrame, DataFrame]]:
+    time_series_points = read_csv(spark, "time_series_points.csv")
+    result = _execute(time_series_points)
---
+++ source/electrical_heating/tests/integration_tests/calculation_scenarios/wip_move_consumption_to_child/test_it.py
@@ -0,0 +1,12 @@
+﻿from typing import Tuple
+
+import pytest
+from pyspark.sql import DataFrame
+
+from testcommon.etl import assert_dataframes, get_then_names
+
+
+@pytest.mark.parametrize("test_case_name", get_then_names())
+def test_get_then_names(test_case_name: str, test_cases: list[Tuple[str, DataFrame, DataFrame]]) -> None:
+    test_case = next(test_case for test_case in test_cases if test_case[0] == test_case_name)
+    assert_dataframes(test_case[1], test_cases[2])
