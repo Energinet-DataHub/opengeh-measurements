c5be07d51dba101abd0f84c18c0122b4471f0ff0
Jonas Michelsen <jvm@energinet.dk>
2024-12-23 13:49:27 +0100

    Update calculation.py

--- source/electrical_heating/src/electrical_heating/domain/calculation.py
+++ source/electrical_heating/src/electrical_heating/domain/calculation.py
@@ -10,10 +10,10 @@ from source.electrical_heating.src.electrical_heating.entry_points.job_args.elec
     ElectricalHeatingArgs,
 )
 
-from electrical_heating.infrastructure.electricity_market.repository import (
+from source.electrical_heating.src.electrical_heating.infrastructure.electricity_market.repository import (
     Repository as em,
 )
-from electrical_heating.infrastructure.measurements_gold.repository import (
+from source.electrical_heating.src.electrical_heating.infrastructure.measurements_gold.repository import (
     Repository as mg,
 )
 
