18a157de19fe32312f391e8733494678ca61f44e
Jonas Michelsen <jvm@energinet.dk>
2024-12-23 10:58:44 +0100

    Update entry_point.py

--- source/capacity_settlement/src/capacity_settlement/entry_points/entry_point.py
+++ source/capacity_settlement/src/capacity_settlement/entry_points/entry_point.py
@@ -7,10 +7,10 @@ import telemetry_logging.logging_configuration as config
 from opentelemetry.trace import SpanKind
 from telemetry_logging.span_recording import span_record_exception
 
-from job_args.capacity_settlement_args import (
+from source.capacity_settlement.src.capacity_settlement.entry_points.job_args.capacity_settlement_args import (
     CapacitySettlementArgs,
 )
-from job_args.capacity_settlement_job_args import (
+from source.capacity_settlement.src.capacity_settlement.entry_points.job_args.capacity_settlement_job_args import (
     parse_command_line_arguments,
     parse_job_arguments,
 )
