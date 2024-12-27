22422d3bda7f2511fa86578ac2d3407f91cc3170
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-12-27 09:31:37 +0100

    Update test_sql_scripts.sql

--- source/database_migration/src/database_migration/migration_scripts/test_sql_scripts.sql
+++ source/database_migration/src/database_migration/migration_scripts/test_sql_scripts.sql
@@ -1,9 +1,44 @@
-CREATE TABLE test_schema.test_table
+CREATE TABLE bronze_schema.bronze_time_series_table
 (
+    metering_point STRUCT<
+        masterdata ARRAY<
+            STRUCT<
+                grid_area STRING,
+                masterdata_end_date TIMESTAMP,
+                masterdata_start_date TIMESTAMP,
+                type_of_mp STRING
+            >
+        >,
+        metering_point_id STRING
+    >,
+    time_series ARRAY<
+        STRUCT<
+            historical_flag STRING,
+            resolution STRING,
+            transaction_id STRING,
+            message_id STRING,
+            transaction_insert_date TIMESTAMP,
+            unit STRING,
+            status INT,
+            read_reason STRING,
+            valid_from_date TIMESTAMP,
+            valid_to_date TIMESTAMP,
+            values ARRAY<
+                STRUCT<
+                    position INT,
+                    quality STRING,
+                    quantity DECIMAL(18, 6)
+                >
+            >
+        >
+    >,
+    _rescued_data STRING,
     created TIMESTAMP NOT NULL,
-) USING DELTA
+    file_path STRING NOT NULL
+)
+USING DELTA
 TBLPROPERTIES (
     delta.autoOptimize.optimizeWrite = true,
-    delta.autoOptimize.autoCompact = true,
+    delta.autoOptimize.autoCompact = false,
     delta.deletedFileRetentionDuration = 'interval 30 days'
 )
