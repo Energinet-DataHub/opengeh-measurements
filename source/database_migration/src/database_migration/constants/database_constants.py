269f543ddb7cf9eeaec174d3e9554e9a1c714c4a
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2024-12-30 14:12:51 +0100

    Update database_constants.py

--- source/database_migration/src/database_migration/constants/database_constants.py
+++ source/database_migration/src/database_migration/constants/database_constants.py
@@ -1,4 +1,4 @@
 ﻿class DatabaseConstants:
     # Should probably be an environment variable because it is created in Terraform.
     bronze_database = "bronze_measurements"
-    migrations_database = "migrations"
+    migrations_database = "measurements_internal"
