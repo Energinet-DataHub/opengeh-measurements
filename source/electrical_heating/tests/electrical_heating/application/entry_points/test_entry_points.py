5b5f3ad2a0f111294405b6ae885174abb0834c85
Alexander <alexanderharrow@hotmail.com>
2025-01-02 14:29:13 +0100

    Update entry_point_test_util.py

--- source/electrical_heating/tests/electrical_heating/application/entry_points/test_entry_points.py
+++ source/electrical_heating/tests/electrical_heating/application/entry_points/test_entry_points.py
@@ -3,7 +3,6 @@
 from source.electrical_heating.src.electrical_heating.application.entry_points import (
     entry_point as module,
 )
-from source.electrical_heating.tests.conftest import installed_package
 from source.tests.test_common.entry_points.entry_point_test_util import (
     assert_entry_point_exists,
 )
