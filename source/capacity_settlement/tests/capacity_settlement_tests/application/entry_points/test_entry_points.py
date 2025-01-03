439aa1617e2a569085e1b8b391faff8f91e31b76
Alexander <alexanderharrow@hotmail.com>
2025-01-03 08:54:45 +0100

    Update test_entry_points.py

--- source/capacity_settlement/tests/capacity_settlement_tests/application/entry_points/test_entry_points.py
+++ source/capacity_settlement/tests/capacity_settlement_tests/application/entry_points/test_entry_points.py
@@ -1,6 +1,6 @@
 ﻿import pytest
 
-from source.electrical_heating.src.electrical_heating.application.entry_points import (
+from source.capacity_settlement.src.capacity_settlement.application.entry_points import (
     entry_point as module,
 )
 from source.tests.test_common.entry_points.entry_point_test_util import (
