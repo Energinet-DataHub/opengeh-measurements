e03cf8c9c87fd37de91ed54bfc171b4b3f4f15e7
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-01-06 14:46:13 +0100

    fix

---
+++ source/database_migration/src/database_migration/constants/column_names/bronze_measurements_column_names.py
@@ -0,0 +1,24 @@
+﻿class BronzeMeasurementsColumnNames:
+    orchestration_type = "orchestration_type"
+    orchestration_instance_id = "orchestration_instance_id"
+    metering_point_id = "metering_point_id"
+    transaction_id = "transaction_id"
+    transaction_creation_datetime = "transaction_creation_datetime"
+    metering_point_type = "metering_point_type"
+    product = "product"
+    unit = "unit"
+    resolution = "resolution"
+    start_datetime = "start_datetime"
+    end_datetime = "end_datetime"
+    points = "points"
+    rescued_data = "_rescued_data"
+    created = "created"
+    file_path = "file_path"
+
+    class Points:
+        """
+        Constants for the column names in the points column of the bronze measurements table.
+        """
+        position = "position"
+        quantity = "quantity"
+        quality = "quality"
