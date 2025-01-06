1ce84f5816148cead48d9fb658488983f517550c
Alexander <alexanderharrow@hotmail.com>
2025-01-06 14:44:38 +0100

    Updates

---
+++ source/electrical_heating/src/electrical_heating/domain/calculation_results.py
@@ -0,0 +1,14 @@
+﻿from dataclasses import dataclass
+
+from pyspark.sql import DataFrame
+
+
+@dataclass
+class CalculationOutput:
+    """
+    Contains the output of a calculation.
+    """
+
+    daily_child_consumption_with_limit: DataFrame | None = None
+
+    calculation: DataFrame | None = None
