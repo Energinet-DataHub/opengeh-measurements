341200795459bb6fd236d872a0ea1057e4ce1b78
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-01-10 13:09:10 +0100

    feat: Add calculation period as job args in capacity settlement (#85)

--- source/capacity_settlement/src/capacity_settlement/application/job_args/capacity_settlement_args.py
+++ source/capacity_settlement/src/capacity_settlement/application/job_args/capacity_settlement_args.py
@@ -1,7 +1,10 @@
 ﻿from dataclasses import dataclass
+from datetime import datetime
 from uuid import UUID
 
 
 @dataclass
 class CapacitySettlementArgs:
     orchestration_instance_id: UUID
+    calculation_period_start: datetime
+    calculation_period_end: datetime
