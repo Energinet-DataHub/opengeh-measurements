8dce17a45fc3f82f9160429e3cebd10fc9949cfa
Jan Duelund <jdd@energinet.dk>
2025-01-10 07:16:35 +0100

    added decimalvalue

---
+++ source/measurements/tests/contracts_tests/decimal_value_tests.py
@@ -0,0 +1,39 @@
+﻿import pytest
+from source.measurements.src.contracts.decimal_value import DecimalValue
+
+def test__from_decimal(
+) -> None:
+    # Arrange
+    value = 12345.6789
+
+    # Act
+    result = DecimalValue.from_decimal(value)
+
+    # Assert
+    assert result.units == 12345
+    assert result.nanos == 678900000
+
+
+def test__to_decimal(
+) -> None:
+    # Arrange
+    value = DecimalValue(12345, 678900000)
+
+    # Act
+    result = value.to_decimal()
+
+    # Assert
+    assert result == 12345.6789
+
+
+def test__negative_value(
+) -> None:
+    # Arrange
+    value = -12345.6789
+
+    # Act
+    result = DecimalValue.from_decimal(value)
+
+    # Assert
+    assert result.units == -12345
+    assert result.nanos == -678900000
