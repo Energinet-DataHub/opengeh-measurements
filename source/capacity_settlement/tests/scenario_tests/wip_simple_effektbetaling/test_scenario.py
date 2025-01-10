2109cf8f82574599cfb7146f5241e10045d245e3
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-01-10 21:36:10 +0100

    ...

--- source/capacity_settlement/tests/scenario_tests/wip_simple_effektbetaling/test_scenario.py
+++ source/capacity_settlement/tests/scenario_tests/wip_simple_effektbetaling/test_scenario.py
@@ -1,9 +1,9 @@
 ﻿import pytest
-from testcommon.dataframes import assert_dataframes
+from testcommon.dataframes.assert_dataframes import assert_dataframes_and_schemas
 from testcommon.etl import get_then_names, TestCases
 
 
 @pytest.mark.parametrize("name", get_then_names())
 def test_get_then_names(name: str, test_cases: TestCases) -> None:
     test_case = test_cases[name]
-    assert_dataframes(actual=test_case.actual, expected=test_case.expected)
+    assert_dataframes_and_schemas(actual=test_case.actual, expected=test_case.expected)
