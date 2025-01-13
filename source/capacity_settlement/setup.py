ae28c0fd983113971ba0c8d7098297e421329d37
Jonas Michelsen <jvm@energinet.dk>
2025-01-13 11:40:03 +0100

    .

--- source/capacity_settlement/setup.py
+++ source/capacity_settlement/setup.py
@@ -8,7 +8,6 @@ setup(
     long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
-    package_dir={"": "source"},
     # Make sure these packages are added to the docker container and pinned to the same versions
     install_requires=[
         "ConfigArgParse==1.7.0",
