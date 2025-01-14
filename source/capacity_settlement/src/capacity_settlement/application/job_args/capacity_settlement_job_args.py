f0a441a139dfd0419b8bdddb3b86962f297c508f
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-01-14 15:45:55 +0100

    ...

--- source/capacity_settlement/src/capacity_settlement/application/job_args/capacity_settlement_job_args.py
+++ source/capacity_settlement/src/capacity_settlement/application/job_args/capacity_settlement_job_args.py
@@ -22,7 +22,7 @@ def parse_job_arguments(
 
         capacity_settlement_args = CapacitySettlementArgs(
             orchestration_instance_id=job_args.orchestration_instance_id,
-            calculation_month=job_args.ca.calculation_month,
+            calculation_month=job_args.calculation_month,
             calculation_year=job_args.calculation_year,
         )
 
