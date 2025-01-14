9b3389394eead9a9d71549dd84485478642d0679
ClausQAEnerginet <xclpe@energinet.dk>
2025-01-14 10:34:26 +0100

    Update Coverage.py

--- source/electrical_heating/tests/scenario_tests/wip_non_matching_child_periods/Coverage.py
+++ source/electrical_heating/tests/scenario_tests/wip_non_matching_child_periods/Coverage.py
@@ -2,7 +2,7 @@ from coverage.all_test_cases import Cases
 
 """
 ## PURPOSE
-The purpose is to test a scenario where parent period and child period do not overlap. This is really an invalid state, but it can happen i prod, so we include a test for it.
+The purpose is to test a scenario where parent period and child period do not overlap. This is really an invalid state, but it can happen i production, so we include a test for it.
 
 ## DESIGN CONSIDERATIONS
 
