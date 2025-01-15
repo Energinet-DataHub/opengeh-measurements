2c720dcd2850249e6d06aa920a731a64388ea4f9
JeppeMadsen <Jeppe1000@live.dk>
2025-01-15 12:00:57 +0100

    Wrong substitution

--- source/silver/src/silver/domain/substitutions.py
+++ source/silver/src/silver/domain/substitutions.py
@@ -4,6 +4,6 @@ from silver.domain.constants.table_names import TableNames
 
 def substitutions() -> dict[str, str]:
     return {
-        "{silver_database.}": DatabaseNames.silver_database,
+        "{silver_database}": DatabaseNames.silver_database,
         "{silver_measurements_table}": TableNames.silver_measurements_table,
     }
