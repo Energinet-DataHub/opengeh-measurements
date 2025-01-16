ebe532057ee938a741bb026a2b866905fa8c9008
Bjarke Meier <hej@bjarkemeier.dk>
2025-01-16 15:23:03 +0100

    black

--- source/electrical_heating/src/electrical_heating/domain/calculation.py
+++ source/electrical_heating/src/electrical_heating/domain/calculation.py
@@ -29,9 +29,7 @@ def execute(spark: SparkSession, args: ElectricalHeatingArgs) -> None:
     consumption_metering_point_periods = (
         electricity_market_repository.read_consumption_metering_point_periods()
     )
-    child_metering_points = (
-        electricity_market_repository.read_child_metering_points()
-    )
+    child_metering_points = electricity_market_repository.read_child_metering_points()
     time_series_points = measurements_gold_repository.read_time_series_points()
 
     # Execute the calculation logic
