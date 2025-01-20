9ebf933d2e3e62e68ac6c44fbb297d06058125ad
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-20 12:00:03 +0100

    test: fix import for child_metering_points_v1
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/electrical_heating/tests/scenario_tests/conftest.py
+++ source/electrical_heating/tests/scenario_tests/conftest.py
@@ -14,7 +14,7 @@ from electrical_heating.application.execute_with_deps import (
 from electrical_heating.application.job_args.electrical_heating_args import (
     ElectricalHeatingArgs,
 )
-from electrical_heating.infrastructure.electricity_market.schemas import (
+from electrical_heating.infrastructure.electricity_market.schemas.child_metering_points_v1 import (
     child_metering_points_v1,
 )
 from electrical_heating.infrastructure.electricity_market.schemas.consumption_metering_point_periods_v1 import (
