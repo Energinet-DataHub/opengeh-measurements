7137d3870dda460db2aa442e62f2f4bc03e7c0f2
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-01-21 15:33:41 +0100

    missing in last

--- source/bronze/src/bronze/entry_points.py
+++ source/bronze/src/bronze/entry_points.py
@@ -1,5 +1,10 @@
 ﻿import bronze.application.migrations as migrations
+import bronze.application.submitted_transactions as submitted_transactions
 
 
 def migrate() -> None:
     migrations.migrate()
+
+
+def ingest_submitted_transactions() -> None:
+    submitted_transactions.submit_transactions()
