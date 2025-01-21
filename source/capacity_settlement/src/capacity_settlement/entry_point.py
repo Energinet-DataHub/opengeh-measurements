75372aea6c22ea1be2775660abb5fe3ac0f22b9e
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-21 14:29:17 +0100

    ci: ...
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/capacity_settlement/src/capacity_settlement/entry_point.py
+++ source/capacity_settlement/src/capacity_settlement/entry_point.py
@@ -6,7 +6,12 @@ from capacity_settlement.application.execute_with_deps import execute_with_deps
 
 
 def execute() -> None:
-    """Execute the capacity-settlement application."""
+    """Execute the capacity-settlement application.
+
+    This function is the entry point for the capacity-settlement application.
+    It reads the required configuration from the environment and executes the
+    application.
+    """
     applicationinsights_connection_string = os.getenv(
         "APPLICATIONINSIGHTS_CONNECTION_STRING"
     )
