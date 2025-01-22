d9cedd03113fde48d5e03bc5050fcb9d4d7246e7
Jonas Michelsen <jvm@energinet.dk>
2025-01-22 08:33:55 +0100

    Revert "Merge branch 'main' into ajw/container_test_v2"
    This reverts commit 63577589525e7ee1a919d94d88ddf9ea635b77ba, reversing
    changes made to ebffddf1f2ba767f859a86174f4c306938f7b3dd.

--- source/electrical_heating/src/electrical_heating/infrastructure/electricity_market/schemas/child_metering_points_v1.py
+++ source/electrical_heating/src/electrical_heating/infrastructure/electricity_market/schemas/child_metering_points_v1.py
@@ -2,6 +2,15 @@ import pyspark.sql.types as t
 
 nullable = True
 
+# Child metering points related to electrical heating.
+#
+# Periods are included when
+# - the metering point is of type
+#   'supply_to_grid' 'consumption_from_grid' | 'electrical_heating' | 'net_consumption'
+# - the metering point is coupled to a parent metering point
+#   Note: The same child metering point cannot be re-coupled after being uncoupled
+# - the child metering point physical status is connected or disconnected.
+# - the period does not end before 2021-01-01
 child_metering_points_v1 = t.StructType(
     [
         #
@@ -27,18 +36,3 @@ child_metering_points_v1 = t.StructType(
         t.StructField("uncoupled_date", t.TimestampType(), nullable),
     ]
 )
-"""
-Child metering points related to electrical heating.
-
-Periods are included when
-- the metering point is of type
-  'supply_to_grid' 'consumption_from_grid' | 'electrical_heating' | 'net_consumption'
-- the metering point is coupled to a parent metering point
-  Note: The same child metering point cannot be re-coupled after being uncoupled
-- the child metering point physical status is connected or disconnected.
-- the period does not end before 2021-01-01
-
-Formatting is according to ADR-144 with the following constraints:
-- No column may use quoted values
-- All date/time values must include seconds
-"""
