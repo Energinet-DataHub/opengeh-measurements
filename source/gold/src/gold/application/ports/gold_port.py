437a1bc21cbe4cee12c839f47335f32377aa4255
JeppeMadsen <Jeppe1000@live.dk>
2025-01-23 11:49:49 +0000

    pyproject toml

--- source/gold/src/gold/application/ports/gold_port.py
+++ source/gold/src/gold/application/ports/gold_port.py
@@ -7,7 +7,7 @@ from pyspark.sql import DataFrame
 class GoldPort(ABC):
     @abstractmethod
     def start_write_stream(self, df_source_stream: DataFrame, query_name: str, table_name: str, batch_operation: Callable[["DataFrame", int], None]) -> None:
-        """Starts a streaming write operation to a Gold Delta table with the specified configurations.
+        """Start a streaming write operation to a Gold Delta table with the specified configurations.
 
         Args:
             df_source_stream (DataFrame): The source streaming DataFrame to be written.
@@ -18,7 +18,7 @@ class GoldPort(ABC):
 
     @abstractmethod
     def append(self, df: DataFrame, table_name: str) -> None:
-        """Appends a static DataFrame to a Delta table.
+        """Append a static DataFrame to a Delta table.
 
         Args:
             df (DataFrame): The DataFrame to append to the Delta table.
--- source/gold/src/gold/domain/constants/column_names/silver_measurements_column_names.py
+++ source/gold/src/gold/domain/constants/column_names/silver_measurements_column_names.py
@@ -1,5 +1,4 @@
-﻿# TODO: Should reference Silver Package, figure out how to using git refs in uv
-class SilverMeasurementsColumnNames:
+﻿class SilverMeasurementsColumnNames:
     orchestration_type = "orchestration_type"
     orchestration_instance_id = "orchestration_instance_id"
     metering_point_id = "metering_point_id"
@@ -15,8 +14,6 @@ class SilverMeasurementsColumnNames:
     created = "created"
 
     class Points:
-        """Constants for the column names in the points column of the silver measurements table.
-        """
         position = "position"
         quantity = "quantity"
         quality = "quality"
