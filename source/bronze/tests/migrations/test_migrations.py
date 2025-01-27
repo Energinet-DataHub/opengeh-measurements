6ebc8ec617f4d63171460d4f2e54f33deac22716
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-01-27 12:26:34 +0100

    Fix reference in test

--- source/bronze/tests/migrations/test_migrations.py
+++ source/bronze/tests/migrations/test_migrations.py
@@ -6,7 +6,7 @@ from bronze.domain.constants.table_names import TableNames
 from bronze.domain.schemas.bronze_measurements import (
     bronze_measurements_schema,
 )
-from bronze.infrastructure.schemas.submitted_transactions import (
+from bronze.domain.schemas.submitted_transactions import (
     submitted_transactions_schema,
 )
 
