91e781bbbe3bff2a81aad432a702b676347ecf5b
Alexander <alexanderharrow@hotmail.com>
2025-01-27 14:16:04 +0100

    Update electrical_heating_test_args.py

--- source/electrical_heating/tests/scenario_tests/electrical_heating_test_args.py
+++ source/electrical_heating/tests/scenario_tests/electrical_heating_test_args.py
@@ -1,17 +1,8 @@
-﻿from datetime import datetime
-from uuid import UUID
-
-from pydantic_settings import BaseSettings
+﻿from electrical_heating.application.job_args.electrical_heating_args import ElectricalHeatingArgs
 
 
-class ElectricalHeatingTestArgs(BaseSettings):
+class ElectricalHeatingTestArgs(ElectricalHeatingArgs):
     """Args for testing the electrical heating job."""
-
-    orchestration_instance_id: UUID
-    time_zone: str
-    catalog_name: str
-    calculation_period_start: datetime
-    calculation_period_end: datetime
 
     def __init__(self, env_file_path: str) -> None:
         super().__init__(_env_file=env_file_path)
