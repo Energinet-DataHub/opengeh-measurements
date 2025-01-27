bee87a2dc388572e3963215992a4e6ae81506bd5
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-01-27 13:13:21 +0100

    Fix database name

--- source/bronze/src/bronze/application/streams/submitted_transactions.py
+++ source/bronze/src/bronze/application/streams/submitted_transactions.py
@@ -2,6 +2,8 @@ from pyspark import SparkConf
 from pyspark.sql.session import SparkSession
 
 from bronze.application.settings.submitted_transactions_stream_settings import SubmittedTransactionsStreamSettings
+from bronze.domain.constants.database_names import DatabaseNames
+from bronze.domain.constants.table_names import TableNames
 
 
 def submit_transactions() -> None:
@@ -9,7 +11,9 @@ def submit_transactions() -> None:
 
     spark = initialize_spark()
 
-    spark.readStream.format("kafka").options(**kafka_options).load().writeTo("bronze_submitted_transactions_table")
+    spark.readStream.format("kafka").options(**kafka_options).load().writeTo(
+        f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}"
+    )
 
 
 def initialize_spark() -> SparkSession:
