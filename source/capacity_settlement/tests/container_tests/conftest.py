b6206766994cac0fdfefefb07ce4e1b108da35da
Jonas Michelsen <jvm@energinet.dk>
2025-01-28 15:44:03 +0100

    .

---
+++ source/capacity_settlement/tests/container_tests/conftest.py
@@ -0,0 +1,11 @@
+import os
+
+import pytest
+from container_tests.databricks_api_client import DatabricksApiClient
+
+
+@pytest.fixture(scope="session")
+def databricks_api_client() -> DatabricksApiClient:
+    databricks_token = os.getenv("DATABRICKS_TOKEN")
+    databricks_host = os.getenv("WORKSPACE_URL")
+    return DatabricksApiClient(databricks_token, databricks_host)
