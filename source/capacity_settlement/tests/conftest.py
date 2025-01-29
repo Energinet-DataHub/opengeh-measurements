040a8fbfc416b37eb397e86313d1bcf32c0b6bef
Jonas Michelsen <jvm@energinet.dk>
2025-01-29 12:55:30 +0100

    Update conftest.py

--- source/capacity_settlement/tests/conftest.py
+++ source/capacity_settlement/tests/conftest.py
@@ -1,14 +1,13 @@
-import os
-from typing import Callable, Generator
+from typing import Generator
 
 import pytest
+import TestSessionConfiguration
 import yaml
 from pyspark.sql import SparkSession
 from telemetry_logging.logging_configuration import configure_logging
 from testcommon.container_test import DatabricksApiClient
 
 from tests import PROJECT_ROOT, Path
-from tests.testsession_configuration import TestSessionConfiguration
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -29,57 +28,6 @@ def spark() -> Generator[SparkSession, None, None]:
 def configure_dummy_logging() -> None:
     """Ensure that logging hooks don't fail due to _TRACER_NAME not being set."""
     configure_logging(cloud_role_name="any-cloud-role-name", tracer_name="any-tracer-name")
-
-
-@pytest.fixture(scope="session")
-def file_path_finder() -> Callable[[str], str]:
-    """
-    Returns the path of the file.
-    Please note that this only works if current folder haven't been changed prior using
-    `os.chdir()`. The correctness also relies on the prerequisite that this function is
-    actually located in a file located directly in the tests folder.
-    """
-
-    def finder(file: str) -> str:
-        return os.path.dirname(os.path.normpath(file))
-
-    return finder
-
-
-@pytest.fixture(scope="session")
-def source_path(file_path_finder: Callable[[str], str]) -> str:
-    """
-    Returns the <repo-root>/source folder path.
-    Please note that this only works if current folder haven't been changed prior using
-    `os.chdir()`. The correctness also relies on the prerequisite that this function is
-    actually located in a file located directly in the tests folder.
-    """
-    return file_path_finder(f"{__file__}/../..")
-
-
-@pytest.fixture(scope="session")
-def capacity_settlement_tests_path(source_path: str) -> str:
-    """
-    Returns the tests folder path for capacity settlement.
-    """
-    return f"{source_path}/capacity_settlement/tests"
-
-
-@pytest.fixture(scope="session")
-def tests_path(file_path_finder: Callable[[str], str]) -> str:
-    """Returns the tests folder path."""
-    return file_path_finder(f"{__file__}")
-
-
-@pytest.fixture(scope="session")
-def capacity_settlement_path(source_path: str) -> str:
-    """
-    Returns the source/capacity_settlement/ folder path.
-    Please note that this only works if current folder haven't been changed prior using
-    `os.chdir()`. The correctness also relies on the prerequisite that this function is
-    actually located in a file located directly in the tests folder.
-    """
-    return f"{source_path}/capacity_settlement/src"
 
 
 @pytest.fixture(scope="session")
