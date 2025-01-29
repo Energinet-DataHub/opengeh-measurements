311f37ae35bbce6bf5f0a674137d74b4775cd110
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-01-29 10:16:24 +0100

    Update electrical_heating_args.py

--- source/electrical_heating/src/opengeh_electrical_heating/application/job_args/electrical_heating_args.py
+++ source/electrical_heating/src/opengeh_electrical_heating/application/job_args/electrical_heating_args.py
@@ -1,12 +1,12 @@
-from dataclasses import dataclass
 from uuid import UUID
+
 from pydantic_settings import BaseSettings
 
-@dataclass
-class ElectricalHeatingArgs:
+
+class ElectricalHeatingArgs(BaseSettings):
     """Args for the electrical heating job."""
 
-    catalog_name: str
     orchestration_instance_id: UUID
     time_zone: str
+    catalog_name: str
     electricity_market_data_path: str
