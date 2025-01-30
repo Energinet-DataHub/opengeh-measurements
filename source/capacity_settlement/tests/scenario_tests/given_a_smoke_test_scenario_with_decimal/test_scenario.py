2c328f8464c616a3fc85720f146dbe1e64a58879
Troels Refstrup <Trore@Energinet.dk>
2025-01-30 10:10:26 +0100

    test

---
+++ source/capacity_settlement/tests/scenario_tests/given_a_smoke_test_scenario_with_decimal/test_scenario.py
@@ -0,0 +1,20 @@
+import pytest
+from testcommon.dataframes import (
+    assert_dataframes_and_schemas,
+    AssertDataframesConfiguration,
+)
+from testcommon.etl import get_then_names, TestCases
+
+
+@pytest.mark.parametrize("name", get_then_names())
+def test_case(
+    name: str,
+    test_cases: TestCases,
+    assert_dataframes_configuration: AssertDataframesConfiguration,
+) -> None:
+    test_case = test_cases[name]
+    assert_dataframes_and_schemas(
+        actual=test_case.actual,
+        expected=test_case.expected,
+        configuration=assert_dataframes_configuration,
+    )
