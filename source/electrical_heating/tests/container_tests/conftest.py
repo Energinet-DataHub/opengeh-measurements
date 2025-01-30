25bad0e3151f3939c86303f4c4a8a1bc89c263a5
Alexander <alexanderharrow@hotmail.com>
2025-01-30 10:14:08 +0100

    Updates

--- source/electrical_heating/tests/container_tests/conftest.py
+++ source/electrical_heating/tests/container_tests/conftest.py
@@ -1,25 +1,23 @@
-﻿import os
-from pathlib import Path
+﻿from pathlib import Path
 
 import pytest
-import yaml
+from pydantic_settings import BaseSettings
 from testcommon.container_test import DatabricksApiClient
 
-PROJECT_ROOT = Path(__file__).parent.parent
+PROJECT_ROOT = Path(__file__).parent.parent.parent
 
 
-def _load_settings_from_file(file_path: Path) -> dict:
-    if file_path.exists():
-        with file_path.open() as stream:
-            return yaml.safe_load(stream)
-    else:
-        return {}
+class ContainerSettings(BaseSettings):
+    DATABRICKS_TOKEN: str
+    WORKSPACE_URL: str
+
+    class Config:
+        env_file = f"{PROJECT_ROOT}/tests/container_tests/.env"
+        env_file_encoding = "utf-8"
 
 
 @pytest.fixture(scope="session")
 def databricks_api_client() -> DatabricksApiClient:
-    settings = _load_settings_from_file(PROJECT_ROOT / "tests" / "test.local.settings.yml")
-    databricks_token = settings.get("DATABRICKS_TOKEN") or os.getenv("DATABRICKS_TOKEN")
-    databricks_host = settings.get("WORKSPACE_URL") or os.getenv("WORKSPACE_URL")
-    databricksApiClient = DatabricksApiClient(databricks_token, databricks_host)
+    container_settings = ContainerSettings()
+    databricksApiClient = DatabricksApiClient(container_settings.DATABRICKS_TOKEN, container_settings.WORKSPACE_URL)
     return databricksApiClient
