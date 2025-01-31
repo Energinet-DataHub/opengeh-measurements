f45e266c3a6bfff215923133cc4a02bc819055ca
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-01-31 10:43:29 +0100

    fix: test and conftest package

--- source/bronze/src/opengeh_bronze/domain/transformations/bronze_measurements_transformation.py
+++ source/bronze/src/opengeh_bronze/domain/transformations/bronze_measurements_transformation.py
@@ -3,7 +3,8 @@ import os
 from pyspark.sql import DataFrame
 from pyspark.sql.protobuf.functions import from_protobuf
 
-message_name = "Measurement"
+message_name = "PersistSubmittedTransaction"
+alias_name = "measurement"
 
 
 def transform(bronze_measurements: DataFrame) -> DataFrame:
@@ -18,24 +19,22 @@ def unpack_proto(df):
         f"{os.getcwd()}/src/opengeh_bronze/infrastructure/contracts/assets/persist_submitted_transaction.binpb"
     )
 
-    return df.select(
-        from_protobuf(df.value, message_name, descFilePath=descriptor_file).alias("measurement"), "properties"
-    ).select("measurement.*", "properties")
+    return df.select(from_protobuf(df.value, message_name, descFilePath=descriptor_file).alias(alias_name))
 
 
 def map_message(df):
     return df.select(
-        "version",
-        "orchestration_instance_id",
-        "orchestratioon_type",
-        "metering_point_id",
-        "transaction_id",
-        "transaction_creation_datetime",
-        "start_datetime",
-        "end_datetime",
-        "metering_point_type",
-        "product",
-        "unit",
-        "resolution",
-        "points",
+        f"{alias_name}.version",
+        f"{alias_name}.orchestration_instance_id",
+        f"{alias_name}.orchestration_type",
+        f"{alias_name}.metering_point_id",
+        f"{alias_name}.transaction_id",
+        f"{alias_name}.transaction_creation_datetime",
+        f"{alias_name}.start_datetime",
+        f"{alias_name}.end_datetime",
+        f"{alias_name}.metering_point_type",
+        f"{alias_name}.product",
+        f"{alias_name}.unit",
+        f"{alias_name}.resolution",
+        f"{alias_name}.points",
     )
