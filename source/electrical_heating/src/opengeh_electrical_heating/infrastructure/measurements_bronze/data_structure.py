bd1b408fc45fa696b3c99cd74fe6421dca48c6b6
Christian Hansen <cenh@halfspace.io>
2025-01-31 15:48:40 +0100

    fixed newline

--- source/electrical_heating/src/opengeh_electrical_heating/infrastructure/measurements_bronze/data_structure.py
+++ source/electrical_heating/src/opengeh_electrical_heating/infrastructure/measurements_bronze/data_structure.py
@@ -2,6 +2,7 @@ from pyspark_functions.data_frame_wrapper import DataFrameWrapper
 from pyspark.sql import DataFrame
 from opengeh_electrical_heating.infrastructure.measurements_bronze.schemas.measurements_bronze_v1 import measurements_bronze_v1
 
+
 class MeasurementsBronze(DataFrameWrapper):
     """
     Represents the bronze measurements data structure.
