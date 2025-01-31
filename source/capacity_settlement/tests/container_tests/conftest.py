4028f5b937ee9faef90b00afb1cb11dd255c3d64
Jonas Michelsen <jvm@energinet.dk>
2025-01-31 07:37:03 +0100

    Update conftest.py

--- source/capacity_settlement/tests/container_tests/conftest.py
+++ source/capacity_settlement/tests/container_tests/conftest.py
@@ -4,8 +4,6 @@ import pytest
 from testcommon.container_test import DatabricksApiClient
 
 
-# this fixture is only used in CD and needs to be commented out for local testing
-# local testing uses the fixture in tests/conftest.py
 @pytest.fixture(scope="session")
 def databricks_api_client() -> DatabricksApiClient:
     databricks_token = os.getenv("DATABRICKS_TOKEN")
