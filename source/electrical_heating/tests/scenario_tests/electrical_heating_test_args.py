3f9ac1fac42700df984d0e46f4a3848180623431
chba <chba@kapacity.dk>
2025-02-03 12:49:18 +0100

    added doc

--- source/electrical_heating/tests/scenario_tests/electrical_heating_test_args.py
+++ source/electrical_heating/tests/scenario_tests/electrical_heating_test_args.py
@@ -7,5 +7,9 @@ class ElectricalHeatingTestArgs(ElectricalHeatingJobArgs):
     """Args for testing the electrical heating job."""
 
     def __init__(self, env_file_path: str) -> None:
+        """
+        Class inherits from ElectricalHeatingJobArgs by overriding the constructor and setting a specific path for
+        the environment file provided in the scenario tests with parameter env_file
+        """
         self.__class__.model_config = SettingsConfigDict(env_file=env_file_path)
         super().__init__()
