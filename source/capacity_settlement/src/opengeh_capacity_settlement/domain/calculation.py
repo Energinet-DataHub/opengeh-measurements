3c441c3e39f7c972cb89d56f341ea6d51c52e541
Jonas Michelsen <jvm@energinet.dk>
2025-02-03 13:31:00 +0100

    Update calculation.py

--- source/capacity_settlement/src/opengeh_capacity_settlement/domain/calculation.py
+++ source/capacity_settlement/src/opengeh_capacity_settlement/domain/calculation.py
@@ -70,7 +70,9 @@ def execute_core_logic(
 
 def _transform_quarterly_time_series_to_hourly(time_series_points: DataFrame) -> DataFrame:
     # Reduces observation time to hour value
-    time_series_points = time_series_points.withColumn(ColumNames.observation_time, F.date_trunc("hour", ColumNames.observation_time))
+    time_series_points = time_series_points.withColumn(
+        ColumNames.observation_time, F.date_trunc("hour", ColumNames.observation_time)
+    )
     # group by all columns except quantity and then sum the quantity
     group_by = [col for col in time_series_points.columns if col != ColumNames.quantity]
     time_series_points = time_series_points.groupBy(group_by).agg(F.sum(ColumNames.quantity).alias(ColumNames.quantity))
