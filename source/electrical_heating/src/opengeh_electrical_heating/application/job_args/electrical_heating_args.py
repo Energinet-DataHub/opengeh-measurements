787da419f44d1a8fb11d509dde969d84385e64dc
chba <chba@kapacity.dk>
2025-02-04 08:54:41 +0000

    changed the default settings for electrical heating

--- source/electrical_heating/src/opengeh_electrical_heating/application/job_args/electrical_heating_args.py
+++ source/electrical_heating/src/opengeh_electrical_heating/application/job_args/electrical_heating_args.py
@@ -1,9 +1,12 @@
 import os
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional, Tuple, Type
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, CliSettingsSource, PydanticBaseSettingsSource, SettingsConfigDict
+
+from opengeh_electrical_heating.application.job_args import electrical_heating_args
 
 DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
@@ -15,12 +18,12 @@ class ElectricalHeatingArgs(BaseSettings):
     The priority is CLI parameters first and then environment variables.
     """
 
-    catalog_name: str
-    time_zone: str = "Europe/Copenhagen"
-    execution_start_datetime: Optional[datetime] = None
-    orchestration_instance_id: uuid.UUID
-    electricity_market_data_path: str
-    model_config = SettingsConfigDict(env_file=DOTENV)
+    catalog_name: str = Field(alias="catalog-name")
+    time_zone: str = Field(alias="time-zone", default="Europe/Copenhagen")
+    execution_start_datetime: datetime = Field(alias="execution-start-datetime", default=datetime.now(timezone.utc))
+    orchestration_instance_id: uuid.UUID = Field(alias="orchestration-instance-id")
+    electricity_market_data_path: str = Field(alias="electricity-market-data-path")
+    model_config = SettingsConfigDict(env_file=DOTENV, populate_by_name=True)
 
     @classmethod
     def settings_customise_sources(
@@ -35,4 +38,5 @@ class ElectricalHeatingArgs(BaseSettings):
             CliSettingsSource(settings_cls, cli_parse_args=True, cli_ignore_unknown_args=True),
             env_settings,
             dotenv_settings,
+            init_settings,
         )
