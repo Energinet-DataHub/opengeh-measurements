8000f9e57886d2e2ba3959f82fb37c6b7c79fddb
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-04 08:35:18 +0100

    add test for protobuf

--- source/bronze/src/opengeh_bronze/infrastructure/helpers/path_helper.py
+++ source/bronze/src/opengeh_bronze/infrastructure/helpers/path_helper.py
@@ -3,3 +3,7 @@ from importlib.resources import files
 
 def get_protobuf_descriptor_path(file_name: str) -> str:
     return str(files("opengeh_bronze.infrastructure.contracts.assets").joinpath(file_name))
+
+
+def get_protobuf_path(file_name: str) -> str:
+    return str(files("opengeh_bronze.infrastructure.contracts").joinpath(file_name))
