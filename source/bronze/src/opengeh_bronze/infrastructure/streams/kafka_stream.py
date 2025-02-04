f90c27714d8d41c2bee788e482f6f303bfd371cc
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2025-02-04 10:33:59 +0100

    Enable continuous streaming of eventhub data (#207)

--- source/bronze/src/opengeh_bronze/infrastructure/streams/kafka_stream.py
+++ source/bronze/src/opengeh_bronze/infrastructure/streams/kafka_stream.py
@@ -1,10 +1,24 @@
 from pyspark.sql import SparkSession
 
+from opengeh_bronze.application.settings import KafkaAuthenticationSettings, SubmittedTransactionsStreamSettings
 from opengeh_bronze.domain.constants.database_names import DatabaseNames
 from opengeh_bronze.domain.constants.table_names import TableNames
 
 
-def submit_transactions(spark: SparkSession, kafka_options: dict) -> None:
-    spark.readStream.format("kafka").options(**kafka_options).load().writeTo(
-        f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}"
+def submit_transactions(spark: SparkSession) -> None:
+    stream_settings = SubmittedTransactionsStreamSettings()  # type: ignore
+    kafka_settings = KafkaAuthenticationSettings()  # type: ignore
+    kafka_options = kafka_settings.create_kafka_options()
+
+    df = spark.readStream.format("kafka").options(**kafka_options).load()
+
+    dfs = None
+
+    if stream_settings.continuous_streaming_enabled is True:
+        dfs = df.writeStream.trigger(continuous="1 second")
+    else:
+        dfs = df.writeStream.trigger(availableNow=True)
+
+    dfs.toTable(
+        f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}", outputMode="append"
     )
