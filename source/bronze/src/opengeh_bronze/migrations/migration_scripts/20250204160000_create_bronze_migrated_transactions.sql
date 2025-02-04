aa25e713349b8be358ca1ec96a8f57e809ffc748
Svend (SLB) <slb@halfspace.io>
2025-02-04 16:25:13 +0100

    Init of migrated table from Migration's Silver

---
+++ source/bronze/src/opengeh_bronze/migrations/migration_scripts/20250204160000_create_bronze_migrated_transactions.sql
@@ -0,0 +1,28 @@
+CREATE TABLE {bronze_database}.{bronze_migrated_transactions_table}
+(
+    metering_point_id STRING NOT NULL,
+    type_of_mp STRING NOT NULL,
+    historical_flag STRING NOT NULL,
+    resolution STRING NOT NULL,
+    transaction_id STRING,
+    transaction_insert_date TIMESTAMP NOT NULL,
+    unit STRING NOT NULL,
+    status INT NOT NULL,
+    read_reason STRING NOT NULL,
+    valid_from_date TIMESTAMP NOT NULL,
+    valid_to_date TIMESTAMP NOT NULL,
+    values ARRAY<
+        STRUCT<
+            position INT,
+            quality STRING,
+            quantity DECIMAL(18, 6)
+        >
+    > NOT NULL,
+    created TIMESTAMP NOT NULL
+)
+USING DELTA
+{SKIP_IN_SUBSYSTEM_TEST} PARTITIONED BY (partitioning_col)
+TBLPROPERTIES (
+    delta.deletedFileRetentionDuration = "interval 30 days",
+    delta.logRetentionDuration = "interval 30 days"
+)
