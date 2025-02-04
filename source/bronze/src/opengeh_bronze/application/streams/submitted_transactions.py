227047e53ec5b8d30f98dcca3650432a715d374c
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-02-04 09:21:01 +0100

    Enable continuous streaming of eventhub data

--- source/bronze/src/opengeh_bronze/application/streams/submitted_transactions.py
+++ source/bronze/src/opengeh_bronze/application/streams/submitted_transactions.py
@@ -1,11 +1,12 @@
 import opengeh_bronze.application.config.spark_session as spark_session
 import opengeh_bronze.infrastructure.streams.kafka_stream as kafka_stream
-from opengeh_bronze.application.settings.submitted_transactions_stream_settings import (
-    SubmittedTransactionsStreamSettings,
+from opengeh_bronze.application.settings.kafka_authentication_settings import (
+    KafkaAuthenticationSettings,
 )
 
 
 def submit_transactions() -> None:
     spark = spark_session.initialize_spark()
-    kafka_options = SubmittedTransactionsStreamSettings().create_kafka_options()
-    kafka_stream.submit_transactions(spark, kafka_options)
+    kafka_settings = KafkaAuthenticationSettings()  # type: ignore
+
+    kafka_stream.submit_transactions(spark, kafka_settings.create_kafka_options())
--- source/bronze/src/opengeh_bronze/infrastructure/streams/kafka_stream.py
+++ source/bronze/src/opengeh_bronze/infrastructure/streams/kafka_stream.py
@@ -1,10 +1,23 @@
 from pyspark.sql import SparkSession
 
+from opengeh_bronze.application.settings.submitted_transactions_stream_settings import (
+    SubmittedTransactionsStreamSettings,
+)
 from opengeh_bronze.domain.constants.database_names import DatabaseNames
 from opengeh_bronze.domain.constants.table_names import TableNames
 
 
 def submit_transactions(spark: SparkSession, kafka_options: dict) -> None:
-    spark.readStream.format("kafka").options(**kafka_options).load().writeTo(
-        f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}"
+    stream_settings = SubmittedTransactionsStreamSettings()  # type: ignore
+
+    df = spark.readStream.format("kafka").options(**kafka_options).load()
+    dfs = None
+
+    if stream_settings.continuous_streaming_enabled is True:
+        dfs = df.writeStream.trigger(continuous="1 second")
+    else:
+        dfs = df.writeStream.trigger(once=True)
+
+    dfs.toTable(
+        f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}", outputMode="append"
     )
