e8dad75355f960f4ab95d90d68f7dcfdeffbb733
HenrikSommer <sommerh@gmail.com>
2025-02-05 14:45:48 +0100

    Fix references in substitutions.py

--- source/core/src/core/migrations/substitutions.py
+++ source/core/src/core/migrations/substitutions.py
@@ -1,5 +1,5 @@
-from src.core.gold.infrastructure.config import GoldDatabaseNames, GoldTableNames
-from src.core.silver.infrastructure.config import SilverDatabaseNames, SilverTableNames
+from core.gold.infrastructure.config import GoldDatabaseNames, GoldTableNames
+from core.silver.infrastructure.config import SilverDatabaseNames, SilverTableNames
 
 
 def substitutions() -> dict[str, str]:
