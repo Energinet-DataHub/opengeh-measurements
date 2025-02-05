94b4c8b545d72c0d739aa56a7c3489bb9f616ddd
HenrikSommer <sommerh@gmail.com>
2025-02-05 15:41:11 +0100

    Fix references

--- source/core/tests/gold/infrastructure/adapters/test_delta_silver_adapter.py
+++ source/core/tests/gold/infrastructure/adapters/test_delta_silver_adapter.py
@@ -4,14 +4,13 @@ from gold.helpers.silver_builder import SilverMeasurementsDataFrameBuilder
 from pyspark.sql import DataFrame, SparkSession
 
 from core.gold.infrastructure.adapters.delta_silver_adapter import DeltaSilverAdapter
-from core.gold.infrastructure.config.database_names import DatabaseNames
-from core.gold.infrastructure.config.table_names import TableNames
+from core.silver.infrastructure.config import SilverDatabaseNames, SilverTableNames
 
 
 def test__read_stream__should_return_dataframe(spark: SparkSession, create_silver_tables):
     # Arrange
     silver_adapter = DeltaSilverAdapter(spark)
-    table_name = TableNames.silver_measurements
+    table_name = SilverTableNames.silver_measurements
 
     # Act
     result = silver_adapter.read_stream(table_name)
@@ -23,21 +22,23 @@ def test__read_stream__should_return_dataframe(spark: SparkSession, create_silve
 def test__read_stream__should_contain_rows_in_silver(spark: SparkSession, create_silver_tables):
     # Arrange
     silver_adapter = DeltaSilverAdapter(spark)
-    table_name = TableNames.silver_measurements
+    table_name = SilverTableNames.silver_measurements
     test_table = f"{table_name}_test"
     metering_point_id = random.randint(0, 999999999999999999)
     df_silver = SilverMeasurementsDataFrameBuilder(spark).add_row(metering_point_id=metering_point_id).build()
-    df_silver.write.format("delta").mode("append").saveAsTable(f"{DatabaseNames.silver}.{table_name}")
+    df_silver.write.format("delta").mode("append").saveAsTable(f"{SilverDatabaseNames.silver}.{table_name}")
 
     # Act
     result = silver_adapter.read_stream(table_name)
     result.writeStream.format("delta").option("checkpointLocation", "/tmp/checkpoints/read_stream_test").foreachBatch(
-        lambda df, epoch_id: df.write.format("delta").mode("append").saveAsTable(f"{DatabaseNames.silver}.{test_table}")
+        lambda df, epoch_id: df.write.format("delta")
+        .mode("append")
+        .saveAsTable(f"{SilverDatabaseNames.silver}.{test_table}")
     ).trigger(once=True).start().awaitTermination()
 
     # Assert
     assert (
-        spark.read.table(f"{DatabaseNames.silver}.{test_table}")
+        spark.read.table(f"{SilverDatabaseNames.silver}.{test_table}")
         .filter(f"metering_point_id == '{metering_point_id}'")
         .count()
         == 1
