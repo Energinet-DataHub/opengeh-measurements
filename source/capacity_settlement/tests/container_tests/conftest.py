21f234c4a1561ecf08809effed9e55ac3e3fba8d
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2025-02-05 12:39:58 +0100

    refac: Align capacity settlement container test setup with electrical… (#220)

--- source/capacity_settlement/tests/container_tests/conftest.py
+++ source/capacity_settlement/tests/container_tests/conftest.py
@@ -1,12 +1,17 @@
-import os
-
 import pytest
+from environment_configuration import EnvironmentConfiguration
 from testcommon.container_test import DatabricksApiClient
 
 
-# For the test to be started from your local machine, you need to set the environment variables in a .env file.
 @pytest.fixture(scope="session")
-def databricks_api_client() -> DatabricksApiClient:
-    databricks_token = os.getenv("DATABRICKS_TOKEN")
-    databricks_host = os.getenv("WORKSPACE_URL")
-    return DatabricksApiClient(databricks_token, databricks_host)
+def environment_configuration() -> EnvironmentConfiguration:
+    return EnvironmentConfiguration()
+
+
+@pytest.fixture(scope="session")
+def databricks_api_client(environment_configuration: EnvironmentConfiguration) -> DatabricksApiClient:
+    databricksApiClient = DatabricksApiClient(
+        environment_configuration.databricks_token,
+        environment_configuration.workspace_url,
+    )
+    return databricksApiClient
