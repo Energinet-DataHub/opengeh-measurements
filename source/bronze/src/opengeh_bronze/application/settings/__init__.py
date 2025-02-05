a9db60668243f0c636a545862c5b81c7024d89ff
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-05 11:36:04 +0100

    feat: Publish receipt bronze stream (#164)
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    Co-authored-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/bronze/src/opengeh_bronze/application/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-from opengeh_bronze.application.settings.kafka_authentication_settings import KafkaAuthenticationSettings
-from opengeh_bronze.application.settings.submitted_transactions_stream_settings import (
-    SubmittedTransactionsStreamSettings,
-)
-
-__all__ = ["KafkaAuthenticationSettings", "SubmittedTransactionsStreamSettings"]
--- source/bronze/src/opengeh_bronze/infrastructure/streams/kafka_stream.py
+++ source/bronze/src/opengeh_bronze/infrastructure/streams/kafka_stream.py
@@ -1,24 +1,53 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 
-from opengeh_bronze.application.settings import KafkaAuthenticationSettings, SubmittedTransactionsStreamSettings
+import opengeh_bronze.infrastructure.helpers.path_helper as path_helper
 from opengeh_bronze.domain.constants.database_names import DatabaseNames
 from opengeh_bronze.domain.constants.table_names import TableNames
+from opengeh_bronze.infrastructure.config.storage_container_names import StorageContainerNames
+from opengeh_bronze.infrastructure.settings import (
+    KafkaAuthenticationSettings,
+    StorageAccountSettings,
+    SubmittedTransactionsStreamSettings,
+)
 
 
-def submit_transactions(spark: SparkSession) -> None:
-    stream_settings = SubmittedTransactionsStreamSettings()  # type: ignore
-    kafka_settings = KafkaAuthenticationSettings()  # type: ignore
-    kafka_options = kafka_settings.create_kafka_options()
+class KafkaStream:
+    kafka_options: dict
 
-    df = spark.readStream.format("kafka").options(**kafka_options).load()
+    def __init__(self) -> None:
+        self.kafka_options = KafkaAuthenticationSettings().create_kafka_options()  # type: ignore
+        self.data_lake_settings = StorageAccountSettings().DATALAKE_STORAGE_ACCOUNT  # type: ignore
 
-    dfs = None
+    def submit_transactions(self, spark: SparkSession) -> None:
+        checkpoint_location = path_helper.get_checkpoint_path(
+            self.data_lake_settings, StorageContainerNames.bronze, "submitted_transactions"
+        )
+        write_stream = (
+            spark.readStream.format("kafka")
+            .options(**self.kafka_options)
+            .load()
+            .writeStream.outputMode("append")
+            .option("checkpointLocation", checkpoint_location)
+        )
 
-    if stream_settings.continuous_streaming_enabled is True:
-        dfs = df.writeStream.trigger(continuous="1 second")
-    else:
-        dfs = df.writeStream.trigger(availableNow=True)
+        stream_settings = SubmittedTransactionsStreamSettings()  # type: ignore
 
-    dfs.toTable(
-        f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}", outputMode="append"
-    )
+        if stream_settings.continuous_streaming_enabled is True:
+            write_stream = write_stream.trigger(continuous="1 second")
+        else:
+            write_stream = write_stream.trigger(availableNow=True)
+
+        write_stream.toTable(f"{DatabaseNames.bronze_database}.{TableNames.bronze_submitted_transactions_table}")
+
+    def write_stream(
+        self,
+        dataframe: DataFrame,
+    ):
+        checkpoint_location = path_helper.get_checkpoint_path(
+            self.data_lake_settings, StorageContainerNames.bronze, "processed_transactions"
+        )
+        event_hub_instance = KafkaAuthenticationSettings().event_hub_instance  # type: ignore
+
+        dataframe.writeStream.format("kafka").options(**self.kafka_options).option("topic", event_hub_instance).option(
+            "checkpointLocation", checkpoint_location
+        ).trigger(availableNow=True).start()
