7a8fe2589d326daaffc8f35abcffc27534b5b369
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-02-06 12:32:41 +0100

    Add env vars to test config

--- source/silver/tests/conftest.py
+++ source/silver/tests/conftest.py
@@ -15,6 +15,9 @@ def pytest_runtest_setup() -> None:
     """
     os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"] = "app_conn_str"
     os.environ["CATALOG_NAME"] = "spark_catalog"
+    os.environ["BRONZE_DATABASE_NAME"] = "measurements_bronze"
+    os.environ["SILVER_DATABASE_NAME"] = "measurements_silver"
+    os.environ["GOLD_DATABASE_NAME"] = "measurements_gold"
     os.environ["DATALAKE_STORAGE_ACCOUNT"] = "datalake"
 
 
