991dba74af8d4b6aaf3f4532f7817f0f40524759
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-02-06 13:10:25 +0100

    Update conftest

--- source/bronze/tests/conftest.py
+++ source/bronze/tests/conftest.py
@@ -14,6 +14,9 @@ def pytest_runtest_setup() -> None:
     This function is called before each test function is executed.
     """
     os.environ["CATALOG_NAME"] = "spark_catalog"
+    os.environ["BRONZE_DATABASE_NAME"] = "measurements_bronze"
+    os.environ["SILVER_DATABASE_NAME"] = "measurements_silver"
+    os.environ["GOLD_DATABASE_NAME"] = "measurements_gold"
 
 
 @pytest.fixture(scope="session")
