916de642b5d01425de521a56cbbc4a073f2d9f93
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-06 16:04:44 +0100

    Update schema.py

--- source/electrical_heating/src/opengeh_electrical_heating/infrastructure/measurements/measurements_bronze/schema.py
+++ source/electrical_heating/src/opengeh_electrical_heating/infrastructure/measurements/measurements_bronze/schema.py
@@ -2,70 +2,41 @@ import pyspark.sql.types as t
 
 nullable = True
 
-
-point = t.StructType(
+# Calculated measurements from electrical heating or capacity settlement calculations.
+calculated_measurements = t.StructType(
     [
         #
-        # A sequential numbering. Always starting at 1.
-        # Combined with start_datetime and resolution it can be used to determine the observation time
-        # of the individual points in the transaction.
-        t.StructField("position", t.StringType(), not nullable),
-        #
-        # The quantity calculated by the electrical heating job
-        t.StructField("quantity", t.DecimalType(18, 3), not nullable),
-        #
-        # Must be: "calculated"
-        t.StructField("quality", t.StringType(), not nullable),
-    ]
-)
-
-# Electrical heating result is delivered to Measurements
-# as one transaction per metering point id.
-measurements_bronze_v1 = t.StructType(
-    [
-        #
-        # Must be: "electrical_heating"
+        # "electrical_heating" or "capacity_settlement"
         t.StructField("orchestration_type", t.StringType(), not nullable),
         #
-        # The GUID value of the electrical heating calculation job
+        # ID of the orchestration that initiated the calculation job
         t.StructField("orchestration_instance_id", t.StringType(), not nullable),
         #
-        # Metering Point ID
-        # GSRN number
+        # Metering point ID
         t.StructField("metering_point_id", t.StringType(), not nullable),
         #
-        # Transaction ID. Created by the electrical heating calculation job.
+        # Transaction ID. Created by the calculation job.
+        # The ID refers to a continous set of measurements for a specific combination of orchestration_id and metering_point_id.
+        # There are no time gaps for a given transaction id. Gaps introduces a new transaction ID after the gap.
         t.StructField("transaction_id", t.StringType(), not nullable),
         #
         # A DateTime value indicating when the transaction was created
-        # by the electrical heating calculation job.
+        # by the calculation job.
         t.StructField("transaction_creation_datetime", t.TimestampType(), not nullable),
         #
-        # Must be "electrical_heating"
+        # "electrical_heating" or "capacity_settlement"
         t.StructField("metering_point_type", t.StringType(), not nullable),
         #
-        # The product must be the code for "active energy"
-        # Must be: 8716867000030
-        t.StructField("product", t.StringType(), not nullable),
-        #
-        # Metering Unit
-        # Must be: "kWh"
-        t.StructField("unit", t.StringType(), not nullable),
-        #
-        # Must be: "PT1H"
+        # Always: "PT1H"
         t.StructField("resolution", t.StringType(), not nullable),
         #
-        # The start DateTime of the transaction (the first position’s observation time)
-        t.StructField("start_datetime", t.TimestampType(), not nullable),
-        #
-        # The end DateTime of the transaction
-        t.StructField("end_datetime", t.TimestampType(), not nullable),
-        #
-        # Points
+        # UTC time
         t.StructField(
-            "points",
-            t.ArrayType(point, containsNull=False),
+            "observation_time",
+            t.TimestampType(),
             not nullable,
         ),
+        # The calculated quantity
+        t.StructField("quantity", t.DecimalType(18, 3), not nullable),
     ]
 )
