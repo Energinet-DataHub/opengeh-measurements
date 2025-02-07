e567d5026d5ce98475a00c426072bec235c1ea86
HenrikSommer <sommerh@gmail.com>
2025-02-07 10:28:22 +0100

    Insert GO statements to split queries

--- source/core/src/core/migrations/migration_scripts/20250206160000_drop_executed_migrations_tables_from_medallion_databases.sql
+++ source/core/src/core/migrations/migration_scripts/20250206160000_drop_executed_migrations_tables_from_medallion_databases.sql
@@ -1,3 +1,9 @@
-DROP TABLE IF EXISTS {bronze_database}.executed_migrations;
-DROP TABLE IF EXISTS {silver_database}.executed_migrations;
-DROP TABLE IF EXISTS {gold_database}.executed_migrations;
+DROP TABLE IF EXISTS {bronze_database}.executed_migrations
+
+GO
+
+DROP TABLE IF EXISTS {silver_database}.executed_migrations
+
+GO
+
+DROP TABLE IF EXISTS {gold_database}.executed_migrations
