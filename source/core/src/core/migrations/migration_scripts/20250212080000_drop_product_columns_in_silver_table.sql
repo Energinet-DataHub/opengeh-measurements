2364cf63c03a32a733b0d0f9474a073e821fb6d4
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-12 08:18:36 +0100

    fix tests

--- source/core/src/core/migrations/migration_scripts/20250212080000_drop_product_columns_in_silver_table.sql
+++ source/core/src/core/migrations/migration_scripts/20250212080000_drop_product_columns_in_silver_table.sql
@@ -1,2 +1,7 @@
 ALTER TABLE {silver_database}.{silver_measurements_table}
+SET TBLPROPERTIES ('delta.columnMapping.mode' = 'name')
+
+GO
+
+ALTER TABLE {silver_database}.{silver_measurements_table}
 DROP COLUMN product
