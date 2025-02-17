562733322e870af612964cf47b2bcedb6968f4a9
Jonas Michelsen <jvm@energinet.dk>
2025-02-17 12:42:14 +0100

    .

---
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/environment_configuration.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
+
+class EnvironmentConfiguration(BaseSettings):
+    """
+    Contains the environment configuration for the tests.
+    This class must be included when running tests in CD.
+    """
+
+    databricks_token: str = Field(alias="DATABRICKS_TOKEN")
+    workspace_url: str = Field(alias="WORKSPACE_URL")
+
+    model_config = SettingsConfigDict(
+        env_file=f"{PROJECT_ROOT}/tests/.env",
+        env_file_encoding="utf-8",
+    )
