f5e3626739a72336ffd65dce083e5df0ef52d99c
Bjarke Meier <hej@bjarkemeier.dk>
2025-02-17 20:36:53 +0100

    ruff fix

--- source/geh_calculated_measurements/tests/electrical_heating/utils/measurements_utils.py
+++ source/geh_calculated_measurements/tests/electrical_heating/utils/measurements_utils.py
@@ -1,4 +1,3 @@
-from ast import alias
 import pyspark.sql.functions as F
 from geh_common.testing.dataframes import assert_schema
 from pyspark.sql import DataFrame, SparkSession
@@ -19,7 +18,9 @@ def create_calculated_measurements_dataframe(spark: SparkSession, measurements: 
         F.from_json(F.col("points"), ArrayType(point)).alias("points"),
         F.to_timestamp(F.col("start_datetime"), "yyyy-MM-dd HH:mm:ss").alias("start_datetime"),
         F.to_timestamp(F.col("end_datetime"), "yyyy-MM-dd HH:mm:ss").alias("end_datetime"),
-        F.to_timestamp(F.col("transaction_creation_datetime"), "yyyy-MM-dd HH:mm:ss").alias("transaction_creation_datetime"),
+        F.to_timestamp(F.col("transaction_creation_datetime"), "yyyy-MM-dd HH:mm:ss").alias(
+            "transaction_creation_datetime"
+        ),
     )
 
     measurements = spark.createDataFrame(measurements.rdd, schema=calculated_measurements_schema, verifySchema=True)
