9fa4fa99d7a56f5adeff64de60cead9f569b0da9
Alexander <alexanderharrow@hotmail.com>
2025-02-17 21:22:35 +0100

    Updates

--- source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/fixtures/eletrical_heating_fixture.py
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/fixtures/eletrical_heating_fixture.py
@@ -1,10 +1,17 @@
 import uuid
+from datetime import timedelta
 
+from azure.identity import DefaultAzureCredential
+from azure.monitor.query import LogsQueryClient, LogsQueryPartialResult, LogsQueryResult
 from databricks.sdk.service.jobs import RunResultState
+from environment_configuration import EnvironmentConfiguration
 from geh_common.testing.container_test.databricks_api_client import DatabricksApiClient
+
+from geh_calculated_measurements.opengeh_electrical_heating.domain import ColumnNames
 
 
 class JobState:
+    orchestrator_instance_id: str
     job_id: int
     run_id: int
     run_result_state: RunResultState
@@ -13,18 +20,27 @@ class JobState:
 class ElectricalHeatingFixture:
     job_state: JobState
 
-    def __init__(self, databricks_api_client: DatabricksApiClient):
-        self.databricks_api_client = databricks_api_client
+    def __init__(self, environment_configuration: EnvironmentConfiguration):
+        self.databricks_api_client = DatabricksApiClient(
+            environment_configuration.databricks_token,
+            environment_configuration.workspace_url,
+        )
         self.job_state = JobState()
+        self.client = LogsQueryClient(DefaultAzureCredential())
+        self.WORKSPACE_ID = environment_configuration.workspace_id
 
     def get_job_id(self) -> int:
         return self.databricks_api_client.get_job_id("ElectricalHeating")
 
     def start_job(self, job_id: int) -> int:
+        self.job_state.orchestrator_instance_id = str(uuid.uuid4())
         params = [
-            f"--orchestration-instance-id={str(uuid.uuid4())}",
+            f"--{ColumnNames.orchestration_instance_id}={self.job_state.orchestrator_instance_id}",
         ]
         return self.databricks_api_client.start_job(job_id, params)
 
     def wait_for_job_to_completion(self, run_id: int) -> RunResultState:
         return self.databricks_api_client.wait_for_job_completion(run_id)
+
+    def query_workspace_logs(self, query: str, timeout: int = 5) -> LogsQueryResult | LogsQueryPartialResult:
+        return self.client.query_workspace(self.WORKSPACE_ID, query, timespan=timedelta(minutes=timeout))
--- source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/test_electrical_heating.py
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/test_electrical_heating.py
@@ -1,7 +1,10 @@
 import unittest
 
 import pytest
+from azure.monitor.query import LogsQueryStatus
 from fixtures.eletrical_heating_fixture import ElectricalHeatingFixture
+
+from geh_calculated_measurements.opengeh_electrical_heating.domain import ColumnNames
 
 
 class TestElectricalHeating(unittest.TestCase):
@@ -14,18 +17,41 @@ class TestElectricalHeating(unittest.TestCase):
         TestElectricalHeating.fixture = electrical_heating_fixture
 
     @pytest.mark.order(1)
-    def test__get_job_id(self):
+    def test__given_job_input(self):
+        # Arrange & Act
         self.fixture.job_state.job_id = self.fixture.get_job_id()
+
+        # Assert
         assert self.fixture.job_state.job_id is not None
 
     @pytest.mark.order(2)
-    def test__start_job(self):
+    def test__when_job_started(self):
+        # Arrange & Act
         self.fixture.job_state.run_id = self.fixture.start_job(self.fixture.job_state.job_id)
+
+        # Assert
         assert self.fixture.job_state.run_id is not None
 
     @pytest.mark.order(3)
-    def test__wait_for_job_to_complete(self):
+    def test__then_job_is_completed(self):
         self.fixture.job_state.run_result_state = self.fixture.wait_for_job_to_completion(self.fixture.job_state.run_id)
         assert self.fixture.job_state.run_result_state.value == "SUCCESS", (
             f"Job did not complete successfully: {self.fixture.job_state.run_result_state.value}"
         )
+
+    @pytest.mark.order(4)
+    def test__and_then_logged_to_application_insights(self):
+        # Arrange
+        query = f"""
+        AppTraces 
+        | where Properties["{ColumnNames.orchestration_instance_id}"] == '{self.fixture.job_state.orchestrator_instance_id}'
+        """
+
+        # Act
+        actual = self.fixture.query_workspace_logs(query)
+
+        # Assert
+        assert actual.status == LogsQueryStatus.SUCCESS, (
+            f"Log workspace query did not complete successfully: {actual.status}"
+        )
+        assert actual.tables.count > 0, "Log workspace query is empty."  # type: ignore
