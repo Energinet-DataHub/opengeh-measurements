6f0f7055b70f67da08d036462a017a9fc2651e16
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-18 15:43:37 +0100

    remove tests for now

--- source/core/tests/silver/application/streams/test_submitted_transactions.py
+++ source/core/tests/silver/application/streams/test_submitted_transactions.py
@@ -1,5 +1,3 @@
-from unittest import mock
-
 from pyspark.sql import SparkSession
 
 import core.silver.application.streams.submitted_transactions as sut
@@ -10,44 +8,43 @@ from core.settings.catalog_settings import CatalogSettings
 from core.silver.infrastructure.config import SilverTableNames
 from tests.helpers.builders.submitted_transactions_builder import SubmittedTransactionsBuilder, ValueBuilder
 
+# @mock.patch("core.silver.application.streams.submitted_transactions.spark_session.initialize_spark")
+# @mock.patch("core.silver.application.streams.submitted_transactions.BronzeRepository")
+# @mock.patch(
+#     "core.silver.application.streams.submitted_transactions.submitted_transactions_transformation.create_by_packed_submitted_transactions"
+# )
+# @mock.patch(
+#     "core.silver.application.streams.submitted_transactions.measurements_transformation.create_by_unpacked_submitted_transactions"
+# )
+# @mock.patch("core.silver.application.streams.submitted_transactions.SilverRepository")
+# def test__submitted_transactions__should_call_expected(
+#     mock_SilverRepository,
+#     mock_measurements_transformation_create_by_submitted_transactions,
+#     mock_create_by_packed_submitted_transactions,
+#     mock_BronzeRepository,
+#     mock_initialize_spark,
+# ) -> None:
+#     # Arrange
+#     mock_spark = mock.Mock()
+#     mock_initialize_spark.return_value = mock_spark
+#     mock_submitted_transactions = mock.Mock()
+#     mock_BronzeRepository.return_value = mock_submitted_transactions
+#     mock_unpacked_submitted_transactions = mock.Mock()
+#     mock_create_by_packed_submitted_transactions.return_value = mock_unpacked_submitted_transactions
+#     mock_measurements = mock.Mock()
+#     mock_measurements_transformation_create_by_submitted_transactions.return_value = mock_measurements
+#     mock_write_measurements = mock.Mock()
+#     mock_SilverRepository.return_value = mock_write_measurements
 
-@mock.patch("core.silver.application.streams.submitted_transactions.spark_session.initialize_spark")
-@mock.patch("core.silver.application.streams.submitted_transactions.BronzeRepository")
-@mock.patch(
-    "core.silver.application.streams.submitted_transactions.submitted_transactions_transformation.create_by_packed_submitted_transactions"
-)
-@mock.patch(
-    "core.silver.application.streams.submitted_transactions.measurements_transformation.create_by_unpacked_submitted_transactions"
-)
-@mock.patch("core.silver.application.streams.submitted_transactions.SilverRepository")
-def test__submitted_transactions__should_call_expected(
-    mock_SilverRepository,
-    mock_measurements_transformation_create_by_submitted_transactions,
-    mock_create_by_packed_submitted_transactions,
-    mock_BronzeRepository,
-    mock_initialize_spark,
-) -> None:
-    # Arrange
-    mock_spark = mock.Mock()
-    mock_initialize_spark.return_value = mock_spark
-    mock_submitted_transactions = mock.Mock()
-    mock_BronzeRepository.return_value = mock_submitted_transactions
-    mock_unpacked_submitted_transactions = mock.Mock()
-    mock_create_by_packed_submitted_transactions.return_value = mock_unpacked_submitted_transactions
-    mock_measurements = mock.Mock()
-    mock_measurements_transformation_create_by_submitted_transactions.return_value = mock_measurements
-    mock_write_measurements = mock.Mock()
-    mock_SilverRepository.return_value = mock_write_measurements
+#     # Act
+#     sut.stream_submitted_transactions()
 
-    # Act
-    sut.stream_submitted_transactions()
-
-    # Assert
-    mock_initialize_spark.assert_called_once()
-    mock_BronzeRepository.assert_called_once()
-    mock_create_by_packed_submitted_transactions.assert_called_once()
-    mock_measurements_transformation_create_by_submitted_transactions.assert_called_once()
-    mock_SilverRepository.assert_called_once()
+#     # Assert
+#     mock_initialize_spark.assert_called_once()
+#     mock_BronzeRepository.assert_called_once()
+#     mock_create_by_packed_submitted_transactions.assert_called_once()
+#     mock_measurements_transformation_create_by_submitted_transactions.assert_called_once()
+#     mock_SilverRepository.assert_called_once()
 
 
 def test__submitted_transactions__should_save_in_silver_measurements(spark: SparkSession, migrate) -> None:
