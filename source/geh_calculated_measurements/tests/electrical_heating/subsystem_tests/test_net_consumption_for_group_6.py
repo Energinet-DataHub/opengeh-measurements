819687a0056172c3bfb371aa8472dba6a328be75
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-02-19 13:07:48 +0000

    Added new test file

---
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/test_net_consumption_for_group_6.py
@@ -0,0 +1,61 @@
+import unittest
+
+import pytest
+from azure.monitor.query import LogsQueryStatus
+from databricks.sdk.service.jobs import RunResultState
+from fixtures.eletrical_heating_fixture import ElectricalHeatingFixture
+
+
+class TestNetConsumptionForGroup6(unittest.TestCase):
+    """
+    Subsystem test that verifies a Databricks electrical heating job runs successfully to completion.
+    """
+
+    @pytest.fixture(autouse=True, scope="class")
+    def setup_fixture(self, electrical_heating_fixture: ElectricalHeatingFixture) -> None:
+        ElectricalHeatingFixture.fixture = electrical_heating_fixture
+
+    @pytest.mark.order(1)
+    def test__given_job_input(self) -> None:
+        # Act
+        self.fixture.job_state.job_id = self.fixture.get_job_id()
+
+        # Assert
+        assert self.fixture.job_state.job_id is not None
+
+    @pytest.mark.order(2)
+    def test__when_job_started(self) -> None:
+        # Act
+        self.fixture.job_state.run_id = self.fixture.start_job(self.fixture.job_state.job_id)
+
+        # Assert
+        assert self.fixture.job_state.run_id is not None
+
+    @pytest.mark.order(3)
+    def test__then_job_is_completed(self) -> None:
+        # Act
+        self.fixture.job_state.run_result_state = self.fixture.wait_for_job_to_completion(self.fixture.job_state.run_id)
+
+        # Assert
+        assert self.fixture.job_state.run_result_state == RunResultState.SUCCESS, (
+            f"Job did not complete successfully: {self.fixture.job_state.run_result_state.value}"
+        )
+
+    @pytest.mark.order(4)
+    def test__and_then_logged_to_application_insights(self) -> None:
+        # Arrange
+        if self.fixture.job_state.run_result_state != RunResultState.SUCCESS:
+            raise Exception("A previous test did not complete successfully.")
+
+        query = f"""
+        AppTraces
+        | where Properties["Subsystem"] == 'measurements' 
+        | where Properties["orchestration-instance-id"] == '{self.fixture.job_state.orchestrator_instance_id}'
+        """
+
+        # Act
+        actual = self.fixture.wait_for_log_query_completion(query)
+
+        # Assert
+        assert actual.status == LogsQueryStatus.SUCCESS, f"Query did not complete successfully: {actual.status}"
+        assert len(actual.tables[0].rows) > 0, "Query is empty."  # type: ignore
