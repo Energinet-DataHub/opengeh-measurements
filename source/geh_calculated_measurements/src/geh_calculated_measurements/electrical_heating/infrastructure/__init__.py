9ff666c7441978ac8744b40cb9ba049d2a2e837c
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-19 08:56:57 +0100

    ...

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/__init__.py
@@ -1,7 +1,7 @@
 from .electricity_market.child_metering_points.wrapper import ChildMeteringPoints
 from .electricity_market.consumption_metering_point_periods.wrapper import ConsumptionMeteringPointPeriods
 from .electricity_market.repository import Repository as ElectricityMarketRepository
-from .measurements.calculated_measurements.wrapper import CalculatedMeasurementsForStorage
+from .measurements.calculated_measurements.wrapper import CalculatedMeasurementsStorageModel
 from .measurements.measurements_gold.wrapper import TimeSeriesPoints
 from .measurements.repository import Repository as MeasurementsRepository
 from .spark_initializor import initialize_spark
@@ -14,6 +14,6 @@ __all__ = [
     "ConsumptionMeteringPointPeriods",
     # Measurements core repository and types
     "MeasurementsRepository",
-    "CalculatedMeasurementsForStorage",
+    "CalculatedMeasurementsStorageModel",
     "TimeSeriesPoints",
 ]
--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/measurements/calculated_measurements/calculated_measurements_factory.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/measurements/calculated_measurements/calculated_measurements_factory.py
@@ -6,14 +6,14 @@ from geh_calculated_measurements.electrical_heating.domain import ColumnNames
 from geh_calculated_measurements.electrical_heating.domain.calculated_measurements_daily import (
     CalculatedMeasurementsDaily,
 )
-from geh_calculated_measurements.electrical_heating.infrastructure import CalculatedMeasurementsForStorage
+from geh_calculated_measurements.electrical_heating.infrastructure import CalculatedMeasurementsStorageModel
 
 
 def create(
     calculated_measurements: CalculatedMeasurementsDaily, orchestration_instance_id: UUID, orchestration_type: str
-) -> CalculatedMeasurementsForStorage:
+) -> CalculatedMeasurementsStorageModel:
     df = calculated_measurements.df.withColumn(
         ColumnNames.orchestration_instance_id, F.lit(str(orchestration_instance_id))
     ).withColumn(ColumnNames.orchestration_type, F.lit(orchestration_type))
 
-    return CalculatedMeasurementsForStorage(df)
+    return CalculatedMeasurementsStorageModel(df)
