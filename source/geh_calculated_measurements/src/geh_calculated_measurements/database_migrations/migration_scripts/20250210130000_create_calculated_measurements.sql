7d53ef85ffa9bae2a06b9d6c63deb1fdfe61e2aa
Christian Hansen <cenh@halfspace.io>
2025-02-19 18:57:54 +0100

    updated

--- source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migration_scripts/20250210130000_create_calculated_measurements.sql
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migration_scripts/20250210130000_create_calculated_measurements.sql
@@ -1,18 +1,13 @@
 CREATE TABLE IF NOT EXISTS {calculated_measurements_database}.{calculated_measurements_table}
 (
-    orchestration_type STRING,
-    orchestration_instance_id STRING,
-    metering_point_id STRING,
-    transaction_id STRING,
-    transaction_creation_datetime TIMESTAMP,
-    metering_point_type STRING,
-    product STRING,
-    unit STRING,
-    resolution STRING,
-    start_datetime TIMESTAMP,
-    end_datetime TIMESTAMP,
-    observation_time TIMESTAMP,
-    quantity DECIMAL(18, 3)
+    orchestration_type STRING NOT NULL,
+    orchestration_instance_id STRING NOT NULL,
+    metering_point_id STRING NOT NULL,
+    transaction_id STRING NOT NULL,
+    transaction_creation_datetime TIMESTAMP NOT NULL,
+    metering_point_type STRING NOT NULL,
+    date TIMESTAMP NOT NULL,
+    quantity DECIMAL(18, 3) NOT NULL
 )
 USING DELTA
 TBLPROPERTIES (
