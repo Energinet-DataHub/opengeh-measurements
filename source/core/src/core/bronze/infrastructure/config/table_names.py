4b32e09a9a4bce58e3672177b9eb6dd305105eb9
Svend Lund Breddam <118453412+slb-halfspace@users.noreply.github.com>
2025-02-19 15:16:48 +0100

    Update source/core/src/core/bronze/infrastructure/config/table_names.py
    Co-authored-by: Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>

--- source/core/src/core/bronze/infrastructure/config/table_names.py
+++ source/core/src/core/bronze/infrastructure/config/table_names.py
@@ -3,5 +3,6 @@ class TableNames:
     bronze_migrated_transactions_table = "migrated_transactions"
 
 
+# Table is from the `Migrations` subsystem
 class MigrationsTableNames:
     silver_time_series_table = "time_series"
