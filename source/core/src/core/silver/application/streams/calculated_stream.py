6ce480259df305e96907d17594bd93166fadf9bb
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-20 12:27:56 +0100

    Update calculated_stream.py

--- source/core/src/core/silver/application/streams/calculated_stream.py
+++ source/core/src/core/silver/application/streams/calculated_stream.py
@@ -58,5 +58,5 @@ def _execute(spark: SparkSession) -> None:
 def _batch_operations(df: DataFrame, batchId: int) -> None:
     silver_settings = SilverSettings()  # type: ignore
     df = transform_calculated_measurements(df)
-    target_table_name = f"{catalog_settings.silver_database_name}.{SilverTableNames.silver_measurements}"
+    target_table_name = f"{silver_settings.silver_database_name}.{SilverTableNames.silver_measurements}"
     df.write.format("delta").mode("append").saveAsTable(target_table_name)
