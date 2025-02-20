2c8e105eec2fd86adb87f20fffff94de45c3a6d7
Bjarke Meier <hej@bjarkemeier.dk>
2025-02-20 21:11:49 +0100

    simplify debug

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/debug.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/debug.py
@@ -10,10 +10,7 @@ def debugging(df: DataFrame, message: str | None = None) -> None:
     if not DEBUG:
         return
 
-    if message is not None:
-        logging.debug(message)
-    else:
-        calling_function = inspect.stack()[1].function
-        logging.debug(calling_function)
+    message = message or inspect.stack()[1].function
+    logging.debug(message)
 
     df.show()
