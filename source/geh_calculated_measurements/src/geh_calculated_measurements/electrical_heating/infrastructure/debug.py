3cdb120731da70f4bf12510f5a7d20fe89faa716
Bjarke Meier <hej@bjarkemeier.dk>
2025-02-21 12:33:01 +0100

    enable simple nsg2 no-D15 tests

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/debug.py
@@ -0,0 +1,15 @@
+import inspect
+
+from pyspark.sql import DataFrame
+
+DEBUG = False
+
+
+def debugging(df: DataFrame, message: str | None = None) -> None:
+    if not DEBUG:
+        return
+
+    caller = inspect.stack()[1].function
+    print(f"######## In {caller}: {message}")
+
+    df.show()
