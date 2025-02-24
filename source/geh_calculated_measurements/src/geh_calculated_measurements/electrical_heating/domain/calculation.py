0d3b4b9277c7f9ccfe8f4fa019c24b1c042cab39
jesper knudsen <jmk@halfspace.io>
2025-02-24 15:12:43 +0000

    add logging for trouble shooting

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/domain/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/domain/calculation.py
@@ -15,6 +15,8 @@ from geh_calculated_measurements.electrical_heating.infrastructure import (
     TimeSeriesPoints,
 )
 
+logging.basicConfig(level=logging.INFO)
+
 
 @use_span()
 def execute(
@@ -29,15 +31,25 @@ def execute(
     """
     consumption_energy = T.get_daily_consumption_energy_in_local_time(time_series_points, time_zone)
 
+    logging.info(f"number of rows in consumption_energy: {consumption_energy.count()}")
+
     old_electrical_heating = T.get_electrical_heating_in_local_time(time_series_points, time_zone)
+
+    logging.info(f"number of rows in old_electrical_heating: {old_electrical_heating.count()}")
 
     metering_point_periods = T.get_joined_metering_point_periods_in_local_time(
         consumption_metering_point_periods, child_metering_points, time_zone
     )
 
+    logging.info(f"number of rows in metering_point_periods: {metering_point_periods.count()}")
+
     new_electrical_heating = T.calculate_electrical_heating_in_local_time(consumption_energy, metering_point_periods)
 
+    logging.info(f"number of rows in new_electrical_heating: {new_electrical_heating.count()}")
+
     changed_electrical_heating = T.filter_unchanged_electrical_heating(new_electrical_heating, old_electrical_heating)
+
+    logging.info(f"number of rows in changed_electrical_heating: {changed_electrical_heating.count()}")
 
     changed_electrical_heating_in_utc = convert_to_utc(changed_electrical_heating, time_zone)
 
