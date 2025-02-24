a85ac8e18bc55ebe898b48f2dfb5abf262f526e4
Svend (SLB) <slb@halfspace.io>
2025-02-24 14:19:11 +0100

    Ruff

--- source/core/src/core/bronze/infrastructure/migration_data/silver_time_series_repository.py
+++ source/core/src/core/bronze/infrastructure/migration_data/silver_time_series_repository.py
@@ -19,6 +19,7 @@ class MigrationsSilverTimeSeriesRepository:
         catalog_settings = CatalogSettings()  # type: ignore
         self.source_database = catalog_settings.migrations_silver_database_name
         self.source_table_name = MigrationsTableNames.silver_time_series_table
+        self.repo_creation_timestamp = datetime.now()
 
     def read_migrations_silver_time_series(self) -> DataFrame:
         return self.spark.read.table(f"{self.source_database}.{self.source_table_name}")
@@ -27,7 +28,7 @@ class MigrationsSilverTimeSeriesRepository:
         self, latest_created_already_migrated: datetime
     ) -> DataFrame:
         return self.read_migrations_silver_time_series().filter(
-            (col(MigrationsSilverTimeSeriesColumnNames.created) < lit(datetime.now().date()))
+            (col(MigrationsSilverTimeSeriesColumnNames.created) < self.repo_creation_timestamp)
             & (col(MigrationsSilverTimeSeriesColumnNames.created) > lit(latest_created_already_migrated))
         )
 
@@ -37,7 +38,7 @@ class MigrationsSilverTimeSeriesRepository:
         return self.read_migrations_silver_time_series().filter(
             (lit(start_partition_date) <= col(MigrationsSilverTimeSeriesColumnNames.partitioning_col))
             & (col(MigrationsSilverTimeSeriesColumnNames.partitioning_col) <= lit(end_partition_date))
-            & (col(MigrationsSilverTimeSeriesColumnNames.created) < lit(datetime.now().date()))
+            & (col(MigrationsSilverTimeSeriesColumnNames.created) < self.repo_creation_timestamp)
         )
 
     def create_chunks_of_partitions_for_data_with_a_single_partition_col(
