cb9846db45194977e2f02759db03c71b8a9f8c3f
HenrikSommer <sommerh@gmail.com>
2025-02-24 14:57:59 +0100

    Fix todo

---
+++ source/core/src/core/silver/domain/constants/orchestration_types.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class OrchestrationTypes(Enum):
+    SUBMITTED_MEASURE_DATA = "OT_SUBMITTED_MEASURE_DATA"
--- source/core/src/core/silver/infrastructure/streams/submitted_transactions_repository.py
+++ source/core/src/core/silver/infrastructure/streams/submitted_transactions_repository.py
@@ -2,6 +2,7 @@ from pyspark.sql import DataFrame, SparkSession
 
 from core.settings.catalog_settings import CatalogSettings
 from core.silver.domain.constants.col_names_silver_measurements import SilverMeasurementsColNames
+from core.silver.domain.constants.orchestration_types import OrchestrationTypes
 from core.silver.infrastructure.config import SilverTableNames
 
 
@@ -16,6 +17,6 @@ class SubmittedTransactionsRepository:
             .option("ignoreDeletes", "true")
             .table(f"{self.silver_database_name}.{SilverTableNames.silver_measurements}")
             .filter(
-                f"{SilverMeasurementsColNames.orchestration_type} = 'OT_SUBMITTED_MEASURE_DATA'"
-            )  # TODO: Add this to constants
+                f"{SilverMeasurementsColNames.orchestration_type} = '{OrchestrationTypes.SUBMITTED_MEASURE_DATA.value}'"
+            )
         )
