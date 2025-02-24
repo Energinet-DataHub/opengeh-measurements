234117408a7922185c0f2adaa157431acdd3ef34
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2025-02-24 14:39:26 +0100

    Add Measurements API (#293)
    Co-authored-by: Nicolaj Dedenroth Aalykke <nicolaj.it@icloud.com>

---
+++ source/Measurements/WebApi.IntegrationTests/DummyIntegrationTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Energinet.DataHub.Measurements.WebApi.IntegrationTests;
+
+public class DummyIntegrationTest
+{
+    [Fact]
+    public void ReplaceThisWithActualIntegrationTestInStepTwo()
+    {
+        Assert.True(true);
+    }
+}
---
+++ source/Measurements/WebApi/Extensions/DependencyInjection/AuthenticationExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Energinet.DataHub.Measurements.WebApi.Extensions.DependencyInjection;
+
+public static class AuthenticationExtensions
+{
+    public static IServiceCollection AddAuthenticationForWebApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        var tenantId = configuration["AzureAd:TenantId"];
+        var audience = configuration["AzureAd:ResourceId"];
+        var authority = $"https://login.microsoftonline.com/{tenantId}/v2.0";
+
+        services.AddAuthentication()
+            .AddJwtBearer(x =>
+            {
+                x.Audience = audience;
+                x.Authority = authority;
+            });
+
+        return services;
+    }
+}
