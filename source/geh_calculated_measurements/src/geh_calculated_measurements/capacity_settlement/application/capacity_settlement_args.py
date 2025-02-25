8d9a54203fdc995462ba8ea3f0a1843050de0b5e
Jonas Michelsen <jvm@energinet.dk>
2025-02-25 15:59:58 +0100

    monkeypatch cap

--- source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/application/capacity_settlement_args.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/application/capacity_settlement_args.py
@@ -1,15 +1,12 @@
 from uuid import UUID
 
 from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
-class CapacitySettlementArgs(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    _env_file: str | None = None
-    
+class CapacitySettlementArgs(BaseSettings, cli_parse_args=True):
     orchestration_instance_id: UUID = Field(init=False)
     time_zone: str = "Europe/Copenhagen"
     calculation_month: int = Field(init=False)
     calculation_year: int = Field(init=False)
-    catalog_name: str = Field()
+    catalog_name: str = Field(init=False)
