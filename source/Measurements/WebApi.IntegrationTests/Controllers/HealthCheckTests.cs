47f0457d3521798635abf2a583022c9f62559c7d
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-02-25 13:51:17 +0100

    Add status hc

--- source/Measurements/WebApi.IntegrationTests/Controllers/HealthCheckTests.cs
+++ source/Measurements/WebApi.IntegrationTests/Controllers/HealthCheckTests.cs
@@ -9,6 +9,7 @@ namespace Energinet.DataHub.Measurements.WebApi.IntegrationTests.Controllers;
 public class HealthCheckTests(WebApiFixture fixture) : IClassFixture<WebApiFixture>
 {
     [Theory]
+    [InlineData("/monitor/status")]
     [InlineData("/monitor/live")]
     [InlineData("/monitor/ready")]
     public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
