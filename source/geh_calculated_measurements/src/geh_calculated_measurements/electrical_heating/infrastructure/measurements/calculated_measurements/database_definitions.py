be9871b8b9e5e9253db35fcf7b10016df9f11eb0
Christian Hansen <cenh@halfspace.io>
2025-02-25 14:17:17 +0100

    dropped migrations + create table

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/measurements/calculated_measurements/database_definitions.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/measurements/calculated_measurements/database_definitions.py
@@ -1,3 +1,3 @@
 class CalculatedMeasurementsDatabaseDefinition:
-    DATABASE_NAME = "measurements_calculated"
+    DATABASE_NAME = "measurements_calculated_internal"
     MEASUREMENTS_NAME = "measurements"
