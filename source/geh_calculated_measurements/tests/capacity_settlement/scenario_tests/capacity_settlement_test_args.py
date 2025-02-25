cf4eea3e82fb292dce5c4ceab260610c0e53e3a8
Jonas Michelsen <jvm@energinet.dk>
2025-02-25 10:03:48 +0100

    Update capacity_settlement_test_args.py

--- source/geh_calculated_measurements/tests/capacity_settlement/scenario_tests/capacity_settlement_test_args.py
+++ source/geh_calculated_measurements/tests/capacity_settlement/scenario_tests/capacity_settlement_test_args.py
@@ -3,8 +3,8 @@ from geh_calculated_measurements.capacity_settlement.application import (
 )
 
 
-class CapacitySettlementTestArgs(CapacitySettlementArgs):
+class CapacitySettlementTestArgs:
     """Args for testing the electrical heating job."""
 
     def __init__(self, env_file_path: str) -> None:
-        super().__init__(_env_file=env_file_path)
+        self.args = CapacitySettlementArgs(_env_file=env_file_path)
