04c4211ee7c1722ea9de6865d156ea9afeea6a88
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-26 11:27:35 +0100

    move column_names to common

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/domain/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/domain/__init__.py
@@ -1,7 +1,5 @@
 import geh_calculated_measurements.common.domain.model.calculated_measurements_factory as calculated_measurements_factory
+from geh_calculated_measurements.common.domain.column_names import ColumnNames
 from geh_calculated_measurements.common.domain.model.calculated_measurements import CalculatedMeasurements
 
-__all__ = [
-    "CalculatedMeasurements",
-    "calculated_measurements_factory",
-]
+__all__ = ["CalculatedMeasurements", "calculated_measurements_factory", "ColumnNames"]
