f29d067715b9f5c2d84b3f34f135f6693337d044
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-26 11:16:20 +0100

    Update calculation.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/domain/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/domain/calculation.py
@@ -7,7 +7,7 @@ from geh_common.pyspark.transformations import (
 from geh_common.telemetry import use_span
 
 import geh_calculated_measurements.electrical_heating.domain.transformations as T
-from geh_calculated_measurements.common.domain.calculated_measurements_factory import create
+from geh_calculated_measurements.common.domain import calculated_measurements_factory
 from geh_calculated_measurements.common.domain.model.calculated_measurements import (
     CalculatedMeasurements,
 )
@@ -44,7 +44,7 @@ def execute(
 
     changed_electrical_heating_in_utc = convert_to_utc(changed_electrical_heating, time_zone)
 
-    calculated_measurements = create(
+    calculated_measurements = calculated_measurements_factory.create(
         measurements=changed_electrical_heating_in_utc,
         orchestration_instance_id=orchestration_instance_id,
         orchestration_type=OrchestrationType.ELECTRICAL_HEATING,
