c5f52b2f022949991d570710c4487361b7c3a853
Alexander <alexanderharrow@hotmail.com>
2025-02-26 10:14:47 +0100

    Updates

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/domain/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/domain/__init__.py
@@ -1,5 +1,9 @@
+from geh_calculated_measurements.electrical_heating.domain.model.time_series_points.wrapper import (
+    TimeSeriesPoints,
+    time_series_points_v1,
+)
+
 from .calculation import execute
 from .column_names import ColumnNames
-from .model.time_series_points.wrapper import TimeSeriesPoints, time_series_points_v1
 
 __all__ = ["ColumnNames", "execute", "TimeSeriesPoints", "time_series_points_v1"]
