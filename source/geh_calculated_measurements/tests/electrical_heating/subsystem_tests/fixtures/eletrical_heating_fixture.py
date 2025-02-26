3abeec07921f2e9ebf7fc5e36357723fcd93c9eb
jesper knudsen <jmk@halfspace.io>
2025-02-26 15:41:22 +0000

    seeding

--- source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/fixtures/eletrical_heating_fixture.py
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/fixtures/eletrical_heating_fixture.py
@@ -9,6 +9,10 @@ from databricks.sdk.service.jobs import RunResultState
 from environment_configuration import EnvironmentConfiguration
 from geh_common.testing.container_test.databricks_api_client import DatabricksApiClient
 
+from geh_calculated_measurements.electrical_heating.infrastructure.measurements.measurements_gold.database_definitions import (
+    MeasurementsGoldDatabaseDefinition,
+)
+
 
 class CalculationInput:
     orchestration_instance_id: uuid.UUID
@@ -21,11 +25,32 @@ class JobState:
     calculation_input: CalculationInput = CalculationInput()
 
 
+query = """INSERT INTO measurements (
+  transaction_id, quantity, transaction_creation_datetime, created, modified, -- dynamic variables
+  metering_point_id, observation_time, quality, metering_point_type -- static variables
+)
+SELECT
+    REPLACE(CAST(uuid() AS VARCHAR(50)), '-', '') AS transaction_id, -- transaction_id
+    CAST(RAND() * 1000000 AS DECIMAL(18, 3)) AS quantity, -- quantity
+    GETDATE() AS transaction_creation_datetime, -- transaction_creation_datetime
+    GETDATE() AS created, -- created
+    GETDATE() AS modified, -- modified
+    '170000030000000201' AS metering_point_id, -- metering_point_id
+    '2024-11-30T23:00:00Z' AS observation_time, -- observation_time
+    'measured' AS quality, -- quality
+    'consumption' AS metering_point_type -- metering_point_type"""
+
+
 class ElectricalHeatingFixture:
     def __init__(self, environment_configuration: EnvironmentConfiguration):
         self.databricks_api_client = DatabricksApiClient(
             environment_configuration.databricks_token,
             environment_configuration.workspace_url,
+        ).seed(
+            warehouse_id=environment_configuration.warehouse_id,
+            catalog=environment_configuration.catalog_name,
+            schema=MeasurementsGoldDatabaseDefinition.DATABASE_NAME,
+            statement=query,
         )
         self.job_state = JobState()
         self.credentials = DefaultAzureCredential()
