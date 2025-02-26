796f67f2ca2f3ab1508a3847ab087a8b59726883
Jonas Michelsen <jvm@energinet.dk>
2025-02-26 08:28:43 +0100

    fix: Use absolute path instead of relative

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/__init__.py
@@ -1,9 +1,12 @@
+from geh_calculated_measurements.electrical_heating.infrastructure.measurements.measurements_gold.wrapper import (
+    TimeSeriesPoints,
+)
+
 from .electricity_market.child_metering_points.wrapper import ChildMeteringPoints
 from .electricity_market.consumption_metering_point_periods.wrapper import ConsumptionMeteringPointPeriods
 from .electricity_market.repository import Repository as ElectricityMarketRepository
 from .measurements.calculated_measurements.database_definitions import CalculatedMeasurementsDatabaseDefinition
 from .measurements.measurements_gold.database_definitions import MeasurementsGoldDatabaseDefinition
-from .measurements.measurements_gold.wrapper import TimeSeriesPoints
 from .measurements.repository import Repository as MeasurementsRepository
 from .spark_initializor import initialize_spark
 
