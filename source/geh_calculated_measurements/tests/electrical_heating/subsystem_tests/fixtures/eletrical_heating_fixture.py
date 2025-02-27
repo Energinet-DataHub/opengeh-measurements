d44212753e59514bf80900f70e363b10a8ba84cd
jesper knudsen <jmk@halfspace.io>
2025-02-27 14:03:59 +0000

    change back from view to table

--- source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/fixtures/eletrical_heating_fixture.py
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/fixtures/eletrical_heating_fixture.py
@@ -25,7 +25,7 @@ class JobState:
     calculation_input: CalculationInput = CalculationInput()
 
 
-query = f"""INSERT INTO {MeasurementsGoldDatabaseDefinition.TIME_SERIES_POINTS_NAME} (
+query = """INSERT INTO measurements (
   transaction_id, quantity, transaction_creation_datetime, created, modified, -- dynamic variables
   metering_point_id, observation_time, quality, metering_point_type -- static variables
 )
