50ca750998985a0562e014e9513b4767e3ef8251
jesper knudsen <jmk@halfspace.io>
2025-02-27 09:31:54 +0000

    move fixtures and test data to subfolders

---
+++ source/geh_calculated_measurements/tests/electrical_heating/electrical_heating_tests/infrastructure/measurements/test_measurements_repository.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.skip(reason="MeasurementsRepository.read_time_series_points: not properly implemented")
+def test__when_missing_expected_column_raises_exception(self) -> None:
+    pass
+
+
+@pytest.mark.skip(reason="MeasurementsRepository.read_time_series_points: not properly implemented")
+def test__when_source_contains_unexpected_columns_returns_data_without_unexpected_column(self) -> None:
+    pass
+
+
+@pytest.mark.skip(reason="MeasurementsRepository.read_time_series_points: not properly implemented")
+def test__when_source_contains_wrong_data_type_raises_exception(self) -> None:
+    pass
