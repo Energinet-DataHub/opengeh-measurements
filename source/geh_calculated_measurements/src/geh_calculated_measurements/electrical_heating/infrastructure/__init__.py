c6c36cc7bc678f93c49cac80d786d2585c19c44b
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-27 12:30:22 +0100

    feat(EH): Read time series points  (#320)

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/__init__.py
@@ -1,6 +1,6 @@
 from .electricity_market.repository import Repository as ElectricityMarketRepository
-from .measurements.measurements_gold.database_definitions import MeasurementsGoldDatabaseDefinition
-from .measurements.repository import Repository as MeasurementsGoldRepository
+from .measurements_gold.database_definitions import MeasurementsGoldDatabaseDefinition
+from .measurements_gold.repository import Repository as MeasurementsGoldRepository
 
 __all__ = [
     # Electricity market repository, types, and database definitions
