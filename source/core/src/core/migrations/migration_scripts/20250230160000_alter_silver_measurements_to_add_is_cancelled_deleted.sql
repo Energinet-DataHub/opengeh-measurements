b7d1c904e5976a02165986cf60d11f3ff9e3d6fa
Svend (SLB) <slb@halfspace.io>
2025-02-27 15:39:49 +0000

    Extend migration script with update of existing, and checks

--- source/core/src/core/migrations/migration_scripts/20250230160000_alter_silver_measurements_to_add_is_cancelled_deleted.sql
+++ source/core/src/core/migrations/migration_scripts/20250230160000_alter_silver_measurements_to_add_is_cancelled_deleted.sql
@@ -1,2 +1,14 @@
 ALTER TABLE {silver_database}.{silver_measurements_table}
 ADD COLUMNS (is_cancelled boolean after points, is_deleted boolean after is_cancelled)
+GO 
+
+UPDATE TABLE {silver_database}.{silver_measurements_table}
+SET is_cancelled = False, is_deleted = False
+GO 
+
+ALTER TALBE {silver_database}.{silver_measurements_table}
+ADD CONSTRAINT is_cancelled_is_not_null_chk CHECK is_cancelled is not null
+GO
+
+ALTER TALBE {silver_database}.{silver_measurements_table}
+ADD CONSTRAINT is_deleted_is_not_null_chk CHECK is_deleted is not null
