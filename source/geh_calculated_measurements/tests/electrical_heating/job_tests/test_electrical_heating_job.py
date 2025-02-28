456f64c3fa889e3774de9f7f96416db630109455
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-28 10:45:41 +0100

    Update test_electrical_heating_job.py

--- source/geh_calculated_measurements/tests/electrical_heating/job_tests/test_electrical_heating_job.py
+++ source/geh_calculated_measurements/tests/electrical_heating/job_tests/test_electrical_heating_job.py
@@ -8,10 +8,10 @@ from pyspark.sql import functions as F
 from geh_calculated_measurements.common.infrastructure import (
     CalculatedMeasurementsInternalDatabaseDefinition,
 )
-from geh_calculated_measurements.electrical_heating.application import execute_application
+from geh_calculated_measurements.electrical_heating.entry_point import execute
 
 
-def test_execute_with_deps(spark: SparkSession, job_environment_variables: dict, seed_gold_table: Any) -> None:
+def test_execute(spark: SparkSession, job_environment_variables: dict, seed_gold_table: Any) -> None:
     # Arrange
     orchestration_instance_id = str(uuid.uuid4())
     sys_argv = ["dummy_script_name", "--orchestration-instance-id", orchestration_instance_id]
@@ -19,7 +19,7 @@ def test_execute_with_deps(spark: SparkSession, job_environment_variables: dict,
     # Act
     with patch("sys.argv", sys_argv):
         with patch.dict("os.environ", job_environment_variables):
-            execute_application()
+            execute()
 
     # Assert
     actual = spark.read.table(
