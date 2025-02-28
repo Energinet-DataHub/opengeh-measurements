886cdf2ed89f48289cb419c285b7a59f88a4eab1
Jonas Michelsen <39729843+johevemi@users.noreply.github.com>
2025-02-28 11:12:07 +0100

    refac: Create job test for capacity settlement (#299)
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>
    Co-authored-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/infrastructure/measurements/measurements_gold/database_definitions.py
@@ -0,0 +1,4 @@
+class MeasurementsGoldDatabaseDefinition:
+    DATABASE_NAME = "measurements_gold"
+    TIME_SERIES_POINTS_NAME = "time_series_points_v1"
+    METERING_POINT_PERIODS_NAME = "metering_point_periods_v1"
