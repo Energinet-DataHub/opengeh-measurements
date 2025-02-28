25d5e400d5ed7d506660166b492c1b525147e557
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-28 12:45:35 +0100

    Update databricks_settings.py

--- source/core/src/core/settings/databricks_settings.py
+++ source/core/src/core/settings/databricks_settings.py
@@ -8,12 +8,12 @@ class DatabricksSettings(BaseSettings):
     Attributes:
     databricks_workspace_url (str): The URL of the Databricks workspace.
     databricks_token (str): The token used to authenticate with the Databricks
-    databricks_jobs (list[str]): The list of jobs in Databricks
+    databricks_jobs (str): The list of jobs in Databricks
     """
 
     databricks_workspace_url: str = Field(init=False)
     databricks_token: str = Field(init=False)
-    databricks_jobs: list[str] = Field(init=False)
+    databricks_jobs: str = Field(init=False)
 
     class Config:
         case_sensitive = False
