e6f1f5e6c64590c3f79969c506c45369fb0faf33
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-28 10:36:39 +0100

    try env variable list

--- source/core/src/core/migrations/migrations_runner.py
+++ source/core/src/core/migrations/migrations_runner.py
@@ -7,18 +7,26 @@ import core.migrations.substitutions as substitutions
 from core.migrations.database_names import DatabaseNames
 from core.migrations.table_names import TableNames
 from core.settings.catalog_settings import CatalogSettings
+from core.settings.databricks_settings import DatabricksSettings
 
 
 def migrate() -> None:
+    spark_config = _configure_spark_sql_migrations()
+
+    databricks_settings = DatabricksSettings()
+    print(databricks_settings.databricks_jobs)
+
+    migration_pipeline.migrate(spark_config)
+
+
+def _configure_spark_sql_migrations() -> SparkSqlMigrationsConfiguration:
     substitution_variables = substitutions.substitutions()
     catalog_name = CatalogSettings().catalog_name
 
-    spark_config = SparkSqlMigrationsConfiguration(
+    return SparkSqlMigrationsConfiguration(
         migration_schema_name=DatabaseNames.measurements_internal_database,
         migration_table_name=TableNames.executed_migrations,
         migration_scripts_folder_path="core.migrations.migration_scripts",
         substitution_variables=substitution_variables,
         catalog_name=catalog_name,
     )
-
-    migration_pipeline.migrate(spark_config)
