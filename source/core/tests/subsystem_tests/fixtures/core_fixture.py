314d00e7035775ef7f3a81994086e2e1b196e23d
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-02-28 08:15:32 +0100

    added some asserts

--- source/core/tests/subsystem_tests/fixtures/core_fixture.py
+++ source/core/tests/subsystem_tests/fixtures/core_fixture.py
@@ -1,9 +1,8 @@
-from azure.eventhub import EventData, EventHubProducerClient
+from azure.eventhub import EventData, EventHubConsumerClient, EventHubProducerClient, PartitionContext
 from azure.identity import DefaultAzureCredential
-from databricks.sdk.service.jobs import RunResultState
+from databricks.sdk.service.jobs import RunLifeCycleState, RunResultState
 from geh_common.testing.container_test.databricks_api_client import DatabricksApiClient
 
-from tests.helpers.builders.submitted_transactions_builder import ValueBuilder
 from tests.subsystem_tests.settings.databricks_settings import DatabricksSettings
 from tests.subsystem_tests.settings.kafka_settings import KafkaSettings
 
@@ -39,7 +38,7 @@ class CoreFixture:
 
         self.run_states.append(RunState(run_id))
 
-    def send_submitted_transactions_event(self, spark) -> None:
+    def send_submitted_transactions_event(self, value) -> None:
         kafka_settings = KafkaSettings()  # type: ignore
 
         credential = DefaultAzureCredential()
@@ -52,10 +51,43 @@ class CoreFixture:
 
         event_data_batch = producer.create_batch()
 
-        value = ValueBuilder(spark).build()
-
         event_data_batch.add(EventData(value))
 
         producer.send_batch(event_data_batch)
 
         producer.close()
+
+    def assert_runs_are_running(self) -> None:
+        for run_state in self.run_states:
+            run = self.databricks_api_client.client.jobs.get_run(run_state.run_id)
+            life_cycle_state = run.state.life_cycle_state.value  # type: ignore
+
+            if life_cycle_state not in [RunLifeCycleState.RUNNING.value, RunLifeCycleState.PENDING.value]:
+                raise AssertionError(f"Run {run_state.run_id} is not running. Current state: {life_cycle_state}")
+
+    def assert_receipt(self) -> None:
+        kafka_settings = KafkaSettings()  # type: ignore
+        credential = DefaultAzureCredential()
+
+        consumer = EventHubConsumerClient(
+            fully_qualified_namespace=f"{kafka_settings.event_hub_namespace}.servicebus.windows.net",
+            eventhub_name=kafka_settings.event_hub_receipt_instance,
+            consumer_group="$Default",
+            credential=credential,
+        )
+
+        def on_event(partition_context: PartitionContext, event: EventData | None):
+            if event is None:
+                print("No event received.")
+                return
+
+            # Print the event data.
+            print(
+                'Received the event: "{}" from the partition with ID: "{}"'.format(
+                    event.body_as_str(encoding="UTF-8"), partition_context.partition_id
+                )
+            )
+
+            assert event.body_as_str(encoding="UTF-8") is not None
+
+        consumer.receive_batch(on_event=on_event, prefetch=10)
--- source/core/tests/subsystem_tests/test_core.py
+++ source/core/tests/subsystem_tests/test_core.py
@@ -1,6 +1,10 @@
+import time
+
 import pytest
 from pyspark.sql import SparkSession
 
+import tests.helpers.identifier_helper as identifier_helper
+from tests.helpers.builders.submitted_transactions_builder import ValueBuilder
 from tests.subsystem_tests.fixtures.core_fixture import CoreFixture
 from tests.subsystem_tests.settings.databricks_settings import DatabricksSettings
 
@@ -9,17 +13,29 @@ def test__dummy_test(spark: SparkSession) -> None:
     # Arrange
     databricks_settings = DatabricksSettings()  # type: ignore
     fixture = CoreFixture(databricks_settings)
+    value = ValueBuilder(spark).add_row().build()
 
-    # fixture.start_jobs()
+    fixture.start_jobs()
+    fixture.send_submitted_transactions_event(value)
 
-    fixture.send_submitted_transactions_event(spark)
+    start_time = time.time()
+    timeout = 1000
+    poll_interval = 10
+
+    while time.time() - start_time < (timeout + 20):
+        fixture.assert_runs_are_running()
+        fixture.assert_receipt()
+
+        if start_time - time.time() > timeout:
+            raise TimeoutError("Timeout error")
+
+        time.sleep(poll_interval)
 
     # Send event to the eventhub
     # Wait for a receipt (max 10 minutes) and assert that jobs are running
     # Assert receipt
 
     # Assert
-    assert True is True
 
 
 @pytest.mark.order(1)
@@ -30,5 +46,19 @@ def test__start_jobs() -> None:
 
     # Act
     fixture.start_jobs()
+
+    # Assert
+
+
+@pytest.mark.order(2)
+def test__send_event_to_eventhub(spark: SparkSession) -> None:
+    # Arrange
+    databricks_settings = DatabricksSettings()  # type: ignore
+    fixture = CoreFixture(databricks_settings)
+    value = ValueBuilder(spark).add_row().build()
+    partition_id = identifier_helper.generate_random_string()
+
+    # Act
+    fixture.send_submitted_transactions_event(value, partition_id)
 
     # Assert
