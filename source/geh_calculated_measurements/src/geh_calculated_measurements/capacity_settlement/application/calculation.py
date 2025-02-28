24706d430f5eedd714fc95c16b74d97af8364698
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-02-28 15:24:17 +0100

    refac(CS): Streamline code structure with electrical heating (#340)

--- source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/application/calculation.py
@@ -5,17 +5,21 @@ from pyspark.sql import SparkSession
 
 from geh_calculated_measurements.capacity_settlement.application.capacity_settlement_args import CapacitySettlementArgs
 from geh_calculated_measurements.capacity_settlement.domain.calculation import execute
-from geh_calculated_measurements.capacity_settlement.infrastructure import MeasurementsRepository
+from geh_calculated_measurements.capacity_settlement.infrastructure import (
+    ElectricityMarketRepository,
+    MeasurementsGoldRepository,
+)
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: CapacitySettlementArgs) -> None:
     # Create repositories to obtain data frames
-    measurements_repository = MeasurementsRepository(spark, args.catalog_name)
+    measurements_gold_repository = MeasurementsGoldRepository(spark, args.catalog_name)
+    electricity_market_repository = ElectricityMarketRepository(spark, args.electricity_market_data_path)
 
     # Read data frames
-    time_series_points = measurements_repository.read_time_series_points()
-    metering_point_periods = measurements_repository.read_metering_point_periods()
+    time_series_points = measurements_gold_repository.read_time_series_points()
+    metering_point_periods = electricity_market_repository.read_metering_point_periods()
 
     orchestration_instance_id = uuid.UUID()
 
---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/infrastructure/electricity_market/schema.py
@@ -0,0 +1,15 @@
+import pyspark.sql.types as t
+
+nullable = True
+
+
+metering_point_periods_v1 = t.StructType(
+    [
+        t.StructField("metering_point_id", t.StringType(), not nullable),
+        t.StructField("period_from_date", t.TimestampType(), not nullable),
+        t.StructField("period_to_date", t.TimestampType(), not nullable),
+        t.StructField("child_metering_point_id", t.StringType(), not nullable),
+        t.StructField("child_period_from_date", t.StringType(), not nullable),
+        t.StructField("child_period_to_date", t.StringType(), not nullable),
+    ]
+)
