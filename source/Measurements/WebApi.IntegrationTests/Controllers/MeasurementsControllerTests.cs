44493e21f1988d2ce0dce4e2d9d9c44267fb8de8
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2025-03-03 12:07:49 +0100

    [MMCore] Add controller integration test of measurements API (#345)

---
+++ source/Measurements/WebApi.IntegrationTests/Controllers/MeasurementsControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Energinet.DataHub.Measurements.WebApi.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace Energinet.DataHub.Measurements.WebApi.IntegrationTests.Controllers;
+
+public class MeasurementsControllerTests(WebApiFixture fixture)
+    : IClassFixture<WebApiFixture>
+{
+    [Theory]
+    [InlineData("meteringPointId", "42")]
+    public async Task GetAsync_ReturnsFortyTwo(
+        string meteringPointId,
+        string expectedResponseBody)
+    {
+        // Arrange
+        var client = fixture.CreateClient();
+        var url = $"measurements?measurementId={meteringPointId}";
+
+        // Act
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        var actualResponseBody = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(expectedResponseBody, actualResponseBody);
+    }
+}
