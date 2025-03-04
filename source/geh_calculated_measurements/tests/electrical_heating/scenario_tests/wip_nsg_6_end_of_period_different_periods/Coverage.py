574bcccf82b13600f97cb5ff4b05c76df0ef74d4
ClausQAEnerginet <xclpe@energinet.dk>
2025-03-04 14:42:15 +0100

    update

---
+++ source/geh_calculated_measurements/tests/electrical_heating/scenario_tests/wip_nsg_6_end_of_period_different_periods/Coverage.py
@@ -0,0 +1,27 @@
+"""
+## PURPOSE
+The purpose is to test scenarios where limit is not 4000 with NSG6
+Also to test settlement moth != January
+
+## DESIGN CONSIDERATIONS
+CASE 2
+                                               |Settlement month 3       |Period change July 1st                  Today|
+                   Nov 2023_________|2024_______|_________________________|______________________________|2025__________
+Parent start/end:       |-----------------------|-------------------------|------------------------------------------...
+Periods:                |_______________________|_________________________|_________________________________________
+Days in period:         | 61 days               | 91 days  | 275 days                     | N/A
+Measurement data:        ++         +++        +++                            ++
+
+NSG2 type:
+- D15/End:              |___________|__________|______________________________|
+- D15/Up to End:                                                              |___________
+
+CASE 3 (summer/winter time)
+                                   |March 30th (1 day before summertime)        | October 28th (1 day after wintertime)
+                        |2024______|____________________________________________|_______|2025
+Parent start/end:                  |--------------------------------------------|
+Days in period:                    | 213 days                                   |
+Measurement data:                  +++                                        +++
+
+## CASES TESTED
+"""
