ff8ef3a04c740e5a6fb15db7dd44fa85afad914b
HenrikSommer <sommerh@gmail.com>
2025-03-04 12:32:18 +0100

    Add dummy validations

--- source/core/src/core/silver/domain/validations/enum_validations.py
+++ source/core/src/core/silver/domain/validations/enum_validations.py
@@ -11,3 +11,18 @@ def validate_metering_point_type_enum() -> Column:
     """Quality check: QCST01-01."""
     valid_metering_point_types = [name for name in MeteringPointType._member_names_ if name != "MPT_UNSPECIFIED"]
     return F.col(SubmittedTransactionsUnpackedColumnNames.metering_point_type).isin(valid_metering_point_types)
+
+
+def validate_resolution_enum() -> Column:
+    """Quality check: QCST01-02."""
+    return F.lit(True)
+
+
+def validate_unit_enum() -> Column:
+    """Quality check: QCST01-03."""
+    return F.lit(True)
+
+
+def validate_quality_enum() -> Column:
+    """Quality check: QCST01-04."""
+    return F.lit(True)
