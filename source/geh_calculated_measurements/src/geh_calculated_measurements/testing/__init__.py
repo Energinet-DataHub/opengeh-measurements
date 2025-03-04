dbc7619217a0bf51d1ed109d37af4caced552b85
Alexander <alexanderharrow@hotmail.com>
2025-03-04 09:06:24 +0100

    Update __init__.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/testing/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/testing/__init__.py
@@ -1,3 +1,3 @@
-from .utilities.log_query_client_wrapper import LogQueryClientWrapper
+from geh_calculated_measurements.testing.utilities.log_query_client_wrapper import LogQueryClientWrapper
 
 __all__ = ["LogQueryClientWrapper"]
