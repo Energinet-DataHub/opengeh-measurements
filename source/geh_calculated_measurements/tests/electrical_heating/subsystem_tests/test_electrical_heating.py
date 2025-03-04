60801a581b83f9934c2c842375d603459902e0b8
Jens Peder Meldgaard <jpm@halfspace.io>
2025-03-04 08:33:03 +0000

    test: orchestration_instance_id use underscore not dashes

--- source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/test_electrical_heating.py
+++ source/geh_calculated_measurements/tests/electrical_heating/subsystem_tests/test_electrical_heating.py
@@ -53,7 +53,7 @@ class TestElectricalHeating(unittest.TestCase):
         query = f"""
         AppTraces
         | where Properties["Subsystem"] == 'measurements'
-        | where Properties["orchestration-instance-id"] == '{self.fixture.job_state.calculation_input.orchestration_instance_id}'
+        | where Properties["orchestration_instance_id"] == '{self.fixture.job_state.calculation_input.orchestration_instance_id}'
         """
 
         # Act
