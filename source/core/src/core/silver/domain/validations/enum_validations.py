13806f1024f3dfc84ee3d13d7ac55ae4522e61c5
HenrikSommer <sommerh@gmail.com>
2025-03-04 16:53:32 +0100

    Test transformation

--- source/core/src/core/silver/domain/validations/enum_validations.py
+++ source/core/src/core/silver/domain/validations/enum_validations.py
@@ -7,27 +7,27 @@ from core.bronze.domain.constants.column_names.submitted_transactions_unpacked_c
 from core.contracts.process_manager.enums.metering_point_type import MeteringPointType
 
 
-def validate_metering_point_type_enum() -> Column:
+def validate_orchestration_type_enum() -> Column:
     """Quality check: QCST01-01."""
-    valid_metering_point_types = [name for name in MeteringPointType._member_names_ if name != "MPT_UNSPECIFIED"]
-    return F.col(SubmittedTransactionsUnpackedColumnNames.metering_point_type).isin(valid_metering_point_types)
-
-
-def validate_resolution_enum() -> Column:
-    """Quality check: QCST01-02."""
-    return F.lit(True)
-
-
-def validate_unit_enum() -> Column:
-    """Quality check: QCST01-03."""
     return F.lit(True)
 
 
 def validate_quality_enum() -> Column:
+    """Quality check: QCST01-02."""
+    return F.lit(True)
+
+
+def validate_metering_point_type_enum() -> Column:
+    """Quality check: QCST01-03."""
+    valid_metering_point_types = [name for name in MeteringPointType._member_names_ if name != "MPT_UNSPECIFIED"]
+    return F.col(SubmittedTransactionsUnpackedColumnNames.metering_point_type).isin(valid_metering_point_types)
+
+
+def validate_unit_enum() -> Column:
     """Quality check: QCST01-04."""
     return F.lit(True)
 
 
-def validate_orchestration_type_enum() -> Column:
+def validate_resolution_enum() -> Column:
     """Quality check: QCST01-05."""
     return F.lit(True)
--- source/core/src/core/silver/domain/validations/submitted_transactions_to_silver_validation.py
+++ source/core/src/core/silver/domain/validations/submitted_transactions_to_silver_validation.py
@@ -9,11 +9,11 @@ import core.utility.rules_validator as rules_validator
 
 def all_validations_list() -> list[Callable[[], Column]]:
     return [
-        enum_validations.validate_metering_point_type_enum,
-        enum_validations.validate_resolution_enum,
-        enum_validations.validate_unit_enum,
-        enum_validations.validate_quality_enum,
         enum_validations.validate_orchestration_type_enum,
+        enum_validations.validate_quality_enum,
+        enum_validations.validate_metering_point_type_enum,
+        enum_validations.validate_unit_enum,
+        enum_validations.validate_resolution_enum,
     ]
 
 
