bd13bd102f00ca0e3498e841898bf5e2404cb817
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-05 15:52:56 +0100

    refac: Introducing EphemiralColumnNames in calculated measurements (#362)

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/domain/ephemeral_column_names.py
@@ -0,0 +1,5 @@
+class EphemeralColumnNames:
+    """Names of calculated columns used internally."""
+
+    selection_period_start = "selection_period_start"
+    selection_period_end = "selection_period_end"
