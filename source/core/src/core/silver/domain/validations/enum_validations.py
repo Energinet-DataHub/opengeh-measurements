b7ebfaf8b0dc1c734e452749766587993d9c0d5b
HenrikSommer <sommerh@gmail.com>
2025-03-06 13:05:02 +0100

    Add test of all validations

--- source/core/src/core/silver/domain/validations/enum_validations.py
+++ source/core/src/core/silver/domain/validations/enum_validations.py
@@ -7,23 +7,26 @@ from core.bronze.domain.constants.column_names.submitted_transactions_unpacked_c
 from core.contracts.process_manager.enums.metering_point_type import MeteringPointType
 from core.contracts.process_manager.enums.orchestration_type import OrchestrationType
 from core.contracts.process_manager.enums.quality import Quality
+from core.contracts.process_manager.enums.resolution import Resolution
 from core.contracts.process_manager.enums.unit import Unit
-from core.utility.columns_helpers import is_not_null
+from core.utility.columns_helper import is_not_null
 
 
 def validate_orchestration_type_enum() -> Column:
     """Quality check: QCST01-01."""
-    valid_orchestration_types = [name for name in OrchestrationType._member_names_ if name != "OT_UNSPECIFIED"]
+    valid_orchestration_types = [
+        name for name in OrchestrationType._member_names_ if name != OrchestrationType.OT_UNSPECIFIED.value
+    ]
     return F.col(SubmittedTransactionsUnpackedColumnNames.orchestration_type).isin(valid_orchestration_types)
 
 
 def validate_quality_enum() -> Column:
     """Quality check: QCST01-02."""
-    valid_qualities = [name for name in Quality._member_names_ if name != "QT_UNSPECIFIED"]
+    valid_qualities = [name for name in Quality._member_names_ if name != Quality.Q_UNSPECIFIED.value]
 
     return is_not_null(SubmittedTransactionsUnpackedColumnNames.points).otherwise(
         F.forall(
-            F.col(SubmittedTransactionsUnpackedColumnNames.points),
+            F.col(SubmittedTransactionsUnpackedColumnNames.Points.quality),
             lambda x: x.isNotNull() & x.isin(valid_qualities),
         )
     )
@@ -31,17 +34,19 @@ def validate_quality_enum() -> Column:
 
 def validate_metering_point_type_enum() -> Column:
     """Quality check: QCST01-03."""
-    valid_metering_point_types = [name for name in MeteringPointType._member_names_ if name != "MPT_UNSPECIFIED"]
+    valid_metering_point_types = [
+        name for name in MeteringPointType._member_names_ if name != MeteringPointType.MPT_UNSPECIFIED.value
+    ]
     return F.col(SubmittedTransactionsUnpackedColumnNames.metering_point_type).isin(valid_metering_point_types)
 
 
 def validate_unit_enum() -> Column:
     """Quality check: QCST01-04."""
-    valid_units = [name for name in Unit._member_names_ if name != "U_UNSPECIFIED"]
+    valid_units = [name for name in Unit._member_names_ if name != Unit.U_UNSPECIFIED.value]
     return F.col(SubmittedTransactionsUnpackedColumnNames.unit).isin(valid_units)
 
 
 def validate_resolution_enum() -> Column:
     """Quality check: QCST01-05."""
-    valid_resolutions = [name for name in Unit._member_names_ if name != "R_UNSPECIFIED"]
+    valid_resolutions = [name for name in Resolution._member_names_ if name != Resolution.R_UNSPECIFIED.value]
     return F.col(SubmittedTransactionsUnpackedColumnNames.resolution).isin(valid_resolutions)
--- source/core/tests/silver/domain/validations/test_enum_validations.py
+++ source/core/tests/silver/domain/validations/test_enum_validations.py
@@ -3,17 +3,75 @@ from pyspark.sql import SparkSession
 
 import core.silver.domain.validations.enum_validations as enum_validations
 from core.contracts.process_manager.enums.metering_point_type import MeteringPointType
-from tests.helpers.builders.submitted_transactions_builder import UnpackedSubmittedTransactionsBuilder
+from core.contracts.process_manager.enums.orchestration_type import OrchestrationType
+from core.contracts.process_manager.enums.quality import Quality
+from core.contracts.process_manager.enums.resolution import Resolution
+from core.contracts.process_manager.enums.unit import Unit
+from tests.helpers.builders.submitted_transactions_builder import PointsBuilder, UnpackedSubmittedTransactionsBuilder
 
-metering_point_type_enum_params = [pytest.param(x.value, 1) for x in MeteringPointType if x.value != "MPT_UNSPECIFIED"]
+metering_point_type_enum_params = [
+    pytest.param(x.value, 1) for x in MeteringPointType if x.value != MeteringPointType.MPT_UNSPECIFIED.value
+]
+orchestration_type_enum_params = [
+    pytest.param(x.value, 1) for x in OrchestrationType if x.value != OrchestrationType.OT_UNSPECIFIED.value
+]
+quality_enum_params = [pytest.param(x.value, 1) for x in Quality if x.value != Quality.Q_UNSPECIFIED.value]
+unit_enum_params = [pytest.param(x.value, 1) for x in Unit if x.value != Unit.U_UNSPECIFIED.value]
+resolution_enum_params = [pytest.param(x.value, 1) for x in Resolution if x.value != Resolution.R_UNSPECIFIED.value]
+
+
+@pytest.mark.parametrize(
+    "orchestration_type, expected_count",
+    [
+        pytest.param(OrchestrationType.OT_UNSPECIFIED.value, 0),
+        pytest.param("", 0),
+        pytest.param(None, 0),
+    ].__add__(orchestration_type_enum_params),
+)
+def test__orchestration_type_enum_validations(
+    spark: SparkSession,
+    orchestration_type: str,
+    expected_count: int,
+) -> None:
+    unpacked_submitted_transactions = (
+        UnpackedSubmittedTransactionsBuilder(spark).add_row(orchestration_type=orchestration_type).build()
+    )
+
+    actual = unpacked_submitted_transactions.filter(enum_validations.validate_orchestration_type_enum())
+    assert actual.count() == expected_count
+
+
+@pytest.mark.parametrize(
+    "quality, expected_count",
+    [
+        pytest.param(Quality.Q_UNSPECIFIED.value, 0),
+        pytest.param("", 0),
+        pytest.param(None, 0),
+    ].__add__(quality_enum_params),
+)
+def test__quality_enum_validations(
+    spark: SparkSession,
+    quality: str,
+    expected_count: int,
+) -> None:
+    points = PointsBuilder(spark).add_row(quality=quality).build()
+
+    print(points[0].quality)
+
+    unpacked_submitted_transactions = UnpackedSubmittedTransactionsBuilder(spark).add_row(points=points).build()
+
+    unpacked_submitted_transactions.show()
+
+    actual = unpacked_submitted_transactions.filter(enum_validations.validate_quality_enum())
+    assert actual.count() == expected_count
 
 
 @pytest.mark.parametrize(
     "metering_point_type, expected_count",
     [
-        pytest.param("UNKNOWN", 0),
-        pytest.param("MPT_UNSPECIFIED", 0),
+        pytest.param(MeteringPointType.MPT_UNSPECIFIED.value, 0),
         pytest.param("", 0),
+        pytest.param(None, 0),
     ].__add__(metering_point_type_enum_params),
 )
 def test__metering_point_type_enum_validations(
@@ -26,4 +84,42 @@ def test__metering_point_type_enum_validations(
     )
 
     actual = unpacked_submitted_transactions.filter(enum_validations.validate_metering_point_type_enum())
+    assert actual.count() == expected_count
+
+
+@pytest.mark.parametrize(
+    "unit, expected_count",
+    [
+        pytest.param(Unit.U_UNSPECIFIED.value, 0),
+        pytest.param("", 0),
+        pytest.param(None, 0),
+    ].__add__(unit_enum_params),
+)
+def test__unit_enum_validations(
+    spark: SparkSession,
+    unit: str,
+    expected_count: int,
+) -> None:
+    unpacked_submitted_transactions = UnpackedSubmittedTransactionsBuilder(spark).add_row(unit=unit).build()
+
+    actual = unpacked_submitted_transactions.filter(enum_validations.validate_unit_enum())
+    assert actual.count() == expected_count
+
+
+@pytest.mark.parametrize(
+    "resolution, expected_count",
+    [
+        pytest.param(Resolution.R_UNSPECIFIED.value, 0),
+        pytest.param("", 0),
+        pytest.param(None, 0),
+    ].__add__(resolution_enum_params),
+)
+def test__resolution_enum_validations(
+    spark: SparkSession,
+    resolution: str,
+    expected_count: int,
+) -> None:
+    unpacked_submitted_transactions = UnpackedSubmittedTransactionsBuilder(spark).add_row(resolution=resolution).build()
+
+    actual = unpacked_submitted_transactions.filter(enum_validations.validate_resolution_enum())
     assert actual.count() == expected_count
