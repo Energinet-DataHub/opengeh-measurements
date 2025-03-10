e4d149c8117f5a400fe1ed9523260438eef44d88
Svend (SLB) <slb@halfspace.io>
2025-03-10 16:18:39 +0100

    Reference issue fix

--- source/core/src/core/bronze/infrastructure/settings/__init__.py
+++ source/core/src/core/bronze/infrastructure/settings/__init__.py
@@ -1,5 +1,8 @@
 from core.bronze.infrastructure.settings.submitted_transactions_stream_settings import (
     SubmittedTransactionsStreamSettings,
 )
+from core.bronze.infrastructure.settings.migrated_transactions_stream_settings import (
+    MigratedTransactionsStreamSettings,
+)
 
-__all__ = ["SubmittedTransactionsStreamSettings"]
+__all__ = ["SubmittedTransactionsStreamSettings", "MigratedTransactionsStreamSettings"]
