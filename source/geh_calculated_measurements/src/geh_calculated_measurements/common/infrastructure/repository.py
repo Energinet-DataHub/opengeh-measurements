c733756a06dd3c15e1a54fa18fba7251fdb18a4f
Jonas Michelsen <jvm@energinet.dk>
2025-03-11 16:00:14 +0100

    the boring way

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/repository.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/repository.py
@@ -1,6 +1,4 @@
-from typing import Any
-
-from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql import SparkSession
 
 from geh_calculated_measurements.common.domain import CalculatedMeasurements
 from geh_calculated_measurements.common.infrastructure.calculated_measurements.database_definitions import (
@@ -22,25 +20,6 @@ class Repository:
         if self._catalog_name:
             return f"{self._catalog_name}.{database_name}.{table_name}"
         return f"{database_name}.{table_name}"
-
-    def _map_data_to_table_and_dataframe(self, data: Any, data_type_to_table_name: dict) -> tuple[str, DataFrame]:
-        """Map the input data to the corresponding table name and DataFrame."""
-        table_name = data_type_to_table_name.get(type(data))
-        if table_name is None:
-            raise ValueError(f"Unsupported data type: {type(data)}")
-
-        return table_name, data.df
-
-    def write_to_calculated_measurements_internal_database(
-        self, data: Any, data_type_to_table_name: dict, write_mode: str = "append"
-    ) -> None:
-        """Write the data to the specified Delta table."""
-        table_name, df = self._map_data_to_table_and_dataframe(data, data_type_to_table_name)
-
-        if table_name is not None and df is not None:
-            df.write.format("delta").mode(write_mode).saveAsTable(self._get_full_table_path(table_name))
-        else:
-            raise ValueError("Could not determine table name or dataframe from input data.")
 
     def write_calculated_measurements(self, data: CalculatedMeasurements) -> None:
         df = data.df
