ff477fd172d80da1aab0586d9a029861c1315985
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-11 16:35:30 +0100

    ...

--- source/geh_calculated_measurements/tests/subsystem_tests/conftest.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/conftest.py
@@ -1,12 +1,7 @@
 import pytest
 
-from tests.subsystem_tests.electrical_heating.environment_configuration import EnvironmentConfiguration
 from tests.subsystem_tests.electrical_heating.fixtures.eletrical_heating_fixture import ElectricalHeatingFixture
-
-
-@pytest.fixture(scope="session")
-def environment_configuration() -> EnvironmentConfiguration:
-    return EnvironmentConfiguration()
+from tests.subsystem_tests.environment_configuration import EnvironmentConfiguration
 
 
 @pytest.fixture(scope="session")
