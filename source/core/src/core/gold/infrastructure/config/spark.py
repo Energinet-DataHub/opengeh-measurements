434507715c149eb89faebc81b75ddd0531b45393
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-11 10:16:43 +0100

    Update spark.py

--- source/core/src/core/gold/infrastructure/config/spark.py
+++ source/core/src/core/gold/infrastructure/config/spark.py
@@ -3,9 +3,5 @@ from pyspark.sql.session import SparkSession
 
 
 def initialize_spark() -> SparkSession:
-    spark_conf = (
-        SparkConf(loadDefaults=True)
-        .set("spark.sql.session.timeZone", "UTC")
-        .set("spark.sql.shuffle.partitions", "auto")
-    )
+    spark_conf = SparkConf(loadDefaults=True).set("spark.sql.session.timeZone", "UTC")
     return SparkSession.builder.config(conf=spark_conf).getOrCreate()
