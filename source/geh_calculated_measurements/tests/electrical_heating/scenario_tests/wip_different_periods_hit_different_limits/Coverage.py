d9de600b7a7412c13fb530b1c472cf4bbd3d8c7e
ClausQAEnerginet <xclpe@energinet.dk>
2025-03-12 15:21:29 +0100

    update

--- source/geh_calculated_measurements/tests/electrical_heating/scenario_tests/wip_different_periods_hit_different_limits/Coverage.py
+++ source/geh_calculated_measurements/tests/electrical_heating/scenario_tests/wip_different_periods_hit_different_limits/Coverage.py
@@ -5,11 +5,11 @@ The purpose is to test scenarios where limit is not 4000
 ## DESIGN CONSIDERATIONS
 
 CASE 1
-                    ________|2024__________________
+                    ________|2024__________|2025___
 Parent start/end:       |-----------------------...
 Child start/end:        |-----------------------...
-Measurement data:         +   +
-Periods:                |___|_____________________
+Measurement data:         +   +              +
+Periods:                |___|______________|____...
 
 CASE 2 - gap across two years
                     ________|2024__________________
@@ -17,7 +17,7 @@ Parent start/end:       |--------------|
 Child start/end:        |--------------|
 
 Measurement data:         +   +
-Periods:                |____|__________|
+Periods:                |___|__________|
 
 CASE 3 - period of 1 day
                     ________|2024__________________
--- source/geh_calculated_measurements/tests/electrical_heating/scenario_tests/wip_gaps/Coverage.py
+++ source/geh_calculated_measurements/tests/electrical_heating/scenario_tests/wip_gaps/Coverage.py
@@ -5,11 +5,13 @@ The purpose is to test gaps in periods with EH for the same metering point.
 ## DESIGN CONSIDERATIONS
 
 CASE 1 - gap in same year
-                    |2024
-Parent start/end:   |---------|     |---|   |---...
-Child start/end:    |---------|     |---|   |---...
+                    |2024        |2025
+Parent start/end:   |---------|         |---|   |---...
+Child 1 start/end:  |---------|
+Child 2 start/end:                      |---|
+Child 3 start/end:                              |---...
 Measurement data:   +++++++++++++++++++++++********
-Added to child:     ***********     *****   *******
+Added to child:     ***********         *****   *******
 
 ## CASES TESTED
 """
