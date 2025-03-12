96519a336825a297e907394d4ae8025d73643ed0
Alexander <alexanderharrow@hotmail.com>
2025-03-12 14:05:36 +0100

    Updates

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
@@ -4,8 +4,11 @@ from pyspark.sql import SparkSession
 from geh_calculated_measurements.missing_measurements_log.application.missing_measurements_log_args import (
     MissingMeasurementsLogArgs,
 )
+from geh_calculated_measurements.missing_measurements_log.domain import (
+    execute,
+)
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: MissingMeasurementsLogArgs) -> None:
-    pass
+    execute()
