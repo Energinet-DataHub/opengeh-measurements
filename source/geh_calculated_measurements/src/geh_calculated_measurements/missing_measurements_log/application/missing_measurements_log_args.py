a0bf2b2dc3f061c376a7a626102df18d571df082
Alexander <alexanderharrow@hotmail.com>
2025-03-12 13:58:36 +0100

    Update missing_measurements_log_args.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/missing_measurements_log_args.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/missing_measurements_log_args.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from uuid import UUID
 
 from geh_common.application.settings import ApplicationSettings
@@ -8,5 +7,4 @@ from pydantic import Field
 class MissingMeasurementsLogArgs(ApplicationSettings):
     orchestration_instance_id: UUID = Field(init=False)
     time_zone: str = "Europe/Copenhagen"
-    execution_start_datetime: datetime = datetime.now()
     catalog_name: str = Field(init=False)
