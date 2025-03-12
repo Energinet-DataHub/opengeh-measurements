91e852d8dcafc42d66fbb9972b628a7f88b6b6b2
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-12 09:33:10 +0100

    refac: Silver and Gold stream (#391)

--- source/core/tests/gold/infrastructure/repositories/test_measurements_gold_repository.py
+++ source/core/tests/gold/infrastructure/repositories/test_measurements_gold_repository.py
@@ -1,44 +1,30 @@
 import os
 from unittest import mock
 
-from core.gold.infrastructure.config import GoldTableNames
 from core.gold.infrastructure.repositories.gold_measurements_repository import GoldMeasurementsRepository
-from core.settings.gold_settings import GoldSettings
-from core.settings.storage_account_settings import StorageAccountSettings
 
 
 def test__start_write_stream__calls_expected(mock_checkpoint_path: mock.MagicMock | mock.AsyncMock):
     # Arrange
     mocked_measurements = mock.Mock()
     mocked_batch_operation = mock.Mock()
+    with mock.patch("core.gold.infrastructure.config.spark.initialize_spark"):
+        # Act
+        GoldMeasurementsRepository().write_stream(
+            mocked_measurements,
+            mocked_batch_operation,
+        )
 
-    expected_checkpoint_path = mock_checkpoint_path.return_value
+        # Assert
+        mocked_measurements.writeStream.format.assert_called_once_with("delta")
+        mocked_measurements.writeStream.format().queryName.assert_called_once_with("measurements_silver_to_gold")
+        mocked_measurements.writeStream.format().queryName().option().trigger.assert_called_once_with(availableNow=True)
+        mocked_measurements.writeStream.format().queryName().option().trigger().foreachBatch.assert_called_once_with(
+            mocked_batch_operation
+        )
 
-    expected_data_lake_settings = StorageAccountSettings().DATALAKE_STORAGE_ACCOUNT
-    expected_gold_container_name = GoldSettings().gold_container_name
-
-    # Act
-    GoldMeasurementsRepository().start_write_stream(
-        mocked_measurements, mocked_batch_operation, terminate_on_empty=True
-    )
-
-    # Assert
-    mocked_measurements.writeStream.format.assert_called_once_with("delta")
-    mocked_measurements.writeStream.format().queryName.assert_called_once_with("measurements_silver_to_gold")
-    mocked_measurements.writeStream.format().queryName().option.assert_called_once_with(
-        "checkpointLocation", expected_checkpoint_path
-    )
-    mocked_measurements.writeStream.format().queryName().option().foreachBatch.assert_called_once_with(
-        mocked_batch_operation
-    )
-    mocked_measurements.writeStream.format().queryName().option().foreachBatch().trigger.assert_called_once()
-
-    mocked_measurements.writeStream.format().queryName().option().foreachBatch().trigger().start.assert_called_once()
-    mocked_measurements.writeStream.format().queryName().option().foreachBatch().trigger().start().awaitTermination.assert_called_once()
-
-    mock_checkpoint_path.assert_called_once_with(
-        expected_data_lake_settings, expected_gold_container_name, GoldTableNames.gold_measurements
-    )
+        mocked_measurements.writeStream.format().queryName().option().trigger().foreachBatch().start.assert_called_once()
+        mocked_measurements.writeStream.format().queryName().option().trigger().foreachBatch().start().awaitTermination.assert_called_once()
 
 
 def test__start_write_stream__when_contionous_streaming_is_disabled__should_not_call_trigger() -> None:
@@ -46,9 +32,9 @@ def test__start_write_stream__when_contionous_streaming_is_disabled__should_not_
     os.environ["CONTINUOUS_STREAMING_ENABLED"] = "true"
     mocked_measurements = mock.Mock()
     mocked_batch_operation = mock.Mock()
+    with mock.patch("core.gold.infrastructure.config.spark.initialize_spark"):
+        # Act
+        GoldMeasurementsRepository().write_stream(mocked_measurements, mocked_batch_operation)
 
-    # Act
-    GoldMeasurementsRepository().start_write_stream(mocked_measurements, mocked_batch_operation)
-
-    # Assert
-    mocked_measurements.writeStream.outputMode().option().trigger.assert_not_called()
+        # Assert
+        mocked_measurements.writeStream.format().queryName().option().trigger.assert_not_called()
