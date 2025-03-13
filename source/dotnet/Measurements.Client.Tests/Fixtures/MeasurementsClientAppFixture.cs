b047a7261e85c4b7ed6f2fb7357972c34eaee329
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-03-13 09:43:36 +0100

    wip

---
+++ source/dotnet/Measurements.Client.Tests/Fixtures/MeasurementsClientAppFixture.cs
@@ -0,0 +1,33 @@
+﻿using Energinet.DataHub.Core.FunctionApp.TestCommon.Configuration;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Databricks;
+using Energinet.DataHub.Measurements.WebApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Energinet.DataHub.Measurements.Client.Tests.Fixtures;
+
+public class MeasurementsClientAppFixture : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    public DatabricksSchemaManager DatabricksSchemaManager { get; set; }
+
+    public IntegrationTestConfiguration IntegrationTestConfiguration { get; }
+
+    public MeasurementsClientAppFixture()
+    {
+        var httpClientFactory = new HttpClientFactory();
+        IntegrationTestConfiguration = new IntegrationTestConfiguration();
+        DatabricksSchemaManager = new DatabricksSchemaManager(
+            new HttpClientFactory(),
+            IntegrationTestConfiguration.DatabricksSettings,
+            "mmcore_measurementsapi");
+    }
+
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ source/dotnet/Measurements.Client.Tests/MeasurementsClientTests.cs
@@ -0,0 +1,58 @@
+﻿using Energinet.DataHub.Measurements.Abstractions.Api.Queries;
+using Energinet.DataHub.Measurements.Client.Extensions.DependencyInjection;
+using Energinet.DataHub.Measurements.Client.Extensions.Options;
+using Energinet.DataHub.Measurements.Client.Tests.Extensions;
+using Energinet.DataHub.Measurements.Client.Tests.Fixtures;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.Measurements.Client.Tests;
+
+public class MeasurementsClientTests : IAsyncLifetime
+{
+    private MeasurementsClientAppFixture Fixture { get; }
+
+    private ServiceProvider ServiceProvider { get; }
+
+    public MeasurementsClientTests(
+        MeasurementsClientAppFixture fixture,
+        ITestOutputHelper testOutputHelper)
+    {
+        Fixture = fixture;
+
+        var services = new ServiceCollection();
+        services.AddInMemoryConfiguration(new Dictionary<string, string?>
+        {
+            [$"{MeasurementHttpClientOptions.SectionName}:{nameof(MeasurementHttpClientOptions.BaseAddress)}"]
+                = Fixture.HttpClient.BaseAddress!.ToString(),
+        });
+        services.AddMeasurementsClient();
+        ServiceProvider = services.BuildServiceProvider();
+    }
+
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task GetMeasurementsForDayAsync_WhenCalledWithValidQuery_ReturnsMeasurementDto()
+    {
+        // Arrange
+        var client = ServiceProvider.GetRequiredService<IMeasurementsClient>();
+        var query = new GetMeasurementsForDayQuery(
+            "MeteringPointId",
+            new DateTimeOffset(2025, 3, 1, 0, 0, 0, TimeSpan.Zero));
+
+        // Act
+        var result = await client.GetMeasurementsForDayAsync(query, CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
