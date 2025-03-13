f494466fd3c3de39903ed86eafdc5bded1079578
Søren Frøhlich Kronborg <sorenfrohlichkronborg@Srens-MacBook-Pro.local>
2025-03-13 10:54:51 +0100

    found all database names and rewrote them into env vars

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/calculated_measurements/database_definitions.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/calculated_measurements/database_definitions.py
@@ -1,10 +1,47 @@
-class CalculatedMeasurementsInternalDatabaseDefinition:
-    DATABASE_NAME = "measurements_calculated_internal"
-    # Should match whatever name is currently being used in Databricks
+from geh_common.application.settings import ApplicationSettings
+from pydantic import Field
+
+#class CalculatedMeasurementsInternalDatabaseDefinition:
+#    # note: This database is also hardcoded somewhere else.
+#    DATABASE_NAME = "measurements_calculated_internal"
+#    # Should match whatever name is currently being used in Databricks
+#    MEASUREMENTS_NAME = "calculated_measurements"
+#    CAPACITY_SETTLEMENT_TEN_LARGEST_QUANTITIES_NAME = "capacity_settlement_ten_largest_quantities"
+#    CAPACITY_SETTLEMENT_CALCULATIONS_NAME = "capacity_settlement_calculations"
+
+
+
+class CalculatedMeasurementsInternalDatabaseDefinition(ApplicationSettings):
+    """Configuration class inheriting pydantic's BaseSettings to automatically load environmental variable.
+
+    Used to define and validate settings for the unity catalog used by package.
+
+    Attributes:
+    catalog_name (str): The name of the unity catalog created in infrastructure.
+    """
+
+    MEASUREMENTS_CALCULATED_INTERNAL_DATABASE: str = Field(init=False)
+
     MEASUREMENTS_NAME = "calculated_measurements"
     CAPACITY_SETTLEMENT_TEN_LARGEST_QUANTITIES_NAME = "capacity_settlement_ten_largest_quantities"
     CAPACITY_SETTLEMENT_CALCULATIONS_NAME = "capacity_settlement_calculations"
 
+    class Config:
+        case_sensitive = False
 
-class CalculatedMeasurementsDatabaseDefinition:
-    DATABASE_NAME = "measurements_calculated"
+#class CalculatedMeasurementsDatabaseDefinition:
+#    DATABASE_NAME = "measurements_calculated"
+
+class CalculatedMeasurementsDatabaseDefinition(ApplicationSettings):
+    """Configuration class inheriting pydantic's BaseSettings to automatically load environmental variable.
+
+    Used to define and validate settings for the unity catalog used by package.
+
+    Attributes:
+    catalog_name (str): The name of the unity catalog created in infrastructure.
+    """
+
+    measurements_calculated_database: str = Field(init=False)
+
+    class Config:
+        case_sensitive = False
--- source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/database_definitions.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/database_definitions.py
@@ -1,4 +1,26 @@
-class MeasurementsCalculatedInternalDatabaseDefinition:
-    measurements_calculated_internal_database = "measurements_calculated_internal"
-    executed_migrations_table_name = "executed_migrations"
-    MEASUREMENTS_NAME = "measurements"
+from geh_common.application.settings import ApplicationSettings
+from pydantic import Field
+
+#class MeasurementsCalculatedInternalDatabaseDefinition:
+#    # TODO; hardcoded data
+#    measurements_calculated_internal_database = "measurements_calculated_internal"
+#    executed_migrations_table_name = "executed_migrations"
+#    MEASUREMENTS_NAME = "measurements"    NOTE: This is not being used
+
+
+class MeasurementsCalculatedInternalDatabaseDefinition(ApplicationSettings):
+    """Configuration class inheriting pydantic's BaseSettings to automatically load environmental variable.
+
+    Used to define and validate settings for the unity catalog used by package.
+
+    Attributes:
+    database_name (str): The name of the unity catalog created in infrastructure.
+    """
+
+    measurements_calculated_internal_database: str = Field(init=False)
+
+    MEASUREMENTS_NAME: str = Field(init=False)
+    executed_migrations_table_name: str = Field(init=False)
+
+    class Config:
+        case_sensitive = False
