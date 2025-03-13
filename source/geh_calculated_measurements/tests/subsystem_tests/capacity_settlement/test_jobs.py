89703802a17d9bdd85517044dfa813a530c70ebf
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-13 15:00:54 +0100

    ...

--- source/geh_calculated_measurements/tests/subsystem_tests/capacity_settlement/test_jobs.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/capacity_settlement/test_jobs.py
@@ -7,7 +7,7 @@ from geh_common.domain.types import MeteringPointType
 from tests.subsystem_tests.base_resources.base_job_fixture import BaseJobFixture
 from tests.subsystem_tests.base_resources.base_job_tests import BaseJobTests
 from tests.subsystem_tests.environment_configuration import EnvironmentConfiguration
-from tests.subsystem_tests.seed_gold_table import GoldTableRow
+from tests.subsystem_tests.seed_gold_table import GoldTableRow, GoldTableSeeder
 
 METERING_POINT_ID = "170000040000000201"
 CALCULATION_YEAR = 2025
@@ -41,6 +41,8 @@ class TestCapacitySettlement(BaseJobTests):
 
     def get_or_create_fixture(self, environment_configuration: EnvironmentConfiguration) -> BaseJobFixture:
         if self.fixture is None:
+            table_seeder = GoldTableSeeder(environment_configuration)
+            table_seeder.seed(self._get_gold_table_rows())
             self.fixture = BaseJobFixture(
                 environment_configuration=environment_configuration,
                 job_name="CapacitySettlement",
