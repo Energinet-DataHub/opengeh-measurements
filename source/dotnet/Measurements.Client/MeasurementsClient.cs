b77f07ddf4c9a18a688d3c5f5d033543c58e8145
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-03-13 20:44:44 +0100

    Add correct formatting of date

--- source/dotnet/Measurements.Client/MeasurementsClient.cs
+++ source/dotnet/Measurements.Client/MeasurementsClient.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http.Json;
+﻿using System.Globalization;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Energinet.DataHub.Measurements.Abstractions.Api.Dtos;
 using Energinet.DataHub.Measurements.Abstractions.Api.Queries;
 using Energinet.DataHub.Measurements.Client.Extensions.DependencyInjection;
@@ -16,18 +19,26 @@ public class MeasurementsClient : IMeasurementsClient
 
     public async Task<MeasurementDto?> GetMeasurementsForDayAsync(GetMeasurementsForDayQuery query, CancellationToken cancellationToken = default)
     {
-        var response = await _httpClient
-            .GetAsync(
-                $"/measurements?MeteringPointId={query.MeteringPointId}&StartDate={query.Date:yyyy-MM-ddTHH:mm:ssZ}&EndDate={query.Date.AddDays(1):yyyy-MM-ddTHH:mm:ssZ}",
-                cancellationToken)
-            .ConfigureAwait(false);
+        var url = CreateUrl(query);
+        var response = await _httpClient.GetAsync(url, cancellationToken).ConfigureAwait(false);
 
         response.EnsureSuccessStatusCode();
 
+        var asString = await response.Content.ReadAsStringAsync(cancellationToken);
         var measurementDto = await response.Content
             .ReadFromJsonAsync<MeasurementDto>(cancellationToken)
             .ConfigureAwait(false);
 
         return measurementDto;
+    }
+
+    private static string CreateUrl(GetMeasurementsForDayQuery query)
+    {
+        return $"/measurements?MeteringPointId={query.MeteringPointId}&StartDate={FormatDate(query.Date)}&EndDate={FormatDate(query.Date.AddDays(1))}";
+    }
+
+    private static string FormatDate(DateTimeOffset date)
+    {
+        return date.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
     }
 }
