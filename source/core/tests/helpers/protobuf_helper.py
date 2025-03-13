4752a9434bdf2605132fc088a6c9b7b04f314dd7
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-13 10:56:01 +0100

    refac: use constants instead of hardcoded strings

--- source/core/tests/helpers/protobuf_helper.py
+++ source/core/tests/helpers/protobuf_helper.py
@@ -4,6 +4,10 @@ import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
 from pyspark.sql.protobuf.functions import from_protobuf
 
+from core.contracts.process_manager.brs021_forward_metered_data_notify_v1_column_names import (
+    Brs021ForwardMeteredDataNotifyV1ColumnNames,
+    Brs021ForwardMeteredDataNotifyV1EventColumnNames,
+)
 from core.silver.domain.constants.descriptor_file_names import DescriptorFileNames
 
 alias_name = "measurement_values"
@@ -20,11 +24,20 @@ def unpack_brs021_forward_metered_data_notify_v1(brs021_forward_metered_data_not
     options = {"recursive.fields.max.depth": "3", "emit.default.values": "true"}
 
     unpacked = brs021_forward_metered_data_notify_v1.select(
-        from_protobuf("value", message_name, descFilePath=descriptor_path, options=options).alias(alias_name),
-        "value",
+        from_protobuf(
+            Brs021ForwardMeteredDataNotifyV1EventColumnNames.value,
+            message_name,
+            descFilePath=descriptor_path,
+            options=options,
+        ).alias(alias_name),
+        Brs021ForwardMeteredDataNotifyV1EventColumnNames.value,
     )
 
     return unpacked.select(
-        F.col(f"{alias_name}.version").alias("version"),
-        F.col(f"{alias_name}.orchestration_instance_id").alias("orchestration_instance_id"),
+        F.col(f"{alias_name}.{Brs021ForwardMeteredDataNotifyV1ColumnNames.version}").alias(
+            Brs021ForwardMeteredDataNotifyV1ColumnNames.version
+        ),
+        F.col(f"{alias_name}.{Brs021ForwardMeteredDataNotifyV1ColumnNames.orchestration_instance_id}").alias(
+            Brs021ForwardMeteredDataNotifyV1ColumnNames.orchestration_instance_id
+        ),
     )
