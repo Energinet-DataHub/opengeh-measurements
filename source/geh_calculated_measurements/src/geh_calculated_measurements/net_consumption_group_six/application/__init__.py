9cc000de80d45d3ab766af1be035ab5ce75aab7c
Jonas Michelsen <jvm@energinet.dk>
2025-03-13 13:29:08 +0100

    feat: Add net consumption group six folders and files

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_six/application/__init__.py
@@ -0,0 +1,4 @@
+from .calculation import execute_application
+from .net_consumption_group_six_args import NetConsumptionGroupSixArgs
+
+__all__ = ["execute_application", "NetConsumptionGroupSixArgs"]
