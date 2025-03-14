fdb082cc9d01f6d60aa182e263cbbfffa769ba14
Søren Frøhlich Kronborg <sorenfrohlichkronborg@Srens-MacBook-Pro.local>
2025-03-14 09:28:27 +0100

    removed changes in core

--- source/core/src/core/migrations/database_names.py
+++ source/core/src/core/migrations/database_names.py
@@ -1,23 +1,3 @@
-from geh_common.application.settings import ApplicationSettings
-from pydantic import Field
-
-
-class DatabaseNames(ApplicationSettings):
-    """Configuration class inheriting pydantic's BaseSettings to automatically load environmental variable.
-
-    Used to define and validate settings for the unity catalog used by package.
-
-    Attributes:
-    database_name (str): The name of the unity catalog created in infrastructure.
-    """
-
-    measurements_internal_database: str = Field(init=False)
-
-    class Config:
-        case_sensitive = False
-
-
-#class DatabaseNames:
-#    # Should probably be an environment variable because it is created in Terraform.
-#    # TODO; hardcoded data
-#    measurements_internal_database = "measurements_internal"
+class DatabaseNames:
+    # Should probably be an environment variable because it is created in Terraform.
+    measurements_internal_database = "measurements_internal"
