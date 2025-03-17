c97af373a6cef0b850282b0f77c579909277affb
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-03-17 11:40:38 +0000

    Renamed file, renamed schema to database, added Arrange-Act-Assert comments

--- source/geh_calculated_measurements/tests/data_products/contract_and_database_are_equal/test_contract_and_database_equality.py
+++ source/geh_calculated_measurements/tests/data_products/contract_and_database_are_equal/test_contract_and_database_equality.py
@@ -12,11 +12,14 @@ def test_contract_and_schema_are_equal(
     assert_dataframes_configuration: AssertDataframesConfiguration,
     spark: SparkSession,
 ) -> None:
+    # Arrange
     view_name = "hourly_calculated_measurements_v1"
-    schema = CalculatedMeasurementsDatabaseDefinition.DATABASE_NAME
+    database = CalculatedMeasurementsDatabaseDefinition.DATABASE_NAME
     catalog = CatalogSettings().catalog_name
-    view_df = spark.sql(f"SELECT * FROM {catalog}.{schema}.{view_name} LIMIT 1")
-
     contract_schema = hourly_calculated_measurements_v1.hourly_calculated_measurements_v1
 
+    # Act
+    view_df = spark.table(f"{catalog}.{database}.{view_name}").limit(1)
+
+    # Assert
     assert_contract(actual_schema=view_df.schema, contract=contract_schema)
