5531c3140ad3dda38c9c2d2b956799801a019971
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-03-17 13:26:48 +0100

    feat: Added data product contract for the hourly calculated measurements view (#421)

---
+++ source/geh_calculated_measurements/tests/data_products/contract_and_database_are_equal/test_contract_and_database_equality.py
@@ -0,0 +1,25 @@
+from geh_common.testing.dataframes import AssertDataframesConfiguration, assert_contract
+from pyspark.sql import SparkSession
+
+from geh_calculated_measurements.common.infrastructure import CalculatedMeasurementsDatabaseDefinition
+from geh_calculated_measurements.contracts.data_products import hourly_calculated_measurements_v1
+from geh_calculated_measurements.database_migrations.settings.catalog_settings import CatalogSettings
+
+
+def test_contract_and_schema_are_equal(
+    migrations_executed: None,
+    patch_environment: None,
+    assert_dataframes_configuration: AssertDataframesConfiguration,
+    spark: SparkSession,
+) -> None:
+    # Arrange
+    view_name = "hourly_calculated_measurements_v1"
+    database = CalculatedMeasurementsDatabaseDefinition.DATABASE_NAME
+    catalog = CatalogSettings().catalog_name
+    contract_schema = hourly_calculated_measurements_v1.hourly_calculated_measurements_v1
+
+    # Act
+    view_df = spark.table(f"{catalog}.{database}.{view_name}").limit(1)
+
+    # Assert
+    assert_contract(actual_schema=view_df.schema, contract=contract_schema)
