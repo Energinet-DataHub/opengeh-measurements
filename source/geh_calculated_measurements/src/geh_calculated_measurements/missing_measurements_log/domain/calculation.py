fc7245792b81fff340d734e62849ea688c4edb71
Alexander <alexanderharrow@hotmail.com>
2025-03-17 21:48:15 +0100

    Updates

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/domain/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/domain/calculation.py
@@ -3,11 +3,4 @@ from geh_common.telemetry import use_span
 
 @use_span()
 def execute() -> None:
-    dummy()
-
-
-# Dummy function that activates logging to Azure.
-# Remove this function when the real implementation is added.
-@use_span()
-def dummy() -> int:
-    return 1 + 1
+    temp = 1 + 1
--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/application/calculation.py
@@ -1,32 +1,12 @@
 from geh_common.telemetry.decorators import use_span
 from pyspark.sql import SparkSession
 
-from geh_calculated_measurements.electrical_heating.domain import (
-    TimeSeriesPoints,
-    child_metering_points_v1,
-    consumption_metering_point_periods_v1,
-    time_series_points_v1,
-)
 from geh_calculated_measurements.net_consumption_group_6.application.net_consumption_group_6_args import (
     NetConsumptionGroup6Args,
 )
 from geh_calculated_measurements.net_consumption_group_6.domain import execute
-from geh_calculated_measurements.net_consumption_group_6.domain.daily import ConsumptionMeteringPointPeriods
-from geh_calculated_measurements.net_consumption_group_6.domain.model import ChildMeteringPoints
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: NetConsumptionGroup6Args) -> None:
-    # TODO: Replace this dummy code with the real implementation
-    df = spark.createDataFrame([], schema=consumption_metering_point_periods_v1)
-    df1 = spark.createDataFrame([], schema=child_metering_points_v1)
-    df2 = spark.createDataFrame([], schema=time_series_points_v1)
-    consumption_metering_points_periods = ConsumptionMeteringPointPeriods(df)
-    child_metering_points = ChildMeteringPoints(df1)
-    time_series_points = TimeSeriesPoints(df2)
-
-    execute(
-        consumption_metering_points_periods,
-        child_metering_points,
-        time_series_points,
-    )
+    execute()
