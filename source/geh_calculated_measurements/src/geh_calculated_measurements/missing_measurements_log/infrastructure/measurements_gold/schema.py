af148a98eb19457d3c3f4b034878f87ff58a1a19
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-17 14:56:55 +0100

    ...

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/infrastructure/measurements_gold/schema.py
@@ -0,0 +1,19 @@
+import pyspark.sql.types as T
+
+nullable = True
+
+# Observation times where a metering point does have measurement (i.e. where it is not missing)
+missing_measurements_log_v1 = T.StructType(
+    [
+        #
+        # GSRN number
+        T.StructField("metering_point_id", T.StringType(), not nullable),
+        #
+        # UTC time
+        T.StructField(
+            "observation_time",
+            T.TimestampType(),
+            not nullable,
+        ),
+    ]
+)
