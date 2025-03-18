9f3297c60320ef28590c111441f32e75eaee6f94
Bjarke Meier <hej@bjarkemeier.dk>
2025-03-18 12:00:46 +0000

    add names

--- source/geh_calculated_measurements/tests/subsystem_tests/demo_test.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/demo_test.py
@@ -16,12 +16,18 @@ def electrical_heating_job_fixture() -> BaseJobFixture:
     return BaseJobFixture(f"electrical-heating-{uuid.uuid4()}")
 
 
-@pytest.mark.parametrize("job_fixture", [capacity_settlement_job_fixture(), electrical_heating_job_fixture()])
+@pytest.mark.parametrize(
+    "name,job_fixture",
+    [
+        ("capacity-settlement", capacity_settlement_job_fixture()),
+        ("electrical-heating", electrical_heating_job_fixture()),
+    ],
+)
 class TestJobTests:
     @pytest.mark.order(1)
-    def test__given_job_input(self, job_fixture: BaseJobFixture) -> None:
+    def test__given_job_input(self, name, job_fixture: BaseJobFixture) -> None:
         print(job_fixture.id)
 
     @pytest.mark.order(2)
-    def test__when_job_is_started(self, job_fixture: BaseJobFixture) -> None:
+    def test__when_job_is_started(self, name, job_fixture: BaseJobFixture) -> None:
         print(job_fixture.id)
