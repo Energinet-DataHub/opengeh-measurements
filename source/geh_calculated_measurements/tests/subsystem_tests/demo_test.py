9eb65d3ea5389d84e12aa4926277988d1e30dc20
Bjarke Meier <hej@bjarkemeier.dk>
2025-03-18 11:41:59 +0000

    demo done

--- source/geh_calculated_measurements/tests/subsystem_tests/demo_test.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/demo_test.py
@@ -8,7 +8,16 @@ class BaseJobFixture:
         self.id = f"########## {id} ##########"
 
 
-class BaseJobTests:
+def capacity_settlement_job_fixture() -> BaseJobFixture:
+    return BaseJobFixture(f"capacity-settlement-{uuid.uuid4()}")
+
+
+def electrical_heating_job_fixture() -> BaseJobFixture:
+    return BaseJobFixture(f"electrical-heating-{uuid.uuid4()}")
+
+
+@pytest.mark.parametrize("job_fixture", [capacity_settlement_job_fixture(), electrical_heating_job_fixture()])
+class TestJobTests:
     @pytest.mark.order(1)
     def test__given_job_input(self, job_fixture: BaseJobFixture) -> None:
         print(job_fixture.id)
@@ -16,17 +25,3 @@ class BaseJobTests:
     @pytest.mark.order(2)
     def test__when_job_is_started(self, job_fixture: BaseJobFixture) -> None:
         print(job_fixture.id)
-
-
-class TestFooJob(BaseJobTests):
-    @pytest.fixture(scope="class", autouse=True)  # Add autouse=True
-    def job_fixture(self) -> BaseJobFixture:
-        print("Creating Foo fixture")  # Optional debug print
-        return BaseJobFixture(f"Foo-{uuid.uuid4()}")
-
-
-class TestBarJob(BaseJobTests):
-    @pytest.fixture(scope="class", autouse=True)  # Add autouse=True
-    def job_fixture(self) -> BaseJobFixture:
-        print("Creating Bar fixture")  # Optional debug print
-        return BaseJobFixture(f"Bar-{uuid.uuid4()}")
