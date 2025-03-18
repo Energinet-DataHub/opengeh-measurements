cb7004cdd00de14021f2455a88bf9c710ffb464f
Svend (SLB) <slb@halfspace.io>
2025-03-18 16:01:47 +0100

    Separate add and drop in mig script

--- source/core/src/core/migrations/migration_scripts/20250318160000_add_is_cancelled_to_gold_measurements.sql
+++ source/core/src/core/migrations/migration_scripts/20250318160000_add_is_cancelled_to_gold_measurements.sql
@@ -2,10 +2,6 @@ ALTER TABLE {gold_database}.{gold_measurements}
 ADD COLUMNS (is_cancelled boolean after transaction_creation_datetime)
 GO
 
-ALTER TABLE {silver_database}.{silver_measurements}
-DROP COLUMNS (is_deleted)
-GO 
-
 UPDATE {gold_database}.{gold_measurements}
 SET is_cancelled = false 
 WHERE orchestration_type != "migration"
---
+++ source/core/src/core/migrations/migration_scripts/20250318170000_drop_column_is_deleted_in_measurements.sql
@@ -0,0 +1,7 @@
+ALTER TABLE {silver_database}.{silver_measurements}
+DROP COLUMNS (is_deleted) 
+GO 
+
+ALTER TABLE {bronze_database}.{submitted_transactions_quarantined_table}
+DROP COLUMNS (is_deleted)
+
