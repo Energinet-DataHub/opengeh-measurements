ca3028ba317cb29e0ef656e10e1cd8c84d483287
Svend (SLB) <slb@halfspace.io>
2025-03-18 16:25:19 +0100

    Fixed mig script

--- source/core/src/core/migrations/migration_scripts/20250318170000_drop_column_is_deleted_in_measurements.sql
+++ source/core/src/core/migrations/migration_scripts/20250318170000_drop_column_is_deleted_in_measurements.sql
@@ -1,7 +1,10 @@
 ALTER TABLE {silver_database}.{silver_measurements_table}
-DROP COLUMNS (is_deleted) 
-GO 
+DROP CONSTRAINT IF EXISTS is_deleted_is_not_null_chk
+GO
+
+ALTER TABLE {silver_database}.{silver_measurements_table}
+DROP COLUMNS IF EXISTS (is_deleted)
+GO
 
 ALTER TABLE {bronze_database}.{submitted_transactions_quarantined_table}
-DROP COLUMNS (is_deleted)
-
+DROP COLUMNS IF EXISTS (is_deleted)
