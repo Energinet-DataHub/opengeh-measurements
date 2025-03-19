069a7aba5c29b3f3c0d6260d2860575fea0b6ba3
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-19 14:14:42 +0100

    Update schema.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/measurements_gold/schema.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/measurements_gold/schema.py
@@ -2,17 +2,13 @@ import pyspark.sql.types as T
 
 nullable = True
 
-"""
-Schema for all current measurements. This is a generic contract used for multiple calculations
-"""
 current_measurements_schema = T.StructType(
     [
         # GSRN (18 characters) that uniquely identifies the metering point
         # Example: 578710000000000103
         T.StructField("metering_point_id", T.StringType(), not nullable),
         # Energy quantity in kWh for the given observation time.
-        # Null when quality is missing.
-        # Example: 1234.534217
+        # Example: 1234.534
         T.StructField("quantity", T.DecimalType(18, 3), not nullable),
         # "missing" | "estimated" | "measured" | "calculated"
         # Example: measured
@@ -21,4 +17,7 @@ current_measurements_schema = T.StructType(
         T.StructField("observation_time", T.TimestampType(), not nullable),
     ]
 )
+"""
+Schema for all current measurements. This is a generic contract used for multiple calculations
+"""
 
