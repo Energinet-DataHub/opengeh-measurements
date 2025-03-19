39eabc93dfe1daf38ddce2c2f43b43845ac12db1
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-19 14:30:52 +0100

    Update missing_measurements_log_args.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/missing_measurements_log_args.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/missing_measurements_log_args.py
@@ -1,14 +1,16 @@
 from datetime import datetime
+from typing import Annotated
 from uuid import UUID
 
 from geh_common.application.settings import ApplicationSettings
 from pydantic import Field
+from pydantic_settings import NoDecode
 
 
 class MissingMeasurementsLogArgs(ApplicationSettings):
     orchestration_instance_id: UUID = Field(init=False)
     period_start_datetime: datetime = Field(init=False)
     period_end_datetime: datetime = Field(init=False)
-    grid_area_codes: list[str] | None = Field(default=None)
+    grid_area_codes: Annotated[list[str], NoDecode] | None = Field(init=False, default=None)
     time_zone: str = "Europe/Copenhagen"
     catalog_name: str = Field(init=False)
