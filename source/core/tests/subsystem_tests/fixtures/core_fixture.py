3fd2aa9d22c59ff3bdc18e4c66b82febba74fe56
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-19 13:42:59 +0100

    fix assertion

--- source/core/tests/subsystem_tests/fixtures/core_fixture.py
+++ source/core/tests/subsystem_tests/fixtures/core_fixture.py
@@ -1,7 +1,10 @@
+from threading import Timer
+
 from azure.eventhub import EventData, EventHubConsumerClient, EventHubProducerClient, PartitionContext
 from azure.identity import DefaultAzureCredential
 from geh_common.databricks.databricks_api_client import DatabricksApiClient
 
+import core.contracts.process_manager.Brs021ForwardMeteredDataNotifyV1_pb2 as Brs021ForwardMeteredDataNotifyV1
 from tests.subsystem_tests.settings.databricks_settings import DatabricksSettings
 from tests.subsystem_tests.settings.kafka_settings import KafkaSettings
 
@@ -33,24 +36,42 @@ class CoreFixture:
 
             credential.close()
 
-    def assert_receipt(self) -> None:
+    def assert_receipt(self, orchestration_instance_id: str) -> None:
         kafka_settings = KafkaSettings()  # type: ignore
         credential = DefaultAzureCredential()
 
-        consumer = EventHubConsumerClient(
+        client = EventHubConsumerClient(
             fully_qualified_namespace=f"{kafka_settings.event_hub_namespace}.servicebus.windows.net",
             eventhub_name=kafka_settings.event_hub_receipt_instance,
-            consumer_group="$Default",
+            consumer_group="measurements-subsystemtest",
             credential=credential,
         )
 
+        def stop_receive(client):
+            print("Stopping client after 1 minute")
+            client.close()
+
+        global found_orchestration_instance_id
+        found_orchestration_instance_id = False
+
         # debug logging
-        def on_event_batch(partition_context: PartitionContext, events: list[EventData]) -> None:
-            print("DEBUG: partition_context: {}".format(partition_context))
-            print("DEBUG: events: {}".format(events))
+        def on_event(partition_context: PartitionContext, event: EventData | None) -> None:
+            global found_orchestration_instance_id
+            if event is None:
+                return
+            body = event.body_as_str()
+            message = Brs021ForwardMeteredDataNotifyV1.Brs021ForwardMeteredDataNotifyV1()
+            body = body.encode("utf-8")
+            message.ParseFromString(body)
 
-        with consumer:
-            print("DEBUG: Receiving events")
-            consumer.receive_batch(on_event_batch=on_event_batch)
+            if message.orchestration_instance_id == orchestration_instance_id:
+                found_orchestration_instance_id = True
+                client.close()
 
+        with client:
+            timer = Timer(60, stop_receive, [client])
+            timer.start()
+            client.receive(on_event=on_event, max_wait_time=60)
+
+        assert found_orchestration_instance_id is True
         credential.close()
--- source/core/tests/subsystem_tests/step_definitions/test_transmission_of_meter_readings.py
+++ source/core/tests/subsystem_tests/step_definitions/test_transmission_of_meter_readings.py
@@ -1,10 +1,10 @@
-import time
 from typing import Any
 
 import pytest
 from pyspark.sql import SparkSession
 from pytest_bdd import given, scenarios, then, when
 
+import tests.helpers.identifier_helper as identifier_helper
 from tests.conftest import Generator
 from tests.helpers.builders.submitted_transactions_builder import ValueBuilder
 from tests.subsystem_tests.fixtures.core_fixture import CoreFixture
@@ -23,26 +23,17 @@ def context() -> Generator[dict, Any, None]:
 
 @given("a valid measurement transaction")
 def _(spark: SparkSession, context: dict[str, Any]) -> None:
-    value = ValueBuilder(spark).add_row().build()
+    context["orchestration_instance_id"] = identifier_helper.generate_random_string()
+    value = ValueBuilder(spark).add_row(orchestration_instance_id=context["orchestration_instance_id"]).build()
     context["value"] = value
 
 
 @when("the measurement transaction is enqueued in the Event Hub")
 def _(context: dict[str, Any]) -> None:
-    print("DEBUG: Sending event with value: {}".format(context["value"]))
     context["fixture"].send_submitted_transactions_event(context["value"])
-    print("DEBUG: done with enqueue")
 
 
 @then("an acknowledgement is sent to the Event Hub")
 def _(context: dict[str, Any]) -> None:
     pass
-    start_time = time.time()
-    timeout = 120
-    poll_interval = 10
-
-    while time.time() - start_time < timeout:
-        print("DEBUG: Waiting for receipt")
-        context["fixture"].assert_receipt()
-        print("Sleeping...")
-        time.sleep(poll_interval)
+    context["fixture"].assert_receipt(context["orchestration_instance_id"])
