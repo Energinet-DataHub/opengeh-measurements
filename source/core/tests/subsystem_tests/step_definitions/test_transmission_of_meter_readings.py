465ce82c9957ae35db914929a35a13ca549498f2
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-19 14:42:43 +0100

    Update test_transmission_of_meter_readings.py

--- source/core/tests/subsystem_tests/step_definitions/test_transmission_of_meter_readings.py
+++ source/core/tests/subsystem_tests/step_definitions/test_transmission_of_meter_readings.py
@@ -1,11 +1,8 @@
-from typing import Any
-
 import pytest
 from pyspark.sql import SparkSession
 from pytest_bdd import given, scenarios, then, when
 
 import tests.helpers.identifier_helper as identifier_helper
-from tests.conftest import Generator
 from tests.helpers.builders.submitted_transactions_builder import ValueBuilder
 from tests.subsystem_tests.fixtures.core_fixture import CoreFixture
 from tests.subsystem_tests.settings.databricks_settings import DatabricksSettings
@@ -14,26 +11,29 @@ scenarios("../features/transmission_of_meter_readings.feature")
 
 
 @pytest.fixture
-def context() -> Generator[dict, Any, None]:
-    ctx: dict[str, Any] = {}
+def core_fixture() -> CoreFixture:
     databricks_settings = DatabricksSettings()  # type: ignore
-    ctx["fixture"] = CoreFixture(databricks_settings)
-    yield ctx
+    return CoreFixture(databricks_settings)
 
 
-@given("a valid measurement transaction")
-def _(spark: SparkSession, context: dict[str, Any]) -> None:
-    context["orchestration_instance_id"] = identifier_helper.generate_random_string()
-    value = ValueBuilder(spark).add_row(orchestration_instance_id=context["orchestration_instance_id"]).build()
-    context["value"] = value
+class TestData:
+    def __init__(self, orchestration_instance_id: str, value: str) -> None:
+        self.orchestration_instance_id = orchestration_instance_id
+        self.value = value
+
+
+@given("a valid measurement transaction", target_fixture="test_data")
+def _(spark: SparkSession) -> TestData:
+    orchestration_instance_id = identifier_helper.generate_random_string()
+    value = ValueBuilder(spark).add_row(orchestration_instance_id=orchestration_instance_id).build()
+    return TestData(orchestration_instance_id, value)
 
 
 @when("the measurement transaction is enqueued in the Event Hub")
-def _(context: dict[str, Any]) -> None:
-    context["fixture"].send_submitted_transactions_event(context["value"])
+def _(core_fixture: CoreFixture, test_data: TestData) -> None:
+    core_fixture.send_submitted_transactions_event(test_data.value)
 
 
 @then("an acknowledgement is sent to the Event Hub")
-def _(context: dict[str, Any]) -> None:
-    pass
-    context["fixture"].assert_receipt(context["orchestration_instance_id"])
+def _(core_fixture: CoreFixture, test_data: TestData) -> None:
+    core_fixture.assert_receipt(test_data.orchestration_instance_id)
