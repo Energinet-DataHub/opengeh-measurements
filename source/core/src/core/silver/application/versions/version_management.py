c6728707f336df59f6748b0cd9ea69db2b2d952c
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-20 14:11:44 +0100

    remove versionmanagement

--- source/core/src/core/silver/application/versions/version_management.py
+++ source/core/src/core/silver/application/versions/version_management.py
@@ -2,20 +2,6 @@ import core.bronze.application.submitted_transactions.submitted_transactions_han
 import core.silver.infrastructure.protobuf.persist_submitted_transaction as persist_submitted_transaction
 
 
-class VersionManagement:
-    class Version:
-        def __init__(self, version: int, unpack, transformation):
-            self.version = version
-            self.unpack = unpack
-            self.transformation = transformation
-
-    versions = [
-        Version(
-            1, persist_submitted_transaction.unpack, submitted_transactions_handler.handle_valid_submitted_transactions
-        )
-    ]
-
-
 class ProtobufManagement:
     class ProtobufMessage:
         def __init__(self, version: int, unpack, transformation):
