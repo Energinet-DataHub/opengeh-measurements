bc76fbac673d84fae32be4c85810dd550eee791f
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-03-20 12:52:42 +0000

    Added seed to net consumption

--- source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/test_jobs.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/test_jobs.py
@@ -1,20 +1,33 @@
+import random
 import uuid
+from datetime import datetime
 
 import pytest
+from geh_common.domain.types import MeteringPointType
 
 from tests.subsystem_tests.base_resources.base_job_fixture import BaseJobFixture
 from tests.subsystem_tests.base_resources.base_job_tests import BaseJobTests
 from tests.subsystem_tests.environment_configuration import EnvironmentConfiguration
+from tests.subsystem_tests.seed_gold_table import GoldTableRow, GoldTableSeeder
 
 fixture = None
 
 job_parameters = {"orchestration-instance-id": uuid.uuid4()}
+
+gold_table_row = GoldTableRow(
+    metering_point_id="150000050000000201",
+    observation_time=datetime(2024, 11, 30, 23, 0, 0),
+    quantity=random.uniform(0.1, 10.0),
+    metering_point_type=MeteringPointType.NET_CONSUMPTION,
+)
 
 
 @pytest.fixture(scope="session")
 def setup_fixture(
     environment_configuration: EnvironmentConfiguration,
 ) -> BaseJobFixture:
+    table_seeder = GoldTableSeeder(environment_configuration)
+    table_seeder.seed(gold_table_row)
     return BaseJobFixture(
         environment_configuration=environment_configuration,
         job_name="NetConsumptionGroup6",
