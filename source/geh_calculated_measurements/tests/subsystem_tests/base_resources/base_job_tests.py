4c916d944ad35ff4a96b62ccae39cedf26512188
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-03-20 09:19:37 +0000

    Added  orchestration-instance-id to query to make sure it is data from this run

--- source/geh_calculated_measurements/tests/subsystem_tests/base_resources/base_job_tests.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/base_resources/base_job_tests.py
@@ -60,7 +60,7 @@ class BaseJobTests:
         database = CalculatedMeasurementsInternalDatabaseDefinition.DATABASE_NAME
         table = "calculated_measurements"
         statement = f"""
-            SELECT * FROM {catalog}.{database}.{table} LIMIT 1
+            SELECT * FROM {catalog}.{database}.{table} WHERE orchestration_instance_id = {setup_fixture.job_parameters.get("orchestration-instance-id")} LIMIT 1
             """
 
         # Act
