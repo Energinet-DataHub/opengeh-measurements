73973b5c610f9cfb3da6863d92070299fa4a0a2d
Jens Peder Meldgaard <jpm@halfspace.io>
2025-03-20 16:28:30 +0100

    chore: add logging to migration start
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migrations_runner.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migrations_runner.py
@@ -2,6 +2,9 @@ from geh_common.migrations import (
     SparkSqlMigrationsConfiguration,
     migration_pipeline,
 )
+from geh_common.telemetry.decorators import start_trace
+from geh_common.telemetry.logger import Logger
+from geh_common.telemetry.logging_configuration import LoggingSettings, configure_logging
 
 import geh_calculated_measurements.database_migrations.substitutions as substitutions
 from geh_calculated_measurements.database_migrations.database_definitions import (
@@ -10,19 +13,23 @@ from geh_calculated_measurements.database_migrations.database_definitions import
 from geh_calculated_measurements.database_migrations.settings.catalog_settings import CatalogSettings
 
 
+@start_trace()
 def migrate() -> None:
-    spark_sql_migrations_configuration = _configure_spark_sql_migrations()
-    migration_pipeline.migrate(spark_sql_migrations_configuration)
+    log = Logger(__name__)
+    log_settings = LoggingSettings(subsystem="measurements", cloud_role_name="dbr-calculated-measurements")
+    configure_logging(logging_settings=log_settings)
 
-
-def _configure_spark_sql_migrations() -> SparkSqlMigrationsConfiguration:
     substitution_variables = substitutions.substitutions()
-    catalog_name = CatalogSettings().catalog_name  # type: ignore
+    catalog_settings = CatalogSettings()
+    catalog_name = catalog_settings.catalog_name  # type: ignore
 
-    return SparkSqlMigrationsConfiguration(
+    log.info(f"Initializing migrations with CatalogSettings: {catalog_settings}")
+    log.info(f"Initializing migrations with LoggingSettings: {log_settings}")
+    spark_sql_migrations_configuration = SparkSqlMigrationsConfiguration(
         migration_schema_name=MeasurementsCalculatedInternalDatabaseDefinition.measurements_calculated_internal_database,
         migration_table_name=MeasurementsCalculatedInternalDatabaseDefinition.executed_migrations_table_name,
         migration_scripts_folder_path="geh_calculated_measurements.database_migrations.migration_scripts",
         substitution_variables=substitution_variables,
         catalog_name=catalog_name,
     )
+    migration_pipeline.migrate(spark_sql_migrations_configuration)
