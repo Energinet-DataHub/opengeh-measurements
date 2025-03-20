756b447cc81e19c5d9513bec253d7344423e2830
jesper knudsen <jmk@halfspace.io>
2025-03-20 13:49:01 +0000

    stash

--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/calculation.py
@@ -1,30 +1,29 @@
 from typing import Tuple
 
 from geh_common.telemetry import use_span
-from pyspark.pandas import DataFrame
+from pyspark.sql import DataFrame
 
+from geh_calculated_measurements.capacity_settlement.domain import TimeSeriesPoints
 from geh_calculated_measurements.common.domain import CalculatedMeasurements
+from geh_calculated_measurements.electrical_heating.domain import ChildMeteringPoints, ConsumptionMeteringPointPeriods
 from geh_calculated_measurements.net_consumption_group_6.domain.cenc import Cenc, calculate_cenc
 from geh_calculated_measurements.net_consumption_group_6.domain.daily import calculate_daily
 
 
 @use_span()
 def execute(
+    time_series_points: TimeSeriesPoints,
+    consumption_metering_point_periods: ConsumptionMeteringPointPeriods,
+    child_metering_points: ChildMeteringPoints,
     internal_daily: DataFrame,
 ) -> Tuple[Cenc, CalculatedMeasurements]:
     orchestration_instance_id = "00000000-0000-0000-0000-000000000001"
-    calculation_day = 1
-    calculation_month = 1
-    calculation_year = 2025
     time_zone = "Europe/Copenhagen"
     cenc = calculate_cenc()
+    print("In execute: Type of cenc: ", type(cenc))
     measurements = calculate_daily(
-        internal_daily,
-        cenc,
-        orchestration_instance_id,
-        calculation_day,
-        calculation_month,
-        calculation_year,
-        time_zone,
+        cenc=cenc,
+        internal_daily=internal_daily,
+        time_zone=time_zone,
     )
     return (cenc, measurements)
--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/daily.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/daily.py
@@ -1,5 +1,5 @@
 import pyspark.sql.functions as F
-from geh_common.pyspark.transformations import convert_to_utc
+from geh_common.pyspark.transformations import convert_from_utc, convert_to_utc
 from geh_common.telemetry import use_span
 from geh_common.testing.dataframes import testing
 from pyspark.sql import Column, DataFrame
@@ -9,11 +9,12 @@ from geh_calculated_measurements.common.domain import CalculatedMeasurements
 from geh_calculated_measurements.net_consumption_group_6.domain.cenc import Cenc
 
 
-def days_in_year(year: int) -> Column:
-    # Create date for January 1st of the given year
-    start_date = F.lit(f"{year}-01-01").cast(T.DateType())
+def days_in_year(year: Column) -> Column:
+    # Create date for January 1st of the given year (for Column input)
+    start_date = F.to_date(F.concat(year.cast(T.StringType()), F.lit("-01-01")))
     # Create date for January 1st of the next year
-    end_date = F.lit(f"{year + 1}-01-01").cast(T.DateType())
+    end_date = F.to_date(F.concat((year + 1).cast(T.StringType()), F.lit("-01-01")))
+
     # Calculate the difference in days
     return F.datediff(end_date, start_date)
 
@@ -22,22 +23,27 @@ def days_in_year(year: int) -> Column:
 @testing()
 def calculate_daily(
     cenc: Cenc,
-    calculation_year: int,
     time_zone: str,
     internal_daily: DataFrame,
 ) -> CalculatedMeasurements:
     # adding needed columns
     df = cenc.df.select(
         "*",
-        days_in_year(calculation_year).alias("days_in_year"),
         F.lit("net_consumption").alias("orchestration_type"),
         F.lit("net_consumption").alias("metering_point_type"),
         F.lit("ignore").alias("transaction_id"),
         F.lit("2024-01-31T23:00:00Z").alias("transaction_creation_datetime").cast(T.TimestampType()),
-        F.col("quantity").cast(T.DecimalType(18, 3)),
+        F.current_timestamp().alias("now"),
     )
 
+    print("Schema of df:")
+    df.printSchema()
+
+    print("added columns:")
     df.show()
+
+    df = convert_from_utc(df, time_zone)
+    internal_daily = convert_from_utc(internal_daily, time_zone)
 
     # selecting needed columns
     df = df.select(
@@ -47,21 +53,35 @@ def calculate_daily(
         F.col("orchestration_instance_id"),
         F.col("metering_point_id"),
         F.col("metering_point_type"),
-        (F.col("quantity") / F.col("days_in_year")).alias("quantity").cast(T.DecimalType(18, 3)),
-        F.current_timestamp().alias("now"),
+        (F.col("quantity") / days_in_year(F.year(F.col("now"))).cast(T.DecimalType(18, 3)))
+        .alias("quantity")
+        .cast(T.DecimalType(18, 3)),
+        F.col("now"),
     )
+
+    print("df after selecting needed columns:")
+    df.show()
+
+    print("internal_daily:")
+    internal_daily.show()
 
     # merging the with internal
     df = df.join(
         internal_daily,
         on=["metering_point_id"],
         how="inner",
-    ).select(
-        "*",
-        F.explode(F.sequence(F.col("last_run"), F.col("now"), F.expr("INTERVAL 1 DAY"))).alias("date"),
     )
 
-    ## todo: patch F.current_timestamp
+    print("df after joining with internal_daily:")
+    df.show()
+
+    df = df.select(
+        "*",
+        F.explode(F.sequence(F.date_add(F.col("last_run"), 1), F.col("now"), F.expr("INTERVAL 1 DAY"))).alias("date"),
+    )
+
+    print("result df:")
+    df.show()
 
     result_df = convert_to_utc(df, time_zone)
 
