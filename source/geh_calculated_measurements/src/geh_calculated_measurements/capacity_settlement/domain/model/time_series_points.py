ca8d44e87129953ad362b2dccdc8852ed26cf8ae
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2025-03-21 09:58:10 +0100

    Refactor measurement schemas and imports (#472)

--- source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/domain/model/time_series_points.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/domain/model/time_series_points.py
@@ -13,28 +13,27 @@ class TimeSeriesPoints(DataFrameWrapper):
     def __init__(self, df: DataFrame):
         super().__init__(
             df,
-            time_series_points_v1,
+            TimeSeriesPoints.schema,
             # We ignore_nullability because it has turned out to be too hard and even possibly
             # introducing more errors than solving in order to stay in exact sync with the
             # logically correct schema.
             ignore_nullability=True,
         )
 
-
-time_series_points_v1 = T.StructType(
-    [
-        T.StructField(ContractColumnNames.metering_point_id, T.StringType(), not nullable),
-        #
-        # 'consumption' | 'capacity_settlement'
-        T.StructField(ContractColumnNames.metering_point_type, T.StringType(), not nullable),
-        #
-        # UTC time
-        T.StructField(
-            ContractColumnNames.observation_time,
-            T.TimestampType(),
-            not nullable,
-        ),
-        #
-        T.StructField(ContractColumnNames.quantity, T.DecimalType(18, 3), not nullable),
-    ]
-)
+    schema = T.StructType(
+        [
+            T.StructField(ContractColumnNames.metering_point_id, T.StringType(), not nullable),
+            #
+            # 'consumption' | 'capacity_settlement'
+            T.StructField(ContractColumnNames.metering_point_type, T.StringType(), not nullable),
+            #
+            # UTC time
+            T.StructField(
+                ContractColumnNames.observation_time,
+                T.TimestampType(),
+                not nullable,
+            ),
+            #
+            T.StructField(ContractColumnNames.quantity, T.DecimalType(18, 3), not nullable),
+        ]
+    )
