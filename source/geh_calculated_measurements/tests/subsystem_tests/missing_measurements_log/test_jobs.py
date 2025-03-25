5f87e0a8af3b06d2736bf7aef4933fe7c152feda
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-25 11:54:28 +0100

    Update test_jobs.py

--- source/geh_calculated_measurements/tests/subsystem_tests/missing_measurements_log/test_jobs.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/missing_measurements_log/test_jobs.py
@@ -2,68 +2,65 @@ import uuid
 from datetime import datetime, timedelta
 
 import pytest
+from geh_common.databricks import DatabricksApiClient
+from geh_common.domain.types import MeteringPointResolution
 from geh_common.domain.types.quantity_quality import QuantityQuality
 
 from tests.subsystem_tests.base_resources.base_job_fixture import BaseJobFixture
 from tests.subsystem_tests.base_resources.base_job_tests import BaseJobTests
 from tests.subsystem_tests.environment_configuration import EnvironmentConfiguration
-from tests.subsystem_tests.missing_measurements_log.seed_metering_point_periods import (
-    MeteringPointPeriodsRow,
-    MeteringPointPeriodsTableSeeder,
-)
 from tests.subsystem_tests.seed_gold_table import GoldTableRow, GoldTableSeeder
 
-METERING_POINT_ID = "170000060000000201"
-DEFAULT_PERIOD_START = datetime(2025, 1, 1, 23, 0, 0)
-DEFAULT_PERIOD_END = datetime(2025, 1, 2, 23, 0, 0)
-FIRST_OBSERVATION_TIME = DEFAULT_PERIOD_START
-LAST_OBSERVATION_TIME = FIRST_OBSERVATION_TIME - timedelta(hours=23)
+_METERING_POINT_ID = "170000060000000201"
+_DEFAULT_GRID_AREA_CODE = "804"
+_DEFAULT_PERIOD_START = datetime(2025, 1, 1, 23, 0, 0)
+_DEFAULT_PERIOD_END = datetime(2025, 1, 2, 23, 0, 0)
+_FIRST_OBSERVATION_TIME = _DEFAULT_PERIOD_START
+
+METERING_POINT_PERIODS_STATEMENT = f"""
+        INSERT INTO electricity_market_measurements_input.missing_measurements_log_metering_point_periods_v1 (
+            metering_point_id,
+            grid_area_code,
+            resolution,
+            period_from_date,
+            period_to_date,
+        )
+        VALUES
+        "('{_METERING_POINT_ID}',{_DEFAULT_GRID_AREA_CODE},{MeteringPointResolution.HOUR.value},'{_DEFAULT_PERIOD_START.strftime("%Y-%m-%d %H:%M:%S")}','{_DEFAULT_PERIOD_END.strftime("%Y-%m-%d %H:%M:%S")}',"
+    """
+
 
 job_parameters = {
     "orchestration-instance-id": uuid.uuid4(),
-    "period-start-datetime": DEFAULT_PERIOD_START.strftime("%Y-%m-%dT%H:%M:%SZ"),
-    "period-end-datetime": DEFAULT_PERIOD_END.strftime("%Y-%m-%dT%H:%M:%SZ"),
+    "period-start-datetime": _DEFAULT_PERIOD_START.strftime("%Y-%m-%dT%H:%M:%SZ"),
+    "period-end-datetime": _DEFAULT_PERIOD_END.strftime("%Y-%m-%dT%H:%M:%SZ"),
 }
 
 
 def _get_gold_table_rows() -> list[GoldTableRow]:
     return [
         GoldTableRow(
-            metering_point_id=METERING_POINT_ID,
-            observation_time=FIRST_OBSERVATION_TIME + timedelta(hours=i),
+            metering_point_id=_METERING_POINT_ID,
+            observation_time=_FIRST_OBSERVATION_TIME + timedelta(hours=i),
             quality=QuantityQuality.MEASURED.value,
         )
         for i in range(24)
     ]
 
 
-def seed_test_data() -> None:
+def seed_test_data(environment_configuration: EnvironmentConfiguration) -> None:
+    databricks_api_client = DatabricksApiClient(
+        environment_configuration.databricks_token,
+        environment_configuration.workspace_url,
+    )
+    databricks_api_client.execute_statement(
+        warehouse_id=environment_configuration.warehouse_id, statement=METERING_POINT_PERIODS_STATEMENT
+    )
+
     # measurements
     table_seeder = GoldTableSeeder(EnvironmentConfiguration())
     gold_table_rows = _get_gold_table_rows()
     table_seeder.seed(gold_table_rows)
-
-    # metering point periods
-    metering_point_periods_row = MeteringPointPeriodsRow(
-        metering_point_id=METERING_POINT_ID,
-        grid_area_code="804",
-        period_from_date=DEFAULT_PERIOD_START,
-        period_to_date=DEFAULT_PERIOD_END,
-    )
-    metering_point_periods_table_seeder = MeteringPointPeriodsTableSeeder(EnvironmentConfiguration())
-    metering_point_periods_table_seeder.seed(metering_point_periods_row)
-
-
-@pytest.fixture(scope="session")
-def job_fixture(
-    environment_configuration: EnvironmentConfiguration,
-) -> BaseJobFixture:
-    seed_test_data()
-    return BaseJobFixture(
-        environment_configuration=environment_configuration,
-        job_name="MissingMeasurementsLog",
-        job_parameters=job_parameters,
-    )
 
 
 class TestMissingMeasurementsLog(BaseJobTests):
