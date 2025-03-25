b4a4cc4a65dbbb180077bbf0f700a6dc11e5c496
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-25 12:52:16 +0100

    ....

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/electricity_market/repository.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/electricity_market/repository.py
@@ -2,7 +2,6 @@ from geh_common.pyspark.read_csv import read_csv_path
 from pyspark.sql import SparkSession
 
 from geh_calculated_measurements.electrical_heating.domain import (
-    ChildMeteringPoints,
     ConsumptionMeteringPointPeriods,
 )
 
@@ -10,22 +9,14 @@ from geh_calculated_measurements.electrical_heating.domain import (
 class Repository:
     def __init__(
         self,
-        spark: SparkSession,
-        electricity_market_data_path: str,
-        consumption_metering_point_periods_file_name: str = "consumption_metering_point_periods_v1.csv",
-        child_metering_points_file_name: str = "child_metering_points_v1.csv",
+        spark: SparkSession,,
     ) -> None:
         self._spark = spark
         self._electricity_market_data_path = electricity_market_data_path
         self._consumption_metering_point_periods_file_name = consumption_metering_point_periods_file_name
         self._child_metering_points_file_name = child_metering_points_file_name
 
-    def read_consumption_metering_point_periods(self) -> ConsumptionMeteringPointPeriods:
+    def read_metering_point_periods(self) -> ConsumptionMeteringPointPeriods:
         file_path = f"{self._electricity_market_data_path}/{self._consumption_metering_point_periods_file_name}"
         df = read_csv_path(spark=self._spark, path=file_path, schema=ConsumptionMeteringPointPeriods.schema)
-        return ConsumptionMeteringPointPeriods(df)
-
-    def read_child_metering_points(self) -> ChildMeteringPoints:
-        file_path = f"{self._electricity_market_data_path}/{self._child_metering_points_file_name}"
-        df = read_csv_path(spark=self._spark, path=file_path, schema=ChildMeteringPoints.schema)
-        return ChildMeteringPoints(df)
+        return MeteringPointPeriods(df)
---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/infrastructure/electricity_market/database_definitions.py
@@ -0,0 +1,3 @@
+class MeteringPointPeriodsDatabaseDefinition:
+    DATABASE_NAME = "electricity_market_measurements_input_v1"
+    METERING_POINT_PERIODS = "metering_point_periods_v1"
