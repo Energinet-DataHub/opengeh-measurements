1d7f7603b3af6394efda59919f7c08692c42dc87
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-26 15:35:11 +0100

    ....

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/__init__.py
@@ -2,12 +2,18 @@ from geh_calculated_measurements.common.infrastructure.calculated_measurements.d
     CalculatedMeasurementsDatabaseDefinition,
     CalculatedMeasurementsInternalDatabaseDefinition,
 )
-from geh_calculated_measurements.common.infrastructure.repository import Repository as CalculatedMeasurementsRepository
+from geh_calculated_measurements.common.infrastructure.calculated_measurements.repository import (
+    Repository as CalculatedMeasurementsRepository,
+)
+from geh_calculated_measurements.common.infrastructure.current_measurements.current_measurements.repository import (
+    Repository as CurrentMeasurementsRepository,
+)
 from geh_calculated_measurements.common.infrastructure.spark_initializor import initialize_spark
 
 __all__ = [
     "CalculatedMeasurementsInternalDatabaseDefinition",
     "CalculatedMeasurementsDatabaseDefinition",
     "CalculatedMeasurementsRepository",
+    "CurrentMeasurementsRepository",
     "initialize_spark",
 ]
--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
@@ -1,20 +1,27 @@
 from geh_common.telemetry.decorators import use_span
 from pyspark.sql import SparkSession
 
+from geh_calculated_measurements.common.infrastructure import (
+    Repository as CurrentMeasurementsRepository,
+)
 from geh_calculated_measurements.missing_measurements_log.application.missing_measurements_log_args import (
     MissingMeasurementsLogArgs,
 )
 from geh_calculated_measurements.missing_measurements_log.domain import (
     execute,
 )
-from geh_calculated_measurements.missing_measurements_log.infrastructure import Repository
+from geh_calculated_measurements.missing_measurements_log.infrastructure import (
+    Repository as MeteringPointPeriodsRepository,
+)
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: MissingMeasurementsLogArgs) -> None:
     # Create repositories to obtain data frames
-    electricity_market_repository = Repository(spark, args.catalog_name)
+    metering_point_periods_repository = MeteringPointPeriodsRepository(spark, args.catalog_name)
+    measurements_repository = CurrentMeasurementsRepository(spark, args.catalog_name)
 
     # Read data frames
-    metering_point_periods = electricity_market_repository.read_metering_point_periods()
-    execute(metering_point_periods)
+    metering_point_periods = metering_point_periods_repository.read_metering_point_periods()
+    measurements = measurements_repository.read_current_measurements()
+    execute(metering_point_periods, measurements)
