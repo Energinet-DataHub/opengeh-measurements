d52a416f1ff6e294c7b0cde21d4d0d032bedd7ec
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-03-26 15:37:42 +0100

    ...

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/__init__.py
@@ -5,7 +5,7 @@ from geh_calculated_measurements.common.infrastructure.calculated_measurements.d
 from geh_calculated_measurements.common.infrastructure.calculated_measurements.repository import (
     Repository as CalculatedMeasurementsRepository,
 )
-from geh_calculated_measurements.common.infrastructure.current_measurements.current_measurements.repository import (
+from geh_calculated_measurements.common.infrastructure.current_measurements.repository import (
     Repository as CurrentMeasurementsRepository,
 )
 from geh_calculated_measurements.common.infrastructure.spark_initializor import initialize_spark
--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
@@ -2,7 +2,7 @@ from geh_common.telemetry.decorators import use_span
 from pyspark.sql import SparkSession
 
 from geh_calculated_measurements.common.infrastructure import (
-    Repository as CurrentMeasurementsRepository,
+    CurrentMeasurementsRepository,
 )
 from geh_calculated_measurements.missing_measurements_log.application.missing_measurements_log_args import (
     MissingMeasurementsLogArgs,
