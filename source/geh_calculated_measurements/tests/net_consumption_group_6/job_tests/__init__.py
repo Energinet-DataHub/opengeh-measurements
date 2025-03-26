8f5fc26a1ca9aca6c9d911ea96df232fb0efff8d
Jonas Michelsen <jvm@energinet.dk>
2025-03-26 15:45:31 +0100

    fix job path

--- source/geh_calculated_measurements/tests/net_consumption_group_6/job_tests/__init__.py
+++ source/geh_calculated_measurements/tests/net_consumption_group_6/job_tests/__init__.py
@@ -1,16 +1,5 @@
 from tests import TESTS_ROOT
 
-TEST_FILES_FOLDER_PATH = (TESTS_ROOT / "net_consumption_group_6" / "job_tests" / "test_files").as_posix()
-
 
 def get_test_files_folder_path() -> str:
-    return TEST_FILES_FOLDER_PATH
-
-
-def create_job_environment_variables() -> dict[str, str]:
-    return {
-        "CATALOG_NAME": "spark_catalog",
-        "TIME_ZONE": "Europe/Copenhagen",
-        "ELECTRICITY_MARKET_DATA_PATH": TEST_FILES_FOLDER_PATH,
-        "APPLICATIONINSIGHTS_CONNECTION_STRING": "some-connection-string",
-    }
+    return (TESTS_ROOT / "net_consumption_group_6" / "job_tests" / "test_files").as_posix()
