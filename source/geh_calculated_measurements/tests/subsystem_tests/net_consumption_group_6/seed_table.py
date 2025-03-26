6d3028b9150a1ad1328c83f2ee5dfd70e4c3cadb
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-03-26 08:49:08 +0000

    Made fixture into generator and made list of queries

--- source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/seed_table.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/seed_table.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-from tests.subsystem_tests.base_resources.base_job_fixture import BaseJobFixture
-
 database = "electricity_market_measurements_input"
 parent_table = "net_consumption_group_6_consumption_metering_point_periods_v1"
 child_table = "net_consumption_group_6_child_metering_point_periods_v1"
@@ -10,33 +8,33 @@ parent_metering_point_id = "170000000000000201"
 child_metering_point_id = "150000001500170200"
 
 
-def _delete_seeded_data(
-    job_fixture: BaseJobFixture,
+def delete_seeded_data(
+    job_fixture,
 ) -> None:
+    statements = []
     # PARENT
-    statement = f"""
+    statements.append(f"""
         DELETE FROM {job_fixture.environment_configuration.catalog_name}.{database}.{parent_table} 
         WHERE metering_point_id = '{parent_metering_point_id}'
-    """
-    job_fixture.databricks_api_client.execute_statement(
-        warehouse_id=job_fixture.environment_configuration.warehouse_id, statement=statement
-    )
-
+    """)
     # CHILD
-    statement = f"""
+    statements.append(f"""
         DELETE FROM {job_fixture.environment_configuration.catalog_name}.{database}.{child_table} 
         WHERE parent_metering_point_id = '{parent_metering_point_id}'
-    """
-    job_fixture.databricks_api_client.execute_statement(
-        warehouse_id=job_fixture.environment_configuration.warehouse_id, statement=statement
-    )
+    """)
+
+    for statement in statements:
+        job_fixture.databricks_api_client.execute_statement(
+            warehouse_id=job_fixture.environment_configuration.warehouse_id, statement=statement
+        )
 
 
-def _seed_table(
-    job_fixture: BaseJobFixture,
+def seed_table(
+    job_fixture,
 ) -> None:
+    statements = []
     # PARENT
-    statement = f"""
+    statements.append(f"""
     INSERT INTO {job_fixture.environment_configuration.catalog_name}.{database}.{parent_table} (
         metering_point_id,
         has_electrical_heating,
@@ -53,13 +51,9 @@ def _seed_table(
         '{datetime(2023, 12, 31, 23, 0, 0).strftime("%Y-%m-%d %H:%M:%S")}',
         {False}
     )
-    """
-    job_fixture.databricks_api_client.execute_statement(
-        warehouse_id=job_fixture.environment_configuration.warehouse_id, statement=statement
-    )
-
+    """)
     # CHILD
-    statement = f"""
+    statements.append(f"""
     INSERT INTO {job_fixture.environment_configuration.catalog_name}.{database}.{child_table} (
         metering_point_id,
         metering_type,
@@ -74,7 +68,9 @@ def _seed_table(
         '{datetime(2022, 12, 31, 23, 0, 0)}',
         '{datetime(2023, 12, 31, 23, 0, 0)}'
     )
-    """
-    job_fixture.databricks_api_client.execute_statement(
-        warehouse_id=job_fixture.environment_configuration.warehouse_id, statement=statement
-    )
+    """)
+
+    for statement in statements:
+        job_fixture.databricks_api_client.execute_statement(
+            warehouse_id=job_fixture.environment_configuration.warehouse_id, statement=statement
+        )
--- source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/test_jobs.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/test_jobs.py
@@ -1,11 +1,12 @@
 import uuid
+from typing import Any, Generator
 
 import pytest
 
 from tests.subsystem_tests.base_resources.base_job_fixture import BaseJobFixture
 from tests.subsystem_tests.base_resources.base_job_tests import BaseJobTests
 from tests.subsystem_tests.environment_configuration import EnvironmentConfiguration
-from tests.subsystem_tests.net_consumption_group_6.seed_table import _delete_seeded_data, _seed_table
+from tests.subsystem_tests.net_consumption_group_6.seed_table import delete_seeded_data, seed_table
 
 job_parameters = {"orchestration-instance-id": uuid.uuid4()}
 
@@ -13,7 +14,7 @@ job_parameters = {"orchestration-instance-id": uuid.uuid4()}
 @pytest.fixture(scope="session")
 def job_fixture(
     environment_configuration: EnvironmentConfiguration,
-) -> BaseJobFixture:
+) -> Generator[BaseJobFixture, Any, None]:
     # Construct fixture
     base_job_fixture = BaseJobFixture(
         environment_configuration=environment_configuration,
@@ -22,12 +23,15 @@ def job_fixture(
     )
 
     # Remove previously inserted seeded data
-    _delete_seeded_data(job_fixture=base_job_fixture)
+    delete_seeded_data(base_job_fixture)
 
     # Insert seeded data
-    _seed_table(job_fixture=base_job_fixture)
+    seed_table(base_job_fixture)
 
-    return base_job_fixture
+    yield base_job_fixture
+
+    # Remove previously inserted seeded data
+    delete_seeded_data(base_job_fixture)
 
 
 class TestNetConsumptionGroup6(BaseJobTests):
@@ -38,7 +42,3 @@ class TestNetConsumptionGroup6(BaseJobTests):
     @pytest.mark.skip(reason="Skipped due to issues with the telemetry data not available in the logs.")
     def test__and_then_job_telemetry_is_created(self, job_fixture: BaseJobFixture) -> None:
         pass
-
-    @pytest.mark.order("last")
-    def test__remove_seeded_data(self, job_fixture: BaseJobFixture) -> None:
-        _delete_seeded_data(job_fixture=job_fixture)
