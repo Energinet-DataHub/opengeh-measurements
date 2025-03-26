d89e639a1556687fa713707b452e4ecde2aa06be
Jens Peder Meldgaard <jpm@halfspace.io>
2025-03-26 11:50:44 +0100

    chore: add logging to migration start (#470)
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/entry_point.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/entry_point.py
@@ -1,6 +1,15 @@
+from geh_common.telemetry.logger import Logger
+from geh_common.telemetry.logging_configuration import LoggingSettings, configure_logging
+
 import geh_calculated_measurements.database_migrations.migrations_runner as migrations_runner
 
 
 def migrate() -> None:
     """Entry point for the database migrations."""
+    log_settings = LoggingSettings(subsystem="measurements", cloud_role_name="dbr-calculated-measurements")
+    configure_logging(logging_settings=log_settings)
+
+    log = Logger(__name__)
+    log.info(f"Initializing migrations with:\nLogging Settings: {log_settings}")
+
     migrations_runner.migrate()
