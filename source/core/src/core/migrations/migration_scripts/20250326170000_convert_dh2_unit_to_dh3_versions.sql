67029695d37207636ebe8e2f054c2ff7a11d6806
Svend (SLB) <slb@halfspace.io>
2025-03-27 09:30:55 +0100

    Reintroduced the missing ones

--- source/core/src/core/migrations/migration_scripts/20250326170000_convert_dh2_unit_to_dh3_versions.sql
+++ source/core/src/core/migrations/migration_scripts/20250326170000_convert_dh2_unit_to_dh3_versions.sql
@@ -1,12 +1,20 @@
 UPDATE {silver_database}.{silver_measurements_table}
 SET unit = CASE 
-        WHEN unit == 'T'     THEN 'Tonne'
+        WHEN unit == 'KWH'   THEN 'kWh'
+        WHEN unit == 'MWH'   THEN 'MWh'
+        WHEN unit == 'KVARH' THEN 'kVArh'
+        WHEN unit == 'KW'    THEN 'kW'
+        WHEN unit == 'T'     THEN 'Tonne' 
         ELSE unit
     END
 GO
 
 UPDATE {gold_database}.{gold_measurements}
 SET unit = CASE 
-        WHEN unit == 'T'     THEN 'Tonne'
+        WHEN unit == 'KWH'   THEN 'kWh'
+        WHEN unit == 'MWH'   THEN 'MWh'
+        WHEN unit == 'KVARH' THEN 'kVArh'
+        WHEN unit == 'KW'    THEN 'kW'
+        WHEN unit == 'T'     THEN 'Tonne' 
         ELSE unit 
     END
