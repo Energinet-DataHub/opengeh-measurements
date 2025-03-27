10484f05ba7d0c18d2e29b93aa8e56335a5eae6a
Svend (SLB) <slb@halfspace.io>
2025-03-27 09:20:19 +0100

    Fixed casing in mig script

--- source/core/src/core/migrations/migration_scripts/20250326170000_convert_dh2_unit_to_dh3_versions.sql
+++ source/core/src/core/migrations/migration_scripts/20250326170000_convert_dh2_unit_to_dh3_versions.sql
@@ -1,12 +1,12 @@
 UPDATE {silver_database}.{silver_measurements_table}
 SET unit = CASE 
-        WHEN unit == 'T'     THEN 'TONNE'
+        WHEN unit == 'T'     THEN 'Tonne'
         ELSE unit
     END
 GO
 
 UPDATE {gold_database}.{gold_measurements}
 SET unit = CASE 
-        WHEN unit == 'T'     THEN 'TONNE'
+        WHEN unit == 'T'     THEN 'Tonne'
         ELSE unit 
     END
