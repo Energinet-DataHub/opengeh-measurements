ffa2eaa6efdca75ec141b2eab8b39b50d96685c8
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-27 13:17:09 +0100

    fix

--- source/core/tests/integration/silver/application/streams/test_submitted_transactions.py
+++ source/core/tests/integration/silver/application/streams/test_submitted_transactions.py
@@ -5,7 +5,9 @@ import tests.helpers.table_helper as table_helper
 from core.bronze.infrastructure.config.table_names import TableNames as BronzeTableNames
 from core.contracts.process_manager.enums.metering_point_type import MeteringPointType
 from core.settings.bronze_settings import BronzeSettings
+from core.settings.silver_settings import SilverSettings
 from core.silver.application.streams import submitted_transactions as sut
+from core.silver.infrastructure.config import SilverTableNames
 from tests.helpers.builders.submitted_transactions_builder import SubmittedTransactionsBuilder, ValueBuilder
 
 
@@ -38,3 +40,30 @@ def test__stream_submitted_transactions__when_invalid_should_save_in_bronze_subm
         f"{bronze_settings.bronze_database_name}.{BronzeTableNames.bronze_submitted_transactions_quarantined}"
     ).where(f"orchestration_instance_id = '{expected_orchestration_id}'")
     assert bronze_submitted_transactions_quarantined_table.count() == 1
+
+
+def test__submitted_transactions__should_save_in_silver_measurements(
+    mock_checkpoint_path,
+    spark: SparkSession,
+    migrations_executed,
+) -> None:
+    # Arrange
+    bronze_settings = BronzeSettings()
+    silver_settings = SilverSettings()
+    expected_orchestration_id = identifier_helper.generate_random_string()
+    value = ValueBuilder(spark).add_row(orchestration_instance_id=expected_orchestration_id).build()
+    submitted_transactions = SubmittedTransactionsBuilder(spark).add_row(value=value).build()
+    table_helper.append_to_table(
+        submitted_transactions,
+        bronze_settings.bronze_database_name,
+        BronzeTableNames.bronze_submitted_transactions_table,
+    )
+
+    # Act
+    sut.stream_submitted_transactions()
+
+    # Assert
+    silver_table = spark.table(f"{silver_settings.silver_database_name}.{SilverTableNames.silver_measurements}").where(
+        f"orchestration_instance_id = '{expected_orchestration_id}'"
+    )
+    assert silver_table.count() == 1
--- source/core/tests/unit_tests/silver/application/streams/test_submitted_transactions.py
+++ source/core/tests/unit_tests/silver/application/streams/test_submitted_transactions.py
@@ -1,14 +1,6 @@
-from pyspark.sql import SparkSession
 from pytest_mock import MockerFixture
 
-import tests.helpers.identifier_helper as identifier_helper
-import tests.helpers.table_helper as table_helper
-from core.bronze.infrastructure.config.table_names import TableNames as BronzeTableNames
-from core.settings.bronze_settings import BronzeSettings
-from core.settings.silver_settings import SilverSettings
 from core.silver.application.streams import submitted_transactions as sut
-from core.silver.infrastructure.config import SilverTableNames
-from tests.helpers.builders.submitted_transactions_builder import SubmittedTransactionsBuilder, ValueBuilder
 
 
 def test__submitted_transactions__should_call_expected(mocker: MockerFixture) -> None:
@@ -24,30 +16,3 @@ def test__submitted_transactions__should_call_expected(mocker: MockerFixture) ->
     mock_initialize_spark.assert_called_once()
     mock_SubmittedTransactionsRepository.assert_called_once()
     mock_SilverMeasurementsRepository.assert_called_once()
-
-
-def test__submitted_transactions__should_save_in_silver_measurements(
-    mock_checkpoint_path,
-    spark: SparkSession,
-    migrations_executed,
-) -> None:
-    # Arrange
-    bronze_settings = BronzeSettings()
-    silver_settings = SilverSettings()
-    expected_orchestration_id = identifier_helper.generate_random_string()
-    value = ValueBuilder(spark).add_row(orchestration_instance_id=expected_orchestration_id).build()
-    submitted_transactions = SubmittedTransactionsBuilder(spark).add_row(value=value).build()
-    table_helper.append_to_table(
-        submitted_transactions,
-        bronze_settings.bronze_database_name,
-        BronzeTableNames.bronze_submitted_transactions_table,
-    )
-
-    # Act
-    sut.stream_submitted_transactions()
-
-    # Assert
-    silver_table = spark.table(f"{silver_settings.silver_database_name}.{SilverTableNames.silver_measurements}").where(
-        f"orchestration_instance_id = '{expected_orchestration_id}'"
-    )
-    assert silver_table.count() == 1
