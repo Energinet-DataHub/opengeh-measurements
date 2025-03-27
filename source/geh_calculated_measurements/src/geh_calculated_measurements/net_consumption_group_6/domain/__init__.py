b19f9beb129ba4d3e6444d3fc3117126028c26b8
Jonas Michelsen <jvm@energinet.dk>
2025-03-27 08:38:45 +0100

    Update __init__.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/__init__.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/__init__.py
@@ -1,10 +1,11 @@
-from geh_calculated_measurements.net_consumption_group_6.domain.calculation import execute
 from geh_calculated_measurements.net_consumption_group_6.domain.model.cenc import Cenc
 from geh_calculated_measurements.net_consumption_group_6.domain.model.child_metering_points import ChildMeteringPoints
 from geh_calculated_measurements.net_consumption_group_6.domain.model.consumption_metering_point_periods import (
     ConsumptionMeteringPointPeriods,
 )
 from geh_calculated_measurements.net_consumption_group_6.domain.model.time_series_points import TimeSeriesPoints
+
+from .calculation import execute
 
 __all__ = [
     "execute",
