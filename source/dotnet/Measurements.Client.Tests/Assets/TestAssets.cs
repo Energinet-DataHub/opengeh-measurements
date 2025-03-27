88e8cdbb4a466cb2edb5a5e8d4bc198a74aaeb58
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2025-03-27 10:49:59 +0100

    Convert client test to unit tests (#522)
    Co-authored-by: Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>

---
+++ source/dotnet/Measurements.Client.Tests/Assets/TestAssets.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Energinet.DataHub.Measurements.Client.Tests.Assets;
+
+public static class TestAssets
+{
+    public static string MeasurementsForDayExcludingHistory => GetJsonFile($"{nameof(MeasurementsForDayExcludingHistory)}.json");
+
+    public static string MeasurementsForMultipleDays => GetJsonFile($"{nameof(MeasurementsForMultipleDays)}.json");
+
+    private static string GetJsonFile(string filename)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var fullResourceName = $"{assembly.GetName().Name}.Assets.{filename}";
+
+        using var stream = assembly.GetManifestResourceStream(fullResourceName)
+                           ?? throw new FileNotFoundException($"Could not find embedded resource: {filename}");
+
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
