947b35da49e1702f6f7257fae892d891519d8c62
Jan Duelund <jdd@energinet.dk>
2025-03-28 13:07:52 +0100

    Add columns to measurement views in SQL

--- source/core/src/core/migrations/migration_scripts/20250328121200_fix_cancelled_issue_in_measurements_views.sql
+++ source/core/src/core/migrations/migration_scripts/20250328121200_fix_cancelled_issue_in_measurements_views.sql
@@ -4,7 +4,12 @@ GO
 CREATE VIEW {gold_database}.{gold_current_v1} AS
 WITH RankedRows AS (
     SELECT
-        *,
+        metering_point_id,
+        observation_time,
+        quantity,
+        quality,
+        metering_point_type,
+        is_cancelled,
         ROW_NUMBER() OVER (PARTITION BY metering_point_id, observation_time ORDER BY transaction_creation_datetime DESC) AS row_num
     FROM {gold_database}.{gold_measurements}
     WHERE
@@ -28,7 +33,10 @@ GO
 CREATE VIEW {gold_database}.{gold_capacity_settlement_v1} AS
 WITH RankedRows AS (
     SELECT
-        *,
+        metering_point_id,
+        quantity,
+        observation_time,
+        metering_point_type,
         ROW_NUMBER() OVER (PARTITION BY metering_point_id, observation_time ORDER BY transaction_creation_datetime DESC) AS row_num
     FROM {gold_database}.{gold_measurements}
     WHERE metering_point_id IS NOT NULL
@@ -51,7 +59,10 @@ GO
 CREATE VIEW {gold_database}.{gold_electrical_heating_v1} AS
 WITH RankedRows AS (
     SELECT
-        *,
+        metering_point_id,
+        quantity,
+        observation_time,
+        metering_point_type,
         ROW_NUMBER() OVER (PARTITION BY metering_point_id, observation_time ORDER BY transaction_creation_datetime DESC) AS row_num
     FROM {gold_database}.{gold_measurements}
     WHERE metering_point_id IS NOT NULL
