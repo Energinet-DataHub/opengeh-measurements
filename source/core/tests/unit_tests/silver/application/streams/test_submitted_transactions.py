12744231972ff952e43192a032c3066ebc5fdb32
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-03-28 11:53:14 +0100

    refac: Change tests folder structure (#530)

---
+++ source/core/tests/unit_tests/silver/application/streams/test_submitted_transactions.py
@@ -0,0 +1,18 @@
+from pytest_mock import MockerFixture
+
+from core.silver.application.streams import submitted_transactions as sut
+
+
+def test__submitted_transactions__should_call_expected(mocker: MockerFixture) -> None:
+    # Arrange
+    mock_initialize_spark = mocker.patch(f"{sut.__name__}.spark_session.initialize_spark")
+    mock_SubmittedTransactionsRepository = mocker.patch(f"{sut.__name__}.SubmittedTransactionsRepository")
+    mock_SilverMeasurementsRepository = mocker.patch(f"{sut.__name__}.SilverMeasurementsRepository")
+
+    # Act
+    sut.stream_submitted_transactions()
+
+    # Assert
+    mock_initialize_spark.assert_called_once()
+    mock_SubmittedTransactionsRepository.assert_called_once()
+    mock_SilverMeasurementsRepository.assert_called_once()
