47ac03636ab063f9d52f360638d3a21e78f5f0a8
Jan Duelund <jdd@energinet.dk>
2025-03-28 12:17:17 +0100

    fix cancelled issue

---
+++ source/core/src/core/migrations/migration_scripts/20250328121200_fix_cancelled_issue_in_measurements_views.sql
@@ -0,0 +1,67 @@
+DROP VIEW IF EXISTS {gold_database}.{gold_current_v1}
+GO
+
+CREATE VIEW {gold_database}.{gold_current_v1} AS
+WITH RankedRows AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY metering_point_id, observation_time ORDER BY transaction_creation_datetime DESC) AS row_num
+    FROM {gold_database}.{gold_measurements}
+    WHERE
+    metering_point_id IS NOT NULL
+    AND observation_time IS NOT NULL
+    AND quantity IS NOT NULL
+    AND quality IS NOT NULL
+    AND metering_point_type IS NOT NULL
+)
+SELECT metering_point_id, observation_time, quantity, quality, metering_point_type
+FROM RankedRows
+WHERE
+    row_num = 1
+    AND NOT is_cancelled;
+
+GO
+
+DROP VIEW IF EXISTS {gold_database}.{gold_capacity_settlement_v1}
+GO
+
+CREATE VIEW {gold_database}.{gold_capacity_settlement_v1} AS
+WITH RankedRows AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY metering_point_id, observation_time ORDER BY transaction_creation_datetime DESC) AS row_num
+    FROM {gold_database}.{gold_measurements}
+    WHERE metering_point_id IS NOT NULL
+    AND quantity IS NOT NULL
+    AND observation_time IS NOT NULL
+    AND metering_point_type IS NOT NULL
+    AND metering_point_type IN ('consumption', 'capacity_settlement')
+)
+SELECT metering_point_id, quantity, observation_time, metering_point_type
+FROM RankedRows
+WHERE
+    row_num = 1
+    AND NOT is_cancelled;
+
+GO
+
+DROP VIEW IF EXISTS {gold_database}.{gold_electrical_heating_v1}
+GO
+
+CREATE VIEW {gold_database}.{gold_electrical_heating_v1} AS
+WITH RankedRows AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY metering_point_id, observation_time ORDER BY transaction_creation_datetime DESC) AS row_num
+    FROM {gold_database}.{gold_measurements}
+    WHERE metering_point_id IS NOT NULL
+    AND quantity IS NOT NULL
+    AND observation_time IS NOT NULL
+    AND metering_point_type IS NOT NULL
+)
+SELECT metering_point_id, quantity, observation_time, metering_point_type
+FROM RankedRows
+WHERE
+    row_num = 1
+    AND NOT is_cancelled;
+
