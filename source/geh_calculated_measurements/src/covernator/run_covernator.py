e6f25a06deec1cb651b4540762518bc89799a8b8
Felix Achter <fa@halfspace.io>
2025-03-31 10:28:53 +0200

    using new version of covernator

--- source/geh_calculated_measurements/src/covernator/run_covernator.py
+++ source/geh_calculated_measurements/src/covernator/run_covernator.py
@@ -1,15 +1,7 @@
 from pathlib import Path
 
-from geh_common.testing.covernator import create_all_cases_file, create_result_and_all_scenario_files
+from geh_common.testing.covernator import run_covernator
 
 output_folder = Path("/workspace/source/geh_calculated_measurements/src/covernator/output_files")
-create_all_cases_file(
-    output_folder,
-    Path(
-        "/workspace/source/geh_calculated_measurements/tests/missing_measurements_log/coverage/all_cases_missing_measurements_log.yml"
-    ),
-)
-create_result_and_all_scenario_files(
-    output_folder,
-    Path("/workspace/source/geh_calculated_measurements/tests/missing_measurements_log/scenario_tests"),
-)
+
+run_covernator(output_folder, Path("/workspace/source/geh_calculated_measurements/tests"))
