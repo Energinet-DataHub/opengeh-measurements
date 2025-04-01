d3d47a638b95eb72f73b1c5b0b19fcec134ad730
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-01 14:45:05 +0200

    Update 20250401130100_create_view_missing_measurements_log_v1.sql

--- source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migration_scripts/20250401130100_create_view_missing_measurements_log_v1.sql
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migration_scripts/20250401130100_create_view_missing_measurements_log_v1.sql
@@ -2,4 +2,4 @@ CREATE OR REPLACE VIEW {catalog_name}.measurements_calculated.missing_measuremen
 SELECT orchestration_instance_id,
        metering_point_id,
        date        
-FROM {catalog_name}.measurements_calculated_internal.missing_measurements_log_v1
+FROM {catalog_name}.measurements_calculated_internal.missing_measurements_log
