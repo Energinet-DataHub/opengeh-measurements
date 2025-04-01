ac32a37589c0545c1b3264308e03b803c7a79f41
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-01 11:49:31 +0200

    update mmlog args test

--- source/geh_calculated_measurements/tests/missing_measurements_log/missing_measurements_log_tests/application/test_missing_measurements_log_job_args.py
+++ source/geh_calculated_measurements/tests/missing_measurements_log/missing_measurements_log_tests/application/test_missing_measurements_log_job_args.py
@@ -10,15 +10,16 @@ from tests.missing_measurements_log import CONTAINER_NAME
 
 _CONTRACTS_PATH = (PROJECT_ROOT / "src" / "geh_calculated_measurements" / CONTAINER_NAME / "contracts").as_posix()
 
-DEFAULT_ORCHESTRATION_INSTANCE_ID = uuid.UUID("12345678-9fc8-409a-a169-fbd49479d711")
-DEFAULT_TIME_ZONE = "Europe/Copenhagen"
-DEFAULT_CATALOG_NAME = "spark_catalog"
+_EXPECTED_ORCHESTRATION_INSTANCE_ID = uuid.UUID("12345678-9fc8-409a-a169-fbd49479d711")
+_EXPECTED_TIME_ZONE = "Europe/Copenhagen"
+_EXPECTED_CATALOG_NAME = "spark_catalog"
+_EXPECTED_GRID_AREA_CODES = ["804", "805"]
 
 
 def _get_contract_parameters(filename: str) -> list[str]:
     with open(filename) as file:
         text = file.read()
-        text = text.replace("{orchestration-instance-id}", str(DEFAULT_ORCHESTRATION_INSTANCE_ID))
+        text = text.replace("{orchestration-instance-id}", str(_EXPECTED_ORCHESTRATION_INSTANCE_ID))
         lines = text.splitlines()
         return list(filter(lambda line: not line.startswith("#") and len(line) > 0, lines))
 
@@ -36,6 +37,7 @@ def test_when_parameters__parses_parameters_from_contract(monkeypatch) -> None:
     actual_args = MissingMeasurementsLogArgs()
 
     # Assert
-    assert actual_args.orchestration_instance_id == DEFAULT_ORCHESTRATION_INSTANCE_ID
-    assert actual_args.time_zone == DEFAULT_TIME_ZONE
-    assert actual_args.catalog_name == DEFAULT_CATALOG_NAME
+    assert actual_args.orchestration_instance_id == _EXPECTED_ORCHESTRATION_INSTANCE_ID
+    assert actual_args.time_zone == _EXPECTED_TIME_ZONE
+    assert actual_args.catalog_name == _EXPECTED_CATALOG_NAME
+    assert actual_args.grid_area_codes == _EXPECTED_GRID_AREA_CODES
