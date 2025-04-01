dd92b00c5621f7e1a51425d822dcf41e86d21ea1
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-01 11:51:09 +0000

    removed unused code

--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/infrastucture/repository.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/infrastucture/repository.py
@@ -1,4 +1,3 @@
-from geh_common.testing.dataframes import assert_contract
 from pyspark.sql import SparkSession
 
 from geh_calculated_measurements.net_consumption_group_6.domain import (
@@ -15,22 +14,18 @@ class Repository:
         self,
         spark: SparkSession,
         catalog_name: str,
-        electricity_market_data_path: str | None = None,
     ) -> None:
         self._spark = spark
-        self._electricity_market_data_path = electricity_market_data_path
         self._catalog_name = catalog_name
 
     def read_net_consumption_group_6_consumption_metering_point_periods(self) -> ConsumptionMeteringPointPeriods:
         table_name = f"{self._catalog_name}.{ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME}.{ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CONSUMPTION_METERING_POINT_PERIODS}"
 
         df = self._spark.read.format("delta").table(table_name)
-        assert_contract(df.schema, ConsumptionMeteringPointPeriods.schema)
         return ConsumptionMeteringPointPeriods(df)
 
     def read_net_consumption_group_6_child_metering_points(self) -> ChildMeteringPoints:
         table_name = f"{self._catalog_name}.{ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME}.{ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT}"
 
         df = self._spark.read.format("delta").table(table_name)
-        assert_contract(df.schema, ChildMeteringPoints.schema)
         return ChildMeteringPoints(df)
