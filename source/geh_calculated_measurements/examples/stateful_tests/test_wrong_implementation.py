a9850c80de84f2fc737cb80d904295b417f4d7dc
Jens Peder Meldgaard <jpm@halfspace.io>
2025-04-01 12:13:16 +0200

    chore: rename example tests
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_calculated_measurements/examples/stateful_tests/test_wrong_implementation.py
+++ source/geh_calculated_measurements/examples/stateful_tests/test_wrong_implementation.py
@@ -4,21 +4,21 @@ from examples.stateful_tests.ExampleTester import ExampleTester, ExampleTestFixt
 
 
 @pytest.mark.xfail(reason="These tests are flaky as they manipulate the same object. Uncomment to see what happens.")
-class TestRunnerWithCorrectImplementation(ExampleTester):
+class TestRunnerWithWrongImplementation(ExampleTester):
     @pytest.fixture(scope="class")
     def fixture(self):
         return ExampleTestFixture("test_wrong")
 
 
 @pytest.mark.xfail(reason="These tests are flaky as they manipulate the same object. Uncomment to see what happens.")
-class TestRunnerWithCorrectImplementation1(ExampleTester):
+class TestRunnerWithWrongImplementation1(ExampleTester):
     @pytest.fixture(scope="class")
     def fixture(self):
         return ExampleTestFixture("another_test_wrong")
 
 
 @pytest.mark.xfail(reason="These tests are flaky as they manipulate the same object. Uncomment to see what happens.")
-class TestRunnerWithCorrectImplementation2(ExampleTester):
+class TestRunnerWithWrongImplementation2(ExampleTester):
     @pytest.fixture(scope="class")
     def fixture(self):
         return ExampleTestFixture("yet_another_test_wrong")
