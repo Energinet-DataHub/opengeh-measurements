2d688e2c9eef589d07579f294f16fa742e5e3519
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-04-01 14:53:07 +0200

    example

--- source/core/src/core/silver/infrastructure/streams/silver_measurements_stream.py
+++ source/core/src/core/silver/infrastructure/streams/silver_measurements_stream.py
@@ -3,12 +3,12 @@ from typing import Callable
 from pyspark.sql import DataFrame
 
 import core.utility.shared_helpers as shared_helpers
+from core.generics.stream import Stream
 from core.settings import StorageAccountSettings
 from core.settings.silver_settings import SilverSettings
-from core.settings.streaming_settings import StreamingSettings
 
 
-class SilverMeasurementsStream:
+class SilverMeasurementsStream(Stream):
     def __init__(self) -> None:
         self.data_lake_settings = StorageAccountSettings().DATALAKE_STORAGE_ACCOUNT
         self.silver_container_name = SilverSettings().silver_container_name
@@ -16,37 +16,21 @@ class SilverMeasurementsStream:
     def stream_submitted_transactions(
         self, measurements: DataFrame, batch_operation: Callable[["DataFrame", int], None]
     ) -> bool | None:
+        query_name = "stream_bronze_submitted_transactions_to_silver"
         checkpoint_name = "submitted_transactions"
         checkpoint_location = shared_helpers.get_checkpoint_path(
             self.data_lake_settings, self.silver_container_name, checkpoint_name
         )
 
-        write_stream = (
-            measurements.writeStream.outputMode("append")
-            .format("delta")
-            .option("checkpointLocation", checkpoint_location)
-        )
-
-        stream_settings = StreamingSettings()
-        write_stream = stream_settings.apply_streaming_settings(write_stream)
-
-        return write_stream.foreachBatch(batch_operation).start().awaitTermination()
+        return self.write_stream(measurements, checkpoint_location, query_name, batch_operation)
 
     def stream_migrated_transactions(
         self, measurements: DataFrame, batch_operation: Callable[["DataFrame", int], None]
     ) -> bool | None:
+        query_name = "stream_bronze_migrated_transactions_to_silver"
         checkpoint_name = "migrated_transactions"
         checkpoint_location = shared_helpers.get_checkpoint_path(
             self.data_lake_settings, self.silver_container_name, checkpoint_name
         )
 
-        write_stream = (
-            measurements.writeStream.outputMode("append")
-            .format("delta")
-            .option("checkpointLocation", checkpoint_location)
-        )
-
-        stream_settings = StreamingSettings()
-        write_stream = stream_settings.apply_streaming_settings(write_stream)
-
-        return write_stream.foreachBatch(batch_operation).start().awaitTermination()
+        return self.write_stream(measurements, checkpoint_location, query_name, batch_operation)
