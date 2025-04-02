271224f4e036a427ecfe69a1bc48de66288f95a4
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2025-04-02 15:05:05 +0200

    feat: Add authentication to MeasurementsApi (#507)
    Co-authored-by: jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>

---
+++ source/dotnet/Measurements.WebApi/Extensions/DependencyInjection/AuthorizationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Energinet.DataHub.Measurements.WebApi.Extensions.DependencyInjection;
+
+public static class AuthorizationExtensions
+{
+    public static IServiceCollection AddAuthorizationForWebApp(this IServiceCollection services)
+    {
+        services.AddAuthorizationBuilder()
+            .SetFallbackPolicy(new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build());
+        return services;
+    }
+}
