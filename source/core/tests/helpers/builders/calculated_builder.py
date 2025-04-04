88bee76efe1c49ae7e4bb3efc808ce1a03b70b10
Gizmooa <jeppe1000@live.dk>
2025-04-04 06:33:24 +0000

    adj

--- source/core/tests/helpers/builders/calculated_builder.py
+++ source/core/tests/helpers/builders/calculated_builder.py
@@ -12,31 +12,31 @@ class CalculatedMeasurementsBuilder:
 
     def add_row(
         self,
+        orchestration_type: str = "capacity_settlement",
+        orchestration_instance_id: str = "123456",
+        transaction_id="",
+        transaction_creation_datetime=datetime.now(),
         metering_point_id: str | None = "502938475674839281",
         metering_point_type: str | None = random.choice(["E17", "E18", "E20", "D01", "D05", "D06", "D07", "D08"]),
         observation_time: datetime | None = datetime.now(),
-        orchestration_instance_id: str = "123456",
-        orchestration_type: str = "capacity_settlement",
         quantity: Decimal | None = Decimal(random.uniform(1, 1000)),
-        quantity_quality=random.choice(["measured", "estimated", "calculated", "missing"]),
         quantity_unit: str = "kWh",
+        quantity_quality=random.choice(["measured", "estimated", "calculated", "missing"]),
         resolution: str = "PT1H",
-        transaction_creation_datetime=datetime.now(),
-        transaction_id="",
     ):
         self.data.append(
             (
+                orchestration_type,
+                orchestration_instance_id,
+                transaction_id,
+                transaction_creation_datetime,
                 metering_point_id,
                 metering_point_type,
                 observation_time,
-                orchestration_instance_id,
-                orchestration_type,
                 quantity,
-                quantity_quality,
                 quantity_unit,
+                quantity_quality,
                 resolution,
-                transaction_creation_datetime,
-                transaction_id,
             )
         )
         return self
