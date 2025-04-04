32e3cce27e56301dd30ec16411c9995649ba4b7f
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-04-04 14:00:44 +0200

    remove print

--- source/core/tests/subsystem_tests/step_definitions/test_transmission_of_calculated_measurements.py
+++ source/core/tests/subsystem_tests/step_definitions/test_transmission_of_calculated_measurements.py
@@ -16,7 +16,6 @@ scenarios("../features/transmission_of_calculated_measurements.feature")
 @given("a new valid calculated measurements", target_fixture="calculated_measurements_row")
 def _() -> CalculatedMeasurementsRow:
     orchestration_instance_id = identifier_helper.generate_random_string()
-    print(orchestration_instance_id)
     return CalculatedMeasurementsRowBuilder().build(orchestration_instance_id=orchestration_instance_id)
 
 
