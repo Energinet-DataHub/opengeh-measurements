8d3ed803562b8ceae04d2e74e5407ce3fca385ba
Jens Peder Meldgaard <jpm@halfspace.io>
2025-04-04 14:27:50 +0200

    chore: timespan should be none
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_calculated_measurements/src/geh_calculated_measurements/testing/utilities/job_tester.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/testing/utilities/job_tester.py
@@ -120,7 +120,9 @@ class JobTestFixture:
         return LogsQueryError(details)
 
     def wait_for_log_query_completion(self, query: str) -> LogsQueryResult | LogsQueryPartialResult:
-        response = self.azure_logs_query_client.query_workspace(self.azure_log_analytics_workspace_id, query)
+        response = self.azure_logs_query_client.query_workspace(
+            self.azure_log_analytics_workspace_id, query, timespan=None
+        )
         if response.status == LogsQueryStatus.SUCCESS and len(response.tables) > 0 and len(response.tables[0].rows) > 0:
             return response
         else:
