196dac45dbdd216f562465b37666049ef4ca70e6
Gizmooa <jeppe1000@live.dk>
2025-04-04 08:33:52 +0000

    ....

--- source/core/tests/helpers/schema_helper.py
+++ source/core/tests/helpers/schema_helper.py
@@ -1,12 +1,12 @@
 from pyspark.sql import SparkSession
 
-from core.gold.infrastructure.config.external_view_names import ExternalViewNames
 from core.migrations import MigrationDatabaseNames
 from core.settings.bronze_settings import BronzeSettings
 from core.settings.calculated_settings import CalculatedSettings
 from core.settings.core_internal_settings import CoreInternalSettings
 from core.settings.gold_settings import GoldSettings
 from core.settings.silver_settings import SilverSettings
+from tests.helpers.sql_scripts.create_calculated_measurements_v1 import create_calculated_measurements_v1
 
 
 def create_internal_schemas(spark: SparkSession) -> None:
@@ -27,7 +27,4 @@ def create_external_schemas(spark: SparkSession) -> None:
 
 
 def create_external_tables(spark: SparkSession) -> None:
-    calculated_settings = CalculatedSettings()
-    spark.sql(
-        f"CREATE TABLE IF NOT EXISTS {calculated_settings.calculated_database_name}.{ExternalViewNames.calculated_measurements_v1}"
-    )
+    spark.sql(create_calculated_measurements_v1)
---
+++ source/core/tests/helpers/sql_scripts/create_calculated_measurements_v1.py
@@ -0,0 +1,26 @@
+from core.gold.infrastructure.config.external_view_names import ExternalViewNames
+from core.settings.calculated_settings import CalculatedSettings
+
+calculated_settings = CalculatedSettings()
+
+create_calculated_measurements_v1 = f"""
+CREATE TABLE IF NOT EXISTS {calculated_settings.calculated_database_name}.{ExternalViewNames.calculated_measurements_v1} (
+    orchestration_type STRING NOT NULL,
+    orchestration_instance_id STRING NOT NULL,
+    transaction_id STRING NOT NULL,
+    transaction_creation_datetime TIMESTAMP NOT NULL,
+    metering_point_id STRING NOT NULL,
+    metering_point_type STRING NOT NULL,
+    observation_time TIMESTAMP NOT NULL,
+    quantity DECIMAL(18,3) NOT NULL,
+    quantity_unit STRING NOT NULL,
+    quantity_quality STRING NOT NULL,
+    resolution STRING NOT NULL
+)
+USING DELTA
+TBLPROPERTIES (
+    delta.autoOptimize.optimizeWrite = true,
+    delta.autoOptimize.autoCompact = false,
+    delta.deletedFileRetentionDuration = 'interval 30 days'
+);
+"""
