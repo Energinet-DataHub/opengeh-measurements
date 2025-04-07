f1c2c9b9d6cb90a7b55d8836967f608eb5badc48
HenrikSommer <sommerh@gmail.com>
2025-04-07 15:37:46 +0200

    Clean-up doc-strings

--- source/dotnet/Measurements.Application/Persistence/IMeasurementsRepository.cs
+++ source/dotnet/Measurements.Application/Persistence/IMeasurementsRepository.cs
@@ -10,15 +10,10 @@ public interface IMeasurementsRepository
     /// <summary>
     /// Get measurements for a given metering point in period defined by from and to timestamps.
     /// </summary>
-    /// <param name="meteringPointId"></param>
-    /// <param name="from"></param>
-    /// <param name="to"></param>
     IAsyncEnumerable<MeasurementsResult> GetMeasurementsAsync(string meteringPointId, Instant from, Instant to);
 
     /// <summary>
     /// Get aggregated measurements for a given metering point for a month defined by the yearMonth parameter.
     /// </summary>
-    /// <param name="meteringPointId"></param>
-    /// <param name="yearMonth"></param>
     IAsyncEnumerable<AggregatedMeasurementsResult> GetAggregatedMeasurementsAsync(string meteringPointId, YearMonth yearMonth);
 }
