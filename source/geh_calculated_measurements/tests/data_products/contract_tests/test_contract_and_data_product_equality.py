02028412972de265d3418e847251eae9e28a5d2d
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-07 11:11:14 +0000

    removed data product contract test

--- source/geh_calculated_measurements/tests/data_products/contract_tests/test_contract_and_data_product_equality.py
+++ source/geh_calculated_measurements/tests/data_products/contract_tests/test_contract_and_data_product_equality.py
@@ -4,49 +4,31 @@ from geh_common.testing.dataframes import assert_contract
 from pyspark.sql import SparkSession
 
 from geh_calculated_measurements.common.infrastructure import CalculatedMeasurementsDatabaseDefinition
-from geh_calculated_measurements.net_consumption_group_6.infrastucture.database_definitions import (
-    ElectricityMarketMeasurementsInputDatabaseDefinition,
-)
-from geh_calculated_measurements.net_consumption_group_6.infrastucture.schema import (
-    net_consumption_group_6_child_metering_point_v1,
-    net_consumption_group_6_consumption_metering_point_periods_v1,
-)
 from tests import SPARK_CATALOG_NAME
 
 
 @pytest.mark.parametrize(
-    ("database_name", "view_name", "contract_schema"),
+    ("view_name", "contract_schema"),
     [
         (
-            CalculatedMeasurementsDatabaseDefinition.DATABASE_NAME,
             CalculatedMeasurementsDatabaseDefinition.CALCULATED_MEASUREMENTS_VIEW_NAME,
             calculated_measurements_v1.calculated_measurements_v1,
         ),
         (
-            CalculatedMeasurementsDatabaseDefinition.DATABASE_NAME,
             CalculatedMeasurementsDatabaseDefinition.MISSING_MEASUREMENTS_LOG_VIEW_NAME,
             missing_measurements_log_v1.missing_measurements_log_v1,
-        ),
-        (
-            ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME,
-            ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CONSUMPTION_METERING_POINT_PERIODS,
-            net_consumption_group_6_consumption_metering_point_periods_v1,
-        ),
-        (
-            ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME,
-            ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT,
-            net_consumption_group_6_child_metering_point_v1,
         ),
     ],
 )
 def test_data_product_matches_contract(
     migrations_executed: None,  # Used implicitly
-    external_dataproducts_created: None,  # Used implicitly
     spark: SparkSession,
-    database_name: str,
     view_name: str,
     contract_schema,
 ) -> None:
+    # Arrange
+    database_name = CalculatedMeasurementsDatabaseDefinition.DATABASE_NAME
+
     # Act
     view_df = spark.table(f"{SPARK_CATALOG_NAME}.{database_name}.{view_name}").limit(1)
 
