b100834f60fc2912532df3c667e261b1cc2b7abd
HenrikSommer <sommerh@gmail.com>
2025-04-07 15:50:48 +0200

    Fix year and month range tests

--- source/dotnet/Measurements.Application/Requests/GetAggregatedMeasurementsForMonthRequest.cs
+++ source/dotnet/Measurements.Application/Requests/GetAggregatedMeasurementsForMonthRequest.cs
@@ -1,8 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace Energinet.DataHub.Measurements.Application.Requests;
 
-namespace Energinet.DataHub.Measurements.Application.Requests;
+public class GetAggregatedMeasurementsForMonthRequest(string meteringPointId, int year, int month)
+{
+    private static bool IsValidYear(int year)
+    {
+        return year is >= -9998 and <= 9999;
+    }
 
-public record GetAggregatedMeasurementsForMonthRequest(
-    string MeteringPointId,
-    [Range(-9998, 9999, ErrorMessage = "{0} must be between {1} and {2}")] int Year,
-    [Range(1, 12, ErrorMessage = "{0} must be between {1} and {2}")] int Month);
+    private static bool IsValidMonth(int month)
+    {
+        return month is >= 1 and <= 12;
+    }
+
+    public string MeteringPointId { get; } = meteringPointId;
+
+    public int Year { get; } = IsValidYear(year) ? year :
+        throw new ArgumentOutOfRangeException(nameof(year), "Year must be between -9998 and 9999");
+
+    public int Month { get; } = IsValidMonth(month) ? month :
+        throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12");
+}
