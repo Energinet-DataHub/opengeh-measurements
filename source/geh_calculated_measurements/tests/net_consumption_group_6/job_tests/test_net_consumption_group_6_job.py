24bfb28eabef1e62b03d94e615c0b0563badbd3b
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-07 09:44:38 +0000

    Updated job_test and added seeding

--- source/geh_calculated_measurements/tests/net_consumption_group_6/job_tests/test_net_consumption_group_6_job.py
+++ source/geh_calculated_measurements/tests/net_consumption_group_6/job_tests/test_net_consumption_group_6_job.py
@@ -7,15 +7,30 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
 from geh_calculated_measurements.common.domain import ContractColumnNames
-from geh_calculated_measurements.common.infrastructure import CalculatedMeasurementsInternalDatabaseDefinition
 from geh_calculated_measurements.net_consumption_group_6.entry_point import execute
+from geh_calculated_measurements.net_consumption_group_6.infrastucture.database_definitions import (
+    ElectricityMarketMeasurementsInputDatabaseDefinition,
+)
 from tests import create_job_environment_variables
 from tests.net_consumption_group_6.job_tests import get_test_files_folder_path
+
+DATABASE = ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME
+PARENT_TABLE = (
+    ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CONSUMPTION_METERING_POINT_PERIODS
+)
+CHILD_TABLE = ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT
+
+# PARENT_METERING_POINT_ID = "170000050000000201"
+# CHILD_METERING_POINT_ID_1 = "150000001500170200"
+# CHILD_METERING_POINT_ID_2 = "060000001500170200"
+# CHILD_METERING_POINT_ID_3 = "070000001500170200"
 
 
 def test_execute(
     spark: SparkSession,
     monkeypatch: pytest.MonkeyPatch,
+    gold_table_seeded: None,  # Used implicitly
+    electricity_market_tables_seeded: None,  # Used implicitly
     migrations_executed: None,  # Used implicitly
     external_dataproducts_created: None,  # Used implicitly
     dummy_logging: None,  # Used implicitly
@@ -29,7 +44,20 @@ def test_execute(
     execute()
 
     # Assert
-    actual_calculated_measurements = spark.read.table(
-        f"{CalculatedMeasurementsInternalDatabaseDefinition.DATABASE_NAME}.{CalculatedMeasurementsInternalDatabaseDefinition.MEASUREMENTS_TABLE_NAME}"
-    ).where(F.col(ContractColumnNames.orchestration_instance_id) == orchestration_instance_id)
-    assert actual_calculated_measurements.count() > 0
+    actual_parent_table = spark.read.table(f"{DATABASE}.{PARENT_TABLE}").where(
+        F.col(ContractColumnNames.metering_point_id) == "170000050000000201"
+    )
+    assert actual_parent_table.count() > 0
+
+    actual_child_table_net_consumption = spark.read.table(f"{DATABASE}.{CHILD_TABLE}").where(
+        F.col(ContractColumnNames.metering_point_id) == "150000001500170200"
+    )
+    assert actual_child_table_net_consumption.count() > 0
+    actual_child_table_supply_to_grid = spark.read.table(f"{DATABASE}.{CHILD_TABLE}").where(
+        F.col(ContractColumnNames.metering_point_id) == "060000001500170200"
+    )
+    assert actual_child_table_supply_to_grid.count() > 0
+    actual_child_table_consumption_from_grid = spark.read.table(f"{DATABASE}.{CHILD_TABLE}").where(
+        F.col(ContractColumnNames.metering_point_id) == "070000001500170200"
+    )
+    assert actual_child_table_consumption_from_grid.count() > 0
