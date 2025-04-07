63b651ccde368d69bf6b46eb9b4a638a167b40b7
HenrikSommer <sommerh@gmail.com>
2025-04-07 14:33:48 +0200

    Test the range request guard

--- source/dotnet/Measurements.Application/Requests/GetAggregatedMeasurementsForMonthRequest.cs
+++ source/dotnet/Measurements.Application/Requests/GetAggregatedMeasurementsForMonthRequest.cs
@@ -2,4 +2,7 @@
 
 namespace Energinet.DataHub.Measurements.Application.Requests;
 
-public record GetAggregatedMeasurementsForMonthRequest(string MeteringPointId, [Range(-9998, 9999)] int Year, [Range(1, 12)] int Month);
+public record GetAggregatedMeasurementsForMonthRequest(
+    string MeteringPointId,
+    [Range(-9998, 9999, ErrorMessage = "{0} must be between {1} and {2}")] int Year,
+    [Range(1, 12, ErrorMessage = "{0} must be between {1} and {2}")] int Month);
---
+++ source/dotnet/Measurements.UnitTests/Application/Requests/GetAggregatedMeasurementsForMonthRequestTests.cs
@@ -0,0 +1,36 @@
+﻿using Energinet.DataHub.Measurements.Application.Requests;
+using Xunit;
+
+namespace Energinet.DataHub.Measurements.UnitTests.Application.Requests;
+
+public class GetAggregatedMeasurementsForMonthRequestTests
+{
+    [Fact]
+    public void GetAggregatedMeasurementsForMonthRequest_WhenValid_ReturnsExpected()
+    {
+        // Arrange
+        const string meteringPointId = "123456789";
+        const int year = 2025;
+        const int month = 1;
+
+        // Act
+        var actual = new GetAggregatedMeasurementsForMonthRequest(meteringPointId, year, month);
+
+        // Assert
+        Assert.Equal(meteringPointId, actual.MeteringPointId);
+        Assert.Equal(year, actual.Year);
+        Assert.Equal(month, actual.Month);
+    }
+
+    [Theory]
+    [InlineData(2025, 13)]
+    [InlineData(2025, 0)]
+    public void GetAggregatedMeasurementsForMonthRequest_WhenInvalidMonth_ThrowsArgumentOutOfRangeException(int year, int month)
+    {
+        // Arrange
+        const string meteringPointId = "123456789";
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => new GetAggregatedMeasurementsForMonthRequest(meteringPointId, year, month));
+    }
+}
