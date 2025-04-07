53d0c4726280ccd6aebf8988a5307800da153a84
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-07 12:17:33 +0000

    Now uses seeded data

--- source/geh_calculated_measurements/tests/net_consumption_group_6/job_tests/test_net_consumption_group_6_job.py
+++ source/geh_calculated_measurements/tests/net_consumption_group_6/job_tests/test_net_consumption_group_6_job.py
@@ -7,45 +7,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
 from geh_calculated_measurements.common.domain import ContractColumnNames
+from geh_calculated_measurements.common.infrastructure import CalculatedMeasurementsInternalDatabaseDefinition
 from geh_calculated_measurements.net_consumption_group_6.entry_point import execute
-from geh_calculated_measurements.net_consumption_group_6.infrastucture.database_definitions import (
-    ElectricityMarketMeasurementsInputDatabaseDefinition,
-)
 from tests import create_job_environment_variables
 from tests.net_consumption_group_6.job_tests import get_test_files_folder_path
 
-DATABASE = ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME
-PARENT_TABLE = (
-    ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CONSUMPTION_METERING_POINT_PERIODS
-)
-CHILD_TABLE = ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT
 
-
-@pytest.mark.parametrize(
-    ("database_name", "table_name", "metering_point_id"),
-    [
-        (
-            ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME,
-            ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CONSUMPTION_METERING_POINT_PERIODS,
-            "170000050000000201",
-        ),
-        (
-            ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME,
-            ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT,
-            "150000001500170200",
-        ),
-        (
-            ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME,
-            ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT,
-            "060000001500170200",
-        ),
-        (
-            ElectricityMarketMeasurementsInputDatabaseDefinition.DATABASE_NAME,
-            ElectricityMarketMeasurementsInputDatabaseDefinition.NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT,
-            "070000001500170200",
-        ),
-    ],
-)
 def test_execute(
     spark: SparkSession,
     monkeypatch: pytest.MonkeyPatch,
@@ -54,9 +21,6 @@ def test_execute(
     migrations_executed: None,  # Used implicitly
     external_dataproducts_created: None,  # Used implicitly
     dummy_logging: None,  # Used implicitly
-    database_name: str,
-    table_name: str,
-    metering_point_id: str,
 ) -> None:
     # Arrange
     orchestration_instance_id = str(uuid.uuid4())
@@ -67,7 +31,7 @@ def test_execute(
     execute()
 
     # Assert
-    actual_table = spark.read.table(f"{database_name}.{table_name}").where(
-        F.col(ContractColumnNames.metering_point_id) == metering_point_id
-    )
-    assert actual_table.count() > 0
+    actual_calculated_measurements = spark.read.table(
+        f"{CalculatedMeasurementsInternalDatabaseDefinition.DATABASE_NAME}.{CalculatedMeasurementsInternalDatabaseDefinition.MEASUREMENTS_TABLE_NAME}"
+    ).where(F.col(ContractColumnNames.orchestration_instance_id) == orchestration_instance_id)
+    assert actual_calculated_measurements.count() > 0
