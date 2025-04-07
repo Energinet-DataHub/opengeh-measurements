6a9efbbd9b61ff8dca1c351aed0e644972cb3f1e
Felix Achter <fa@halfspace.io>
2025-04-07 15:58:12 +0200

    using latest covernator streamlit functionality

--- source/geh_calculated_measurements/src/covernator/run_covernator.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from geh_common.testing.covernator import run_covernator
-
-output_folder = Path("/workspace/source/geh_calculated_measurements/src/covernator/output_files")
-
-run_covernator(output_folder, Path("/workspace/source/geh_calculated_measurements/tests"))
