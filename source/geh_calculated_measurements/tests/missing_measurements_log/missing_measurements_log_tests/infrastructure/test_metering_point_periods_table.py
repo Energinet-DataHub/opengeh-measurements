b45fa7c35fcdaa30b80d6eff4647e21cfd80a4bd
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-08 14:19:50 +0200

    add MeteringPointPeriodsTable

--- source/geh_calculated_measurements/tests/missing_measurements_log/missing_measurements_log_tests/infrastructure/test_metering_point_periods_table.py
+++ source/geh_calculated_measurements/tests/missing_measurements_log/missing_measurements_log_tests/infrastructure/test_metering_point_periods_table.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from datetime import datetime, timezone
 
 import pytest
 from geh_common.domain.types import MeteringPointResolution
@@ -7,10 +6,10 @@ from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql import functions as F
 
 from geh_calculated_measurements.missing_measurements_log.domain import MeteringPointPeriods
+from geh_calculated_measurements.missing_measurements_log.infrastructure import MeteringPointPeriodsTable
 from geh_calculated_measurements.missing_measurements_log.infrastructure.database_definitions import (
     MeteringPointPeriodsDatabaseDefinition,
 )
-from geh_calculated_measurements.missing_measurements_log.infrastructure.repository import Repository
 from tests import SPARK_CATALOG_NAME
 
 TABLE_OR_VIEW_NAME = f"{MeteringPointPeriodsDatabaseDefinition.DATABASE_NAME}.{MeteringPointPeriodsDatabaseDefinition.METERING_POINT_PERIODS}"
@@ -24,8 +23,8 @@ def valid_dataframe(spark: SparkSession) -> DataFrame:
                 "123456789012345",
                 "804",
                 MeteringPointResolution.HOUR.value,
-                datetime(2022, 1, 1, 0, tzinfo=ZoneInfo("Europe/Copenhagen")),
-                datetime(2022, 1, 1, 1, tzinfo=ZoneInfo("Europe/Copenhagen")),
+                datetime(2022, 1, 1, 0, tzinfo=timezone.utc),
+                datetime(2022, 1, 1, 1, tzinfo=timezone.utc),
             ),
         ],
         schema=MeteringPointPeriods.schema,
@@ -35,13 +34,13 @@ def valid_dataframe(spark: SparkSession) -> DataFrame:
 
 
 @pytest.fixture(scope="module")
-def repository(spark: SparkSession) -> Repository:
-    return Repository(spark, catalog_name=SPARK_CATALOG_NAME)
+def metering_point_periods_table() -> MeteringPointPeriodsTable:
+    return MeteringPointPeriodsTable(catalog_name=SPARK_CATALOG_NAME)
 
 
 def test__when_invalid_contract__raises_with_useful_message(
     valid_dataframe: DataFrame,
-    repository: Repository,
+    metering_point_periods_table: MeteringPointPeriodsTable,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     # Arrange
@@ -50,7 +49,7 @@ def test__when_invalid_contract__raises_with_useful_message(
     def mock_read_table(*args, **kwargs):
         return invalid_df
 
-    monkeypatch.setattr(Repository, "_read", mock_read_table)
+    monkeypatch.setattr(MeteringPointPeriodsTable, "_read", mock_read_table)
 
     # Assert
     with pytest.raises(
@@ -58,12 +57,12 @@ def test__when_invalid_contract__raises_with_useful_message(
         match=r"The data source does not comply with the contract.*",
     ):
         # Act
-        repository.read_metering_point_periods()
+        metering_point_periods_table.read()
 
 
 def test__when_source_contains_unexpected_columns__returns_data_without_unexpected_column(
     valid_dataframe: DataFrame,
-    repository: Repository,
+    metering_point_periods_table: MeteringPointPeriodsTable,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     """Test that the repository can handle columns being added as it is defined to _not_ be a breaking change.
@@ -74,10 +73,10 @@ def test__when_source_contains_unexpected_columns__returns_data_without_unexpect
     def mock_read_table(*args, **kwargs):
         return valid_df_with_extra_col
 
-    monkeypatch.setattr(Repository, "_read", mock_read_table)
+    monkeypatch.setattr(MeteringPointPeriodsTable, "_read", mock_read_table)
 
     # Act
-    actual = repository.read_metering_point_periods()
+    actual = metering_point_periods_table.read()
 
     # Assert
     assert actual.df.schema == MeteringPointPeriods.schema
