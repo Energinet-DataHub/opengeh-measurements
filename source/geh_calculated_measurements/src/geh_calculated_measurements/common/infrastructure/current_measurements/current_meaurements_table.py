dd2b3613822db5ed232a3bae01b421b7a2ee4851
Alexander <alexanderharrow@hotmail.com>
2025-04-08 13:09:23 +0200

    Update current_meaurements_table.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/current_measurements/current_meaurements_table.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/current_measurements/current_meaurements_table.py
@@ -1,4 +1,4 @@
-import pyspark.sql.types as t
+﻿import pyspark.sql.types as t
 
 from geh_calculated_measurements.common.infrastructure.current_measurements.database_definitions import (
     MeasurementsGoldDatabaseDefinition,
@@ -8,7 +8,7 @@ from geh_calculated_measurements.common.infrastructure.current_measurements.tabl
 
 class CurrentMeasurementsTable(Table):
     def __init__(self, catalog_name: str) -> None:
-        self.fully_qualified_name = f"{catelog_name}.{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}"
+        self.fully_qualified_name = f"{catalog_name}.{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}"
 
     metering_point_id = t.StructField("metering_point_id", t.StringType(), True)
     observation_time = t.StructField("observation_time", t.TimestampType(), True)
