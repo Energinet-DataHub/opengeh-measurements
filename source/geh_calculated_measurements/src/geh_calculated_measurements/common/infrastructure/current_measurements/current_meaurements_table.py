3cc5bb4eb75f2b17efaf9173495de85d1c662f9e
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-09 13:43:13 +0200

    Update current_meaurements_table.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/current_measurements/current_meaurements_table.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/common/infrastructure/current_measurements/current_meaurements_table.py
@@ -10,8 +10,8 @@ class CurrentMeasurementsTable(Table):
     def __init__(self, catalog_name: str) -> None:
         self.fully_qualified_name = f"{catalog_name}.{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}"
 
-    metering_point_id = t.StructField("metering_point_id", t.StringType(), True)
-    observation_time = t.StructField("observation_time", t.TimestampType(), True)
-    quantity = t.StructField("quantity", t.DecimalType(18, 3), True)
-    quality = t.StructField("quality", t.StringType(), True)
-    metering_point_type = t.StructField("metering_point_type", t.StringType(), True)
+    metering_point_id = t.StructField("metering_point_id", t.StringType(), False)
+    observation_time = t.StructField("observation_time", t.TimestampType(), False)
+    quantity = t.StructField("quantity", t.DecimalType(18, 3), False)
+    quality = t.StructField("quality", t.StringType(), False)
+    metering_point_type = t.StructField("metering_point_type", t.StringType(), False)
