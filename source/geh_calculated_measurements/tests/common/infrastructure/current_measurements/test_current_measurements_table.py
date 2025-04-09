438e5dc4ceca4cab6670840f42bc3d9fc89bf735
Alexander <alexanderharrow@hotmail.com>
2025-04-09 11:22:47 +0200

    Updates

--- source/geh_calculated_measurements/tests/common/infrastructure/current_measurements/test_current_measurements_table.py
+++ source/geh_calculated_measurements/tests/common/infrastructure/current_measurements/test_current_measurements_table.py
@@ -4,25 +4,22 @@ from decimal import Decimal
 import pytest
 from geh_common.data_products.measurements_core.measurements_gold.current_v1 import current_v1
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 from pyspark.sql.dataframe import DataFrame
 
 from geh_calculated_measurements.common.infrastructure.current_measurements.current_meaurements_table import (
     CurrentMeasurementsTable,
 )
-from geh_calculated_measurements.common.infrastructure.current_measurements.database_definitions import (
-    MeasurementsGoldDatabaseDefinition,
-)
 from tests.conftest import SPARK_CATALOG_NAME
 
 
 @pytest.fixture(scope="module")
-def current_measurements(spark: SparkSession) -> CurrentMeasurementsTable:
-    spark.sql(f"CREATE DATABASE IF NOT EXISTS {MeasurementsGoldDatabaseDefinition.DATABASE_NAME}")
+def current_measurements_table(spark: SparkSession) -> CurrentMeasurementsTable:
     return CurrentMeasurementsTable(SPARK_CATALOG_NAME)
 
 
 @pytest.fixture(scope="module")
-def expected(spark: SparkSession) -> DataFrame:
+def valid_df(spark: SparkSession) -> DataFrame:
     return spark.createDataFrame(
         [
             (
@@ -38,20 +35,64 @@ def expected(spark: SparkSession) -> DataFrame:
 
 
 def test__current_measurements_read__has_correct_schema_and_records(
-    current_measurements: CurrentMeasurementsTable,
-    expected: DataFrame,
+    current_measurements_table: CurrentMeasurementsTable,
+    valid_df: DataFrame,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     # Arrange
-    def mock_read() -> DataFrame:
-        return expected
+    def mock_read_table() -> DataFrame:
+        return valid_df
 
-    monkeypatch.setattr(current_measurements, "read", mock_read)
+    monkeypatch.setattr(current_measurements_table, "_read", mock_read_table)
 
     # Act
-    actual = current_measurements.read()
+    actual = current_measurements_table.read()
 
     # Assert
-    assert actual.collect() == expected.collect()
-    assert actual.schema == expected.schema
+    assert actual.collect() == valid_df.collect()
+    assert actual.schema == valid_df.schema
+    assert actual.schema == current_v1
+
+
+def test__when_invalid_contract__raises_with_useful_message(
+    current_measurements_table: CurrentMeasurementsTable,
+    valid_df: DataFrame,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    # Arrange
+    invalid_df = valid_df.drop(F.col("quantity"))
+
+    def mock_read_table(*args, **kwargs) -> DataFrame:
+        return invalid_df
+
+    monkeypatch.setattr(CurrentMeasurementsTable, "_read", mock_read_table)
+
+    # Assert
+    with pytest.raises(
+        Exception,
+        match=r"The data source does not comply with the contract.*",
+    ):
+        # Act
+        current_measurements_table.read()
+
+
+def test__when_source_contains_unexpected_columns__returns_data_without_unexpected_column(
+    current_measurements_table: CurrentMeasurementsTable,
+    valid_df: DataFrame,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test that the table can handle columns being added as it is defined to _not_ be a breaking change.
+    The table should return the data without the unexpected column."""
+    # Arrange
+    valid_df_with_extra_col = valid_df.withColumn("extra_col", F.lit("extra_value"))
+
+    def mock_read_table(*args, **kwargs) -> DataFrame:
+        return valid_df_with_extra_col
+
+    monkeypatch.setattr(CurrentMeasurementsTable, "_read", mock_read_table)
+
+    # Act
+    actual = current_measurements_table.read()
+
+    # Assert
     assert actual.schema == current_v1
