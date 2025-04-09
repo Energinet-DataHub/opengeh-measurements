b0d75de2c8f09c119235090c1785abba9e053f0e
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-04-09 13:03:10 +0200

    refac: Migration Executed in integration tests (#617)

--- source/core/tests/integration/bronze/infrastructure/repositories/test_submitted_transactions_quarantined_repository.py
+++ source/core/tests/integration/bronze/infrastructure/repositories/test_submitted_transactions_quarantined_repository.py
@@ -9,7 +9,7 @@ from core.settings.bronze_settings import BronzeSettings
 from tests.helpers.builders.submitted_transactions_quarantined_builder import SubmittedTransactionsQuarantinedBuilder
 
 
-def test__append__should_append_to_table(spark: SparkSession, migrations_executed) -> None:
+def test__append__should_append_to_table(spark: SparkSession) -> None:
     # Arrange
     transaction_id = identifier_helper.generate_random_string()
     submitted_transactions_quarantined = (
