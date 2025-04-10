a9f4d4924d0eab407eb05e4b6ed3422ca58dd84c
HenrikSommer <sommerh@gmail.com>
2025-04-10 14:44:29 +0200

    Parametrize GetMeasurementsQuery

---
+++ source/dotnet/Measurements.UnitTests/Infrastructure/Persistence/Queries/GetMeasurementsQueryTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture.Xunit2;
+using Energinet.DataHub.Measurements.Application.Extensions.Options;
+using Energinet.DataHub.Measurements.Infrastructure.Persistence;
+using Energinet.DataHub.Measurements.Infrastructure.Persistence.Queries;
+using NodaTime;
+using Xunit;
+
+namespace Energinet.DataHub.Measurements.UnitTests.Infrastructure.Persistence.Queries;
+
+public class GetMeasurementsQueryTests
+{
+    [Theory]
+    [AutoData]
+    public void ToString_Returns_ExpectedResult(string meteringPointId, Instant startDate, Instant endDate)
+    {
+        // Arrange
+        var databricksSchemaOptions = new DatabricksSchemaOptions { CatalogName = "spark_catalog", SchemaName = "schema_name" };
+
+        var expectedResult =
+            $"with most_recent as (" +
+            $"select row_number() over (partition by {MeasurementsGoldConstants.MeteringPointIdColumnName}, {MeasurementsGoldConstants.ObservationTimeColumnName} order by {MeasurementsGoldConstants.TransactionCreationDatetimeColumnName} desc) as row, " +
+            $"{MeasurementsGoldConstants.MeteringPointIdColumnName}, {MeasurementsGoldConstants.UnitColumnName}, {MeasurementsGoldConstants.ObservationTimeColumnName}, {MeasurementsGoldConstants.QuantityColumnName}, {MeasurementsGoldConstants.QualityColumnName}, {MeasurementsGoldConstants.IsCancelledColumnName}, {MeasurementsGoldConstants.CreatedColumnName} " +
+            $"from {databricksSchemaOptions.CatalogName}.{databricksSchemaOptions.SchemaName}.{MeasurementsGoldConstants.TableName} " +
+            $"where {MeasurementsGoldConstants.MeteringPointIdColumnName} = :{QueryParameterConstants.MeteringPointIdParameter} " +
+            $"and {MeasurementsGoldConstants.ObservationTimeColumnName} >= :{QueryParameterConstants.ObservationTimeFromParameter} " +
+            $"and {MeasurementsGoldConstants.ObservationTimeColumnName} < :{QueryParameterConstants.ObservationTimeToParameter} " +
+            $") " +
+            $"select {MeasurementsGoldConstants.MeteringPointIdColumnName}, {MeasurementsGoldConstants.UnitColumnName}, {MeasurementsGoldConstants.ObservationTimeColumnName}, {MeasurementsGoldConstants.QuantityColumnName}, {MeasurementsGoldConstants.QualityColumnName}, {MeasurementsGoldConstants.CreatedColumnName} " +
+            $"from most_recent " +
+            $"where row = 1 " +
+            $"and not {MeasurementsGoldConstants.IsCancelledColumnName} " +
+            $"order by {MeasurementsGoldConstants.ObservationTimeColumnName}";
+
+        var getMeasurementsQuery = new GetMeasurementsQuery(meteringPointId, startDate, endDate, databricksSchemaOptions);
+
+        // Act
+        var result = getMeasurementsQuery.ToString();
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
