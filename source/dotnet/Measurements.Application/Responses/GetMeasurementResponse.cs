f07b44d2a6c97c890ce7f36051e60d5dec5a2ce0
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-04-10 09:29:27 +0200

    wip

--- source/dotnet/Measurements.Application/Responses/GetMeasurementResponse.cs
+++ source/dotnet/Measurements.Application/Responses/GetMeasurementResponse.cs
@@ -3,12 +3,15 @@ using System.Text.Json.Serialization;
 using Energinet.DataHub.Measurements.Application.Exceptions;
 using Energinet.DataHub.Measurements.Application.Persistence;
 using Energinet.DataHub.Measurements.Domain;
+using NodaTime;
 
 namespace Energinet.DataHub.Measurements.Application.Responses;
 
 public class GetMeasurementResponse
 {
     public IReadOnlyCollection<Point> Points { get; init; } = [];
+
+    public SortedList<Instant, Point> Points2 { get; init; } = [];
 
     [JsonConstructor]
     [Browsable(false)]
@@ -31,6 +34,11 @@ public class GetMeasurementResponse
                     ResolutionParser.ParseResolution(measurement.Resolution),
                     measurement.Created))
             .ToList();
+
+        foreach (var measurement in measurements)
+        {
+
+        }
 
         return points.Count <= 0
             ? throw new MeasurementsNotFoundDuringPeriodException()
