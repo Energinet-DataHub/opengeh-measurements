21f1b35374f7a5ce9735cc41ae97cfb3f1d5a68c
Alexander <alexanderharrow@hotmail.com>
2025-04-10 10:25:04 +0200

    Updates

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/infrastructure/metering_point_periods_table.py
@@ -0,0 +1,23 @@
+﻿import pyspark.sql.types as T
+from pyspark.sql import DataFrame
+
+from geh_calculated_measurements.common.domain import ContractColumnNames
+from geh_calculated_measurements.common.infrastructure import Table
+from geh_calculated_measurements.missing_measurements_log.infrastructure.database_definitions import (
+    MeteringPointPeriodsDatabaseDefinition,
+)
+
+
+class MeteringPointPeriodsTable(Table):
+    def __init__(self, catalog_name: str) -> None:
+        self.fully_qualified_name = f"{catalog_name}.{MeteringPointPeriodsDatabaseDefinition.DATABASE_NAME}.{MeteringPointPeriodsDatabaseDefinition.METERING_POINT_PERIODS}"
+        super().__init__()
+
+    metering_point_id = T.StructField(ContractColumnNames.metering_point_id, T.StringType(), False)
+    grid_area_code = T.StructField(ContractColumnNames.grid_area_code, T.StringType(), False)
+    resolution = T.StructField(ContractColumnNames.resolution, T.StringType(), False)
+    period_from_date = T.StructField(ContractColumnNames.period_from_date, T.TimestampType(), False)
+    period_to_date = T.StructField(ContractColumnNames.period_to_date, T.TimestampType(), True)
+
+    def read(self) -> DataFrame:
+        return self.spark.table(self.fully_qualified_name)
