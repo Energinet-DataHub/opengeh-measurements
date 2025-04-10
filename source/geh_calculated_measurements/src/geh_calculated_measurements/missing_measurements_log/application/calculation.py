2fa0db1426404eec1a6d61e1aa924e24cd4f3d96
Alexander <alexanderharrow@hotmail.com>
2025-04-10 15:03:30 +0200

    Update calculation.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
@@ -1,25 +1,20 @@
 from geh_common.telemetry.decorators import use_span
 from pyspark.sql import SparkSession
 
-from geh_calculated_measurements.common.infrastructure import (
-    CurrentMeasurementsTable,
-)
 from geh_calculated_measurements.missing_measurements_log.application.missing_measurements_log_args import (
     MissingMeasurementsLogArgs,
 )
-from geh_calculated_measurements.missing_measurements_log.domain import (
-    execute,
-)
-from geh_calculated_measurements.missing_measurements_log.infrastructure import MeteringPointPeriodsTable
+from geh_calculated_measurements.missing_measurements_log.domain import MissingMeasurementsLogTable
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: MissingMeasurementsLogArgs) -> None:
-    # Create repositories to obtain data frames
-    metering_point_periods_table = MeteringPointPeriodsTable(args.catalog_name)
-    current_measurements_table = CurrentMeasurementsTable(args.catalog_name)
+    missing_measurements_log_table = MissingMeasurementsLogTable(
+        args.catalog_name, args.time_zone, args.orchestration_instance_id
+    )
+    # option 1
+    df = missing_measurements_log_table.read()
+    missing_measurements_log_table.write(df)
 
-    # Read data frames
-    metering_point_periods = metering_point_periods_table.read()
-    measurements = current_measurements_table.read()
-    execute(metering_point_periods, measurements)
+    # option 2
+    missing_measurements_log_table.write()
