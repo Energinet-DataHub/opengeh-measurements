3582950cec860cc551b75501791036e59b4f7987
Bjarke Meier <hej@bjarkemeier.dk>
2025-04-10 11:02:11 +0000

    refac: Update schema references for metering point periods and remove unused schema file

--- source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/infrastructure/electricity_market/schema.py
+++
@@ -1,6 +0,0 @@
-from geh_common.data_products.electricity_market_measurements_input.capacity_settlement_metering_point_periods_v1 import (
-    schema,
-)
-
-# TODO BJM
-metering_point_periods_v1 = schema
