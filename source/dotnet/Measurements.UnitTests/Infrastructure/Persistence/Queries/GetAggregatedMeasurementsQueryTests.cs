a69ff28a4fcc9d38661970ad3cc0b09202fc34fd
HenrikSommer <sommerh@gmail.com>
2025-04-10 14:10:29 +0200

    Parametrize GetAggregatedMeasurementsQuery

--- source/dotnet/Measurements.UnitTests/Infrastructure/Persistence/Queries/GetAggregatedMeasurementsQueryTests.cs
+++ source/dotnet/Measurements.UnitTests/Infrastructure/Persistence/Queries/GetAggregatedMeasurementsQueryTests.cs
@@ -1,37 +1,33 @@
-﻿using Energinet.DataHub.Core.Databricks.SqlStatementExecution;
+﻿using System.ComponentModel.DataAnnotations;
+using AutoFixture.Xunit2;
 using Energinet.DataHub.Measurements.Application.Extensions.Options;
-using Energinet.DataHub.Measurements.Infrastructure.Extensions;
+using Energinet.DataHub.Measurements.Infrastructure.Persistence;
+using Energinet.DataHub.Measurements.Infrastructure.Persistence.Queries;
 using NodaTime;
+using Xunit;
 
-namespace Energinet.DataHub.Measurements.Infrastructure.Persistence.Queries;
+namespace Energinet.DataHub.Measurements.UnitTests.Infrastructure.Persistence.Queries;
 
-public class GetAggregatedMeasurementsQuery : DatabricksStatement
+public class GetAggregatedMeasurementsQueryTests
 {
-    private const string EuropeCopenhagenTimeZone = "Europe/Copenhagen";
-
-    private readonly string _meteringPointId;
-    private readonly YearMonth _yearMonth;
-    private readonly DatabricksSchemaOptions _databricksSchemaOptions;
-
-    public GetAggregatedMeasurementsQuery(string meteringPointId, YearMonth yearMonth, DatabricksSchemaOptions databricksSchemaOptions)
+    [Theory]
+    [AutoData]
+    public void ToString_Returns_ExpectedResult(string meteringPointId, [Range(-9998, 9999)]int year, [Range(1, 12)]int month)
     {
-        _meteringPointId = meteringPointId;
-        _yearMonth = yearMonth;
-        _databricksSchemaOptions = databricksSchemaOptions;
-    }
+        // Arrange
+        const string europeCopenhagenTimeZone = "Europe/Copenhagen";
+        var yearMonth = new YearMonth(year, month);
+        var databricksSchemaOptions = new DatabricksSchemaOptions { CatalogName = "spark_catalog", SchemaName = "schema_name" };
 
-    protected override string GetSqlStatement()
-    {
         const string groupByStatement = $"{MeasurementsGoldConstants.MeteringPointIdColumnName}" +
-                                        $", year(from_utc_timestamp(cast({MeasurementsGoldConstants.ObservationTimeColumnName} as timestamp), '{EuropeCopenhagenTimeZone}'))" +
-                                        $", month(from_utc_timestamp(cast({MeasurementsGoldConstants.ObservationTimeColumnName} as timestamp), '{EuropeCopenhagenTimeZone}'))" +
-                                        $", dayofmonth(from_utc_timestamp(cast({MeasurementsGoldConstants.ObservationTimeColumnName} as timestamp), '{EuropeCopenhagenTimeZone}'))";
+                                        $", year(from_utc_timestamp(cast({MeasurementsGoldConstants.ObservationTimeColumnName} as timestamp), '{europeCopenhagenTimeZone}'))" +
+                                        $", month(from_utc_timestamp(cast({MeasurementsGoldConstants.ObservationTimeColumnName} as timestamp), '{europeCopenhagenTimeZone}'))" +
+                                        $", dayofmonth(from_utc_timestamp(cast({MeasurementsGoldConstants.ObservationTimeColumnName} as timestamp), '{europeCopenhagenTimeZone}'))";
 
-        return
-            $"with most_recent as (" +
+        var expectedResult = $"with most_recent as (" +
             $"select row_number() over (partition by {MeasurementsGoldConstants.MeteringPointIdColumnName}, {MeasurementsGoldConstants.ObservationTimeColumnName} order by {MeasurementsGoldConstants.TransactionCreationDatetimeColumnName} desc) as row, " +
             $"{MeasurementsGoldConstants.MeteringPointIdColumnName}, {MeasurementsGoldConstants.UnitColumnName}, {MeasurementsGoldConstants.ObservationTimeColumnName}, {MeasurementsGoldConstants.QuantityColumnName}, {MeasurementsGoldConstants.QualityColumnName}, {MeasurementsGoldConstants.ResolutionColumnName}, {MeasurementsGoldConstants.IsCancelledColumnName} " +
-            $"from {_databricksSchemaOptions.CatalogName}.{_databricksSchemaOptions.SchemaName}.{MeasurementsGoldConstants.TableName} " +
+            $"from {databricksSchemaOptions.CatalogName}.{databricksSchemaOptions.SchemaName}.{MeasurementsGoldConstants.TableName} " +
             $"where {MeasurementsGoldConstants.MeteringPointIdColumnName} = :{QueryParameterConstants.MeteringPointIdParameter} " +
             $"and {MeasurementsGoldConstants.ObservationTimeColumnName} >= :{QueryParameterConstants.ObservationTimeFromParameter} " +
             $"and {MeasurementsGoldConstants.ObservationTimeColumnName} < :{QueryParameterConstants.ObservationTimeToParameter} " +
@@ -48,18 +44,13 @@ public class GetAggregatedMeasurementsQuery : DatabricksStatement
             $"and not {MeasurementsGoldConstants.IsCancelledColumnName} " +
             $"group by {groupByStatement} " +
             $"order by {AggregatedMeasurementsConstants.MinObservationTime}";
-    }
 
-    protected override IReadOnlyCollection<QueryParameter> GetParameters()
-    {
-        var (startDate, endDate) = _yearMonth.ToDateInterval();
+        var getAggregatedMeasurementsQuery = new GetAggregatedMeasurementsQuery(meteringPointId, yearMonth, databricksSchemaOptions);
 
-        List<QueryParameter> parameters = [
-            QueryParameter.Create(QueryParameterConstants.MeteringPointIdParameter, _meteringPointId),
-            QueryParameter.Create(QueryParameterConstants.ObservationTimeFromParameter, startDate.ToUtcString()),
-            QueryParameter.Create(QueryParameterConstants.ObservationTimeToParameter, endDate.ToUtcString())
-        ];
+        // Act
+        var result = getAggregatedMeasurementsQuery.ToString();
 
-        return parameters;
+        // Assert
+        Assert.Equal(expectedResult, result);
     }
 }
