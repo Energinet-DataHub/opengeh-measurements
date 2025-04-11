145cfb07327920342a082513ca49bd653f68b65d
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-04-11 09:36:38 +0200

    fix test

--- source/core/src/core/silver/domain/validations/enum_validations.py
+++ source/core/src/core/silver/domain/validations/enum_validations.py
@@ -2,13 +2,13 @@ import pyspark.sql.functions as F
 from geh_common.domain.types.metering_point_resolution import MeteringPointResolution as GehCommonResolution
 from geh_common.domain.types.metering_point_type import MeteringPointType as GehCommonMeteringPointType
 from geh_common.domain.types.orchestration_type import OrchestrationType as GehCommonOrchestrationType
+from geh_common.domain.types.quantity_quality import QuantityQuality as GehCommonQuality
 from geh_common.domain.types.quantity_unit import QuantityUnit as GehCommonUnit
 from pyspark.sql import Column
 
 from core.bronze.domain.constants.column_names.submitted_transactions_unpacked_column_names import (
     SubmittedTransactionsUnpackedColumnNames,
 )
-from core.contracts.process_manager.PersistSubmittedTransaction.generated.PersistSubmittedTransaction_pb2 import Quality
 from core.utility.columns_helper import invalid_if_null
 
 
@@ -20,7 +20,7 @@ def validate_orchestration_type_enum() -> Column:
 
 def validate_quality_enum() -> Column:
     """Quality check: QCST01-02."""
-    valid_qualities = [name for name in Quality.values() if name != Quality.Q_UNSPECIFIED]
+    valid_qualities = [name.value for name in GehCommonQuality]
 
     return invalid_if_null(SubmittedTransactionsUnpackedColumnNames.points).otherwise(
         F.forall(
