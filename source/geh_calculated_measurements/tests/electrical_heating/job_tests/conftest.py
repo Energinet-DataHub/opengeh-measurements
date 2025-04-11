b685a600bcccc3a6db746b690b02a1380fb9de78
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-11 18:09:29 +0200

    ....

--- source/geh_calculated_measurements/tests/electrical_heating/job_tests/conftest.py
+++ source/geh_calculated_measurements/tests/electrical_heating/job_tests/conftest.py
@@ -2,11 +2,8 @@ import pytest
 from geh_common.pyspark.read_csv import read_csv_path
 from pyspark.sql import SparkSession
 
-from geh_calculated_measurements.common.domain import CurrentMeasurements
-from geh_calculated_measurements.common.infrastructure.current_measurements.database_definitions import (
-    MeasurementsGoldDatabaseDefinition,
-)
 from tests.electrical_heating.job_tests import get_test_files_folder_path
+from tests.external_data_products import ExternalDataProducts
 
 
 @pytest.fixture(scope="session")
@@ -14,10 +11,13 @@ def gold_table_seeded(
     spark: SparkSession,
     external_dataproducts_created: None,  # Used implicitly
 ) -> None:
-    file_name = f"{get_test_files_folder_path()}/{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}-{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}.csv"
-    time_series_points = read_csv_path(spark, file_name, CurrentMeasurements.schema)
+    database_name = ExternalDataProducts.CURRENT_MEASUREMENTS.database_name
+    table_name = ExternalDataProducts.CURRENT_MEASUREMENTS.view_name
+    schema = ExternalDataProducts.CURRENT_MEASUREMENTS.schema
+    file_name = f"{get_test_files_folder_path()}/{database_name}-{table_name}.csv"
+    time_series_points = read_csv_path(spark, file_name, schema)
     time_series_points.write.saveAsTable(
-        f"{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}",
+        f"{database_name}.{table_name}",
         format="delta",
         mode="overwrite",
     )
---
+++ source/geh_calculated_measurements/tests/external_data_products.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from typing import Any
+
+from geh_common.data_products.measurements_core.measurements_gold import current_v1 as current
+
+
+@dataclass
+class DataProduct:
+    database_name: str
+    view_name: str
+    schema: Any
+
+
+@dataclass
+class ExternalDataProducts:
+    CURRENT_MEASUREMENTS: DataProduct = DataProduct(
+        database_name=current.database_name,
+        view_name=current.view_name,
+        schema=current.schema,
+    )
+
+    @staticmethod
+    def get_all_database_names() -> list[str]:
+        return [
+            getattr(DataProduct, attr).database_name
+            for attr in dir(ExternalDataProducts)
+            if isinstance(getattr(ExternalDataProducts, attr), DataProduct)
+        ]
+
+    @staticmethod
+    def get_all_dataproducts() -> list[DataProduct]:
+        return [
+            getattr(ExternalDataProducts, attr)
+            for attr in dir(ExternalDataProducts)
+            if isinstance(getattr(ExternalDataProducts, attr), DataProduct)
+        ]
