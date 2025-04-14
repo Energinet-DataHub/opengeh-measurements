beab9777d325a152a5c528f47fae3c2223d5539e
jesper knudsen <jmk@halfspace.io>
2025-04-14 12:33:29 +0000

    all scenario test pass when run individually

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/cenc_logic.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from typing import Tuple
+
+from geh_common.telemetry import use_span
+from pyspark.sql import DataFrame
+
+from geh_calculated_measurements.common.domain import CurrentMeasurements
+from geh_calculated_measurements.net_consumption_group_6.domain import (
+    ChildMeteringPoints,
+    ConsumptionMeteringPointPeriods,
+)
+from geh_calculated_measurements.net_consumption_group_6.domain.cenc import calculate_cenc
+from geh_calculated_measurements.net_consumption_group_6.domain.daily import calculate_daily
+
+
+@use_span()
+def execute_logic(
+    current_measurements: CurrentMeasurements,
+    consumption_metering_point_periods: ConsumptionMeteringPointPeriods,
+    child_metering_points: ChildMeteringPoints,
+    time_zone: str,
+    execution_start_datetime: datetime,
+) -> Tuple[DataFrame, DataFrame]:
+    cenc = calculate_cenc(
+        consumption_metering_point_periods,
+        child_metering_points,
+        current_measurements,
+        time_zone,
+        execution_start_datetime,
+    )
+
+    measurements = calculate_daily(
+        current_measurements=current_measurements,
+        cenc=cenc,
+        time_zone=time_zone,
+        execution_start_datetime=execution_start_datetime,
+    )
+
+    return cenc, measurements
