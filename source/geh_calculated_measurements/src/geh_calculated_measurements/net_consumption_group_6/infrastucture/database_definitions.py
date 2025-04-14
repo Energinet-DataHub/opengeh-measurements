cc3f8fcd51c2f66c79b341e64ba86b636223077f
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-14 06:49:36 +0000

    update

--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/infrastucture/database_definitions.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/infrastucture/database_definitions.py
@@ -4,7 +4,7 @@ class ElectricityMarketMeasurementsInputDatabaseDefinition:
     The database and tables are all defined in Terraform and should be equivalent here for reference.
     """
 
-    DATABASE_NAME = "test"  # "electricity_market_measurements_input"
+    DATABASE_NAME = "electricity_market_measurements_input"
     NET_CONSUMPTION_GROUP_6_CHILD_METERING_POINT = "net_consumption_group_6_child_metering_point_v1"
     NET_CONSUMPTION_GROUP_6_CONSUMPTION_METERING_POINT_PERIODS = (
         "net_consumption_group_6_consumption_metering_point_periods_v1"
