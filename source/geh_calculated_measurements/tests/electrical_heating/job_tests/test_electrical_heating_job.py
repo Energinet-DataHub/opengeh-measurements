7b9e9102ecf25b62f3e04e6fa9561d39978c4eb4
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-14 07:45:06 +0000

    update

--- source/geh_calculated_measurements/tests/electrical_heating/job_tests/test_electrical_heating_job.py
+++ source/geh_calculated_measurements/tests/electrical_heating/job_tests/test_electrical_heating_job.py
@@ -3,57 +3,23 @@ import sys
 import uuid
 from typing import Any
 
-from geh_common.pyspark.read_csv import read_csv_path
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StringType
 
-from geh_calculated_measurements.common.domain import ContractColumnNames, CurrentMeasurements
-from geh_calculated_measurements.common.infrastructure import CalculatedMeasurementsInternalDatabaseDefinition
-from geh_calculated_measurements.common.infrastructure.current_measurements.database_definitions import (
-    MeasurementsGoldDatabaseDefinition,
+from geh_calculated_measurements.common.infrastructure import (
+    CalculatedMeasurementsInternalDatabaseDefinition,
 )
 from geh_calculated_measurements.electrical_heating.entry_point import execute
-from tests import create_job_environment_variables, create_random_metering_point_id
+from tests import create_job_environment_variables
 from tests.electrical_heating.job_tests import get_test_files_folder_path
-
-generate_metering_point_id_udf = F.udf(create_random_metering_point_id, StringType())
-
-
-def _seed_gold(spark: SparkSession) -> None:
-    file_name = f"{get_test_files_folder_path()}/{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}-{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}.csv"
-    # Read csv
-    time_series_points = read_csv_path(spark, file_name, CurrentMeasurements.schema)
-
-    # Get unique metering_point_ids
-    distinct_ids = time_series_points.select("metering_point_id").distinct()
-
-    # Add a new randomized ID per original
-    randomized_ids = distinct_ids.withColumn("new_metering_point_id", generate_metering_point_id_udf())
-
-    # Join back to original DataFrame
-    time_series_points = time_series_points.join(randomized_ids, on="metering_point_id", how="left")
-    time_series_points = time_series_points.select(
-        F.col("new_metering_point_id").alias("metering_point_id"),
-        "observation_time",
-        "quantity",
-        "quality",
-        "metering_point_type",
-    )
-
-    time_series_points.write.saveAsTable(
-        f"{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}",
-        format="delta",
-        mode="append",
-    )
 
 
 def test_execute(
     spark: SparkSession,
-    gold_table_seeded,
-    dummy_logging: Any,  # Used implicitly
+    gold_table_seeded: Any,  # Used implicitly
     migrations_executed: None,  # Used implicitly
     external_dataproducts_created: None,  # Used implicitly
+    dummy_logging: Any,  # Used implicitly
     monkeypatch,
 ) -> None:
     # Arrange
@@ -61,18 +27,11 @@ def test_execute(
     monkeypatch.setattr(sys, "argv", ["dummy_script_name", "--orchestration-instance-id", orchestration_instance_id])
     monkeypatch.setattr(os, "environ", create_job_environment_variables(get_test_files_folder_path()))
 
-    spark.sql(
-        f"""SELECT * FROM {MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}"""
-    ).show(truncate=False)
-
     # Act
     execute()
-    spark.read.table(
-        f"{CalculatedMeasurementsInternalDatabaseDefinition.DATABASE_NAME}.{CalculatedMeasurementsInternalDatabaseDefinition.MEASUREMENTS_TABLE_NAME}"
-    ).show(truncate=False)
 
     # Assert
     actual = spark.read.table(
         f"{CalculatedMeasurementsInternalDatabaseDefinition.DATABASE_NAME}.{CalculatedMeasurementsInternalDatabaseDefinition.MEASUREMENTS_TABLE_NAME}"
-    ).where(F.col(ContractColumnNames.orchestration_instance_id) == orchestration_instance_id)
+    ).where(F.col("orchestration_instance_id") == orchestration_instance_id)
     assert actual.count() > 0
