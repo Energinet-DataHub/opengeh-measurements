c988f3281c351826ba270dca75a569d1f64db6b2
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-16 11:44:38 +0000

    Changed from fixture into function and made overwrite into append

--- source/geh_calculated_measurements/tests/capacity_settlement/job_tests/test_capacity_settlement_job.py
+++ source/geh_calculated_measurements/tests/capacity_settlement/job_tests/test_capacity_settlement_job.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import uuid
-from typing import Any
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
@@ -10,11 +9,11 @@ from geh_calculated_measurements.capacity_settlement.entry_point import execute
 from geh_calculated_measurements.common.infrastructure import CalculatedMeasurementsInternalDatabaseDefinition
 from tests import create_job_environment_variables
 from tests.capacity_settlement.job_tests import TEST_FILES_FOLDER_PATH
+from tests.capacity_settlement.job_tests.seeding import seed_gold
 
 
 def test_execute(
     spark: SparkSession,
-    gold_table_seeded: Any,
     migrations_executed: None,  # Used implicitly
     external_dataproducts_created: None,  # Used implicitly
     dummy_logging,  # Used implicitly
@@ -33,6 +32,8 @@ def test_execute(
         ],
     )
     monkeypatch.setattr(os, "environ", create_job_environment_variables(str(TEST_FILES_FOLDER_PATH)))
+
+    seed_gold(spark)
 
     # Act
     execute()
