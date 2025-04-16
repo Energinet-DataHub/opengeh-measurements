512aa3af4ac2e257ba627cefbaf4f56164b38371
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-16 11:45:18 +0000

    update

---
+++ source/geh_calculated_measurements/tests/capacity_settlement/job_tests/seeding.py
@@ -0,0 +1,20 @@
+from geh_common.pyspark.read_csv import read_csv_path
+from pyspark.sql import SparkSession
+
+from geh_calculated_measurements.common.domain import CurrentMeasurements
+from geh_calculated_measurements.common.infrastructure.current_measurements.database_definitions import (
+    MeasurementsGoldDatabaseDefinition,
+)
+from tests.capacity_settlement.job_tests import TEST_FILES_FOLDER_PATH
+
+
+def seed_gold(
+    spark: SparkSession,
+) -> None:
+    file_name = f"{TEST_FILES_FOLDER_PATH}/{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}-{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}.csv"
+    time_series_points = read_csv_path(spark, file_name, CurrentMeasurements.schema)
+    time_series_points.write.saveAsTable(
+        f"{MeasurementsGoldDatabaseDefinition.DATABASE_NAME}.{MeasurementsGoldDatabaseDefinition.CURRENT_MEASUREMENTS}",
+        format="delta",
+        mode="append",
+    )
