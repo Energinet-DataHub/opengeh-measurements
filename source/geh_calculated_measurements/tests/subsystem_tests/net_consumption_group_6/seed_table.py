91439ba455c4ae25fe20d8682197983f94207071
Jonas Michelsen <jvm@energinet.dk>
2025-04-22 10:41:40 +0200

    hardcoded metering point ids

--- source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/seed_table.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/seed_table.py
@@ -8,7 +8,6 @@ from geh_common.data_products.electricity_market_measurements_input import (
 from geh_common.domain.types import MeteringPointType, OrchestrationType
 
 from geh_calculated_measurements.testing.utilities.job_tester import JobTestFixture
-from tests import CalculationType, create_random_metering_point_id
 from tests.subsystem_tests import seed_gold_table
 from tests.subsystem_tests.seed_gold_table import GoldTableRow
 
@@ -23,21 +22,21 @@ def _seed_gold_table(job_fixture: JobTestFixture) -> None:
     year = datetime.now().year - 1
     gold_table_rows = [
         GoldTableRow(
-            metering_point_id=create_random_metering_point_id(CalculationType.NET_CONSUMPTION),
+            metering_point_id="170000000000000201",
             metering_point_type=MeteringPointType.NET_CONSUMPTION,
             orchestration_type=OrchestrationType.NET_CONSUMPTION,
             observation_time=datetime(year, 12, 31, 23, 0, 0, tzinfo=timezone.utc),
             quantity=random.uniform(0.1, 10.0),
         ),
         GoldTableRow(
-            metering_point_id=create_random_metering_point_id(CalculationType.NET_CONSUMPTION),
+            metering_point_id="070000001500170200",
             metering_point_type=MeteringPointType.CONSUMPTION_FROM_GRID,
             orchestration_type=OrchestrationType.NET_CONSUMPTION,
             observation_time=datetime(year, 12, 31, 23, 0, 0, tzinfo=timezone.utc),
             quantity=random.uniform(0.1, 10.0),
         ),
         GoldTableRow(
-            metering_point_id=create_random_metering_point_id(CalculationType.NET_CONSUMPTION),
+            metering_point_id="060000001500170200",
             metering_point_type=MeteringPointType.SUPPLY_TO_GRID,
             orchestration_type=OrchestrationType.NET_CONSUMPTION,
             observation_time=datetime(year, 12, 31, 23, 0, 0, tzinfo=timezone.utc),
