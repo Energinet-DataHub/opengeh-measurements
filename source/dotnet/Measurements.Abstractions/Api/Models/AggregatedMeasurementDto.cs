1977a0baf70b66d75f933b65bba8407430ef72a8
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-04-23 12:39:48 +0200

    Fix wording in documentation

--- source/dotnet/Measurements.Abstractions/Api/Models/AggregatedMeasurementDto.cs
+++ source/dotnet/Measurements.Abstractions/Api/Models/AggregatedMeasurementDto.cs
@@ -7,6 +7,6 @@
 /// <param name="Quantity">Quantity of aggregated measurement.</param>
 /// <param name="Quality">Value of lowest quality among measurements in aggregation.</param>
 /// <param name="Unit">Unit of aggregated measurement.</param>
-/// <param name="MissingValues">Indicates if aggregate contains missing values during day of aggregation.</param>
-/// <param name="ContainsUpdatedValues">Indicates if measurements in aggregate has been updated since creation.</param>
+/// <param name="MissingValues">Indicates whether aggregate contains missing values during day of aggregation.</param>
+/// <param name="ContainsUpdatedValues">Indicates whether any measurements in aggregate has been updated since creation.</param>
 public sealed record MeasurementAggregationDto(DateOnly Date, decimal Quantity, Quality Quality, Unit Unit, bool MissingValues, bool ContainsUpdatedValues);
