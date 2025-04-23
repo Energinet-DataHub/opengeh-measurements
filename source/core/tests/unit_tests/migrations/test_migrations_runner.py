95cd7ce89781cf3e2f50891810b94d7133107113
Soren <XSSFR@energinet.dk>
2025-04-23 13:05:27 +0000

    reimplemented  test

--- source/core/tests/unit_tests/migrations/test_migrations_runner.py
+++ source/core/tests/unit_tests/migrations/test_migrations_runner.py
@@ -23,3 +23,24 @@ def test__migrate__calls_expected(mocker: MockerFixture):
     mock_configure_spark_sql_migrations.assert_called_once()
     mock_stop_job_runs.assert_called_once()
     mock_migrate_pipeline.assert_called_once()
+
+
+def test__stop_job_runs__calls_expected(mocker: MockerFixture):
+    # Arrange
+    mock_databricks_settings = mocker.patch(f"{sut.__name__}.DatabricksSettings")
+    mock_databricks_api_client = mocker.patch(f"{sut.__name__}.DatabricksApiClient")
+    mock_settings_instance = mock_databricks_settings.return_value
+    mock_settings_instance.databricks_jobs = "job1,job2"
+    mock_client_instance = mock_databricks_api_client.return_value
+
+    # Act
+    sut._stop_job_runs()
+
+    # Assert
+    mock_databricks_api_client.assert_called_once_with(
+        databricks_host=mock_settings_instance.databricks_workspace_url,
+        databricks_token=mock_settings_instance.databricks_token,
+    )
+    assert mock_client_instance.get_job_id.call_count == 2
+    assert mock_client_instance.get_latest_job_run.call_count == 2
+    assert mock_client_instance.cancel_job_run.call_count == 2
