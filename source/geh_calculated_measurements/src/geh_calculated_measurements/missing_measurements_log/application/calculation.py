c86457cbed0b08a204a1a83ba36329822e9b659a
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-24 13:43:29 +0200

    add domain.calculation

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
@@ -1,17 +1,28 @@
 from geh_common.telemetry.decorators import use_span
 from pyspark.sql import DataFrame, SparkSession
 
+from geh_calculated_measurements.common.infrastructure import CurrentMeasurementsRepository
 from geh_calculated_measurements.missing_measurements_log.application.missing_measurements_log_args import (
     MissingMeasurementsLogArgs,
 )
-from geh_calculated_measurements.missing_measurements_log.domain import MissingMeasurementsLogTable
+from geh_calculated_measurements.missing_measurements_log.domain import execute
+from geh_calculated_measurements.missing_measurements_log.infrastructure import MeteringPointPeriodsTable
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: MissingMeasurementsLogArgs) -> DataFrame:
-    missing_measurements_log_table = MissingMeasurementsLogTable(
-        args.catalog_name, args.time_zone, args.orchestration_instance_id, args.grid_area_codes
+    current_measurements_repository = CurrentMeasurementsRepository(spark, args.catalog_name)
+    metering_point_periods_table = MeteringPointPeriodsTable(args.catalog_name)
+
+    # Read data frames
+    current_measurements = current_measurements_repository.read_current_measurements()
+    metering_point_periods = metering_point_periods_table.read()
+
+    missing_measurements_log = execute(
+        current_measurements=current_measurements,
+        metering_point_periods=metering_point_periods,
+        time_zone=args.time_zone,
+        orchestration_instance_id=args.orchestration_instance_id,
+        grid_area_codes=args.grid_area_codes,
     )
-    df = missing_measurements_log_table.read()
-    # TODO JMG: missing_measurements_log_table.write(df)
-    return df
+    return missing_measurements_log
---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/domain/calculation.py
@@ -0,0 +1,118 @@
+from uuid import UUID
+
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from geh_common.application import GridAreaCodes
+from geh_common.domain.types import MeteringPointResolution
+from geh_common.pyspark.transformations import convert_from_utc, convert_to_utc
+from geh_common.telemetry import use_span
+from pyspark.sql import DataFrame
+
+from geh_calculated_measurements.common.domain import ContractColumnNames, CurrentMeasurements
+from geh_calculated_measurements.missing_measurements_log.infrastructure import MeteringPointPeriodsTable
+
+
+@use_span()
+def execute(
+    current_measurements: CurrentMeasurements,
+    metering_point_periods: DataFrame,
+    grid_area_codes: GridAreaCodes | None,
+    orchestration_instance_id: UUID,
+    time_zone: str,
+) -> DataFrame:
+    if grid_area_codes is not None:
+        metering_point_periods = metering_point_periods.where(
+            F.col(MeteringPointPeriodsTable.grid_area_code.name).isin(grid_area_codes)
+        )
+
+    expected_measurement_counts = _get_expected_measurement_counts(metering_point_periods, time_zone)
+    actual_measurement_counts = _get_actual_measurement_counts(current_measurements, time_zone)
+
+    missing_measurements = _get_missing_measurements(
+        expected_measurement_counts=expected_measurement_counts,
+        actual_measurement_counts=actual_measurement_counts,
+    )
+
+    return missing_measurements.withColumn(
+        ContractColumnNames.orchestration_instance_id, F.lit(orchestration_instance_id)
+    )
+
+
+def _get_expected_measurement_counts(metering_point_periods: DataFrame, time_zone: str) -> DataFrame:
+    """Calculate the expected measurement counts grouped by metering point and date."""
+    metering_point_periods_local_time = convert_from_utc(metering_point_periods, time_zone)
+
+    expected_measurement_counts = metering_point_periods_local_time.withColumn(
+        "start_of_day",
+        F.explode(
+            F.sequence(
+                F.col(MeteringPointPeriodsTable.period_from_date.name),
+                F.col(MeteringPointPeriodsTable.period_to_date.name),
+                F.expr("INTERVAL 1 DAY"),
+            )
+        ),
+    ).where(
+        # to date is exclusive
+        F.col("start_of_day") < F.col(MeteringPointPeriodsTable.period_to_date.name)
+    )
+
+    expected_measurement_counts = expected_measurement_counts.select(
+        F.col(MeteringPointPeriodsTable.metering_point_id.name),
+        F.col("start_of_day"),
+        (F.col("start_of_day") + F.expr("INTERVAL 1 DAY")).alias("end_of_day"),
+        (
+            (F.unix_timestamp(F.col("end_of_day")) - F.unix_timestamp(F.col("start_of_day")))
+            / F.when(F.col(MeteringPointPeriodsTable.resolution.name) == MeteringPointResolution.HOUR.value, 3600).when(
+                F.col(MeteringPointPeriodsTable.resolution.name) == MeteringPointResolution.QUARTER.value, 900
+            )
+            # 3600 seconds for 1 hour, 900 seconds for 15 minutes
+        ).alias("measurement_counts"),
+    ).select(
+        F.col(MeteringPointPeriodsTable.metering_point_id.name),
+        F.col("start_of_day").alias(ContractColumnNames.date),
+        F.col("measurement_counts"),
+    )
+
+    return convert_to_utc(expected_measurement_counts, time_zone)
+
+
+def _get_actual_measurement_counts(current_measurements: CurrentMeasurements, time_zone: str) -> DataFrame:
+    """Calculate the actual measurement counts grouped by metering point and date."""
+    current_measurements_df = current_measurements.df.where(
+        F.col(ContractColumnNames.observation_time).isNotNull()
+    ).where(F.col(ContractColumnNames.quality) != "missing")  # TODO Use data type from common.
+
+    current_measurements_df = convert_from_utc(current_measurements_df, time_zone)
+
+    actual_measurement_counts = (
+        current_measurements_df.withColumn(
+            ContractColumnNames.date, F.to_date(F.col(ContractColumnNames.observation_time))
+        )
+        .groupBy(ContractColumnNames.date, ContractColumnNames.metering_point_id)
+        .agg(F.count("*").alias("measurement_counts"))
+    ).select(
+        F.col(ContractColumnNames.metering_point_id),
+        F.col(ContractColumnNames.date).cast(T.TimestampType()),
+        F.col("measurement_counts"),
+    )
+
+    return convert_to_utc(actual_measurement_counts, time_zone)
+
+
+def _get_missing_measurements(
+    expected_measurement_counts: DataFrame, actual_measurement_counts: DataFrame
+) -> DataFrame:
+    return expected_measurement_counts.join(
+        actual_measurement_counts,
+        [
+            expected_measurement_counts[MeteringPointPeriodsTable.metering_point_id.name]
+            == actual_measurement_counts[ContractColumnNames.metering_point_id],
+            expected_measurement_counts[ContractColumnNames.date]
+            == actual_measurement_counts[ContractColumnNames.date],
+            expected_measurement_counts["measurement_counts"] == actual_measurement_counts["measurement_counts"],
+        ],
+        "left_anti",
+    ).select(
+        F.col(ContractColumnNames.metering_point_id),
+        F.col(ContractColumnNames.date),
+    )
