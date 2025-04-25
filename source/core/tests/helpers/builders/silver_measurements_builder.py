67564198d3194799f0927af92bf16315ccd1e3bb
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-04-25 11:17:59 +0200

    fix builder

--- source/core/tests/helpers/builders/silver_measurements_builder.py
+++ source/core/tests/helpers/builders/silver_measurements_builder.py
@@ -15,6 +15,17 @@ class SilverMeasurementsBuilder:
         self.spark = spark_session
         self.data = []
 
+    @staticmethod
+    def generate_default_points():
+        return [
+            {
+                "position": position,
+                "quantity": Decimal(1.0),
+                "quality": QuantityQuality.MEASURED.value,  # Quality is not transformed in the silver layer.
+            }
+            for position in range(1, 25)
+        ]  # 24 points for 1 day in hours
+
     def add_row(
         self,
         orchestration_type=OrchestrationType.SUBMITTED.value,
@@ -27,12 +38,10 @@ class SilverMeasurementsBuilder:
         resolution=MeteringPointResolution.HOUR.value,
         start_datetime=datetime_helper.get_datetime(year=2020, month=1),
         end_datetime=datetime_helper.get_datetime(year=2020, month=2),
-        points=None,
+        points=generate_default_points(),
         is_cancelled=False,
         created=datetime_helper.get_datetime(year=2020, month=1, day=1),
     ):
-        if points is None:
-            points = self._generate_default_points()
         self.data.append(
             (
                 orchestration_type,
@@ -58,16 +67,6 @@ class SilverMeasurementsBuilder:
             "quantity": quantity,
             "quality": quality,
         }
-
-    def _generate_default_points(self):
-        return [
-            {
-                "position": position,
-                "quantity": Decimal(1.0),
-                "quality": QuantityQuality.MEASURED.value,  # Quality is not transformed in the silver layer.
-            }
-            for position in range(1, 25)
-        ]
 
     def build(self):
         return self.spark.createDataFrame(self.data, schema=silver_measurements_schema)
