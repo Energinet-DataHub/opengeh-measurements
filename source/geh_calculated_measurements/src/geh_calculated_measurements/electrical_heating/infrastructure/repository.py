ea08653fa2f93d44bf4af11524b6827a5e2937d4
Jonas Michelsen <jvm@energinet.dk>
2025-04-28 09:05:48 +0200

    Update repository.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/repository.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/electrical_heating/infrastructure/repository.py
@@ -1,3 +1,7 @@
+from geh_common.data_products.electricity_market_measurements_input import (
+    electrical_heating_child_metering_points_v1,
+    electrical_heating_consumption_metering_point_periods_v1,
+)
 from geh_common.testing.dataframes import assert_contract
 from pyspark.sql import DataFrame, SparkSession
 
@@ -5,7 +9,6 @@ from geh_calculated_measurements.electrical_heating.domain import (
     ChildMeteringPoints,
     ConsumptionMeteringPointPeriods,
 )
-from tests.external_data_products import ExternalDataProducts
 
 
 class Repository:
@@ -18,17 +21,15 @@ class Repository:
         self._catalog_name = catalog_name
 
     def read_consumption_metering_point_periods(self) -> ConsumptionMeteringPointPeriods:
-        consumption_metering_point_periods = ExternalDataProducts.ELECTRICAL_HEATING_CONSUMPTION_METERING_POINT_PERIODS
-        table_name = f"{self._catalog_name}.{consumption_metering_point_periods.database_name}.{consumption_metering_point_periods.view_name}"
+        table_name = f"{self._catalog_name}.{electrical_heating_consumption_metering_point_periods_v1.database_name}.{electrical_heating_consumption_metering_point_periods_v1.view_name}"
         df = self._read_table(table_name)
-        assert_contract(df.schema, ExternalDataProducts.ELECTRICAL_HEATING_CONSUMPTION_METERING_POINT_PERIODS.schema)
+        assert_contract(df.schema, electrical_heating_consumption_metering_point_periods_v1.schema)
         return ConsumptionMeteringPointPeriods(df)
 
     def read_child_metering_points(self) -> ChildMeteringPoints:
-        child_metering_points = ExternalDataProducts.ELECTRICAL_HEATING_CHILD_METERING_POINTS
-        table_name = f"{self._catalog_name}.{child_metering_points.database_name}.{child_metering_points.view_name}"
+        table_name = f"{self._catalog_name}.{electrical_heating_child_metering_points_v1.database_name}.{electrical_heating_child_metering_points_v1.view_name}"
         df = self._read_table(table_name)
-        assert_contract(df.schema, ExternalDataProducts.ELECTRICAL_HEATING_CHILD_METERING_POINTS.schema)
+        assert_contract(df.schema, electrical_heating_child_metering_points_v1.schema)
         return ChildMeteringPoints(df)
 
     def _read_table(self, table_name: str) -> DataFrame:
