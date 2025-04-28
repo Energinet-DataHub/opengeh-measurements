25ee266fcb003d226be4cb9644f98544653d24a5
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-04-28 10:21:16 +0200

    Add test case

---
+++ source/dotnet/Measurements.WebApi.IntegrationTests/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Energinet.DataHub.Measurements.WebApi.IntegrationTests.Fixtures;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.Measurements.WebApi.IntegrationTests.Controllers;
+
+[IntegrationTest]
+public class ErrorControllerTests(WebApiFixture fixture) : IClassFixture<WebApiFixture>
+{
+    [Fact]
+    public async Task HandleError_WhenUnhandledException_ThenRedirectToErrorController()
+    {
+        // Arrange
+
+        // Act
+        var actual = await fixture.Client.GetAsync("/invalid-endpoint");
+        var actualContent = await actual.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Contains("An unknown error has occured", actualContent);
+    }
+}
