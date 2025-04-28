5dbec87a6c55f3b8b5578886fc708f536ebb1cb6
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-28 12:00:25 +0200

    refac: Electrical heating now reads from delta and changed overwrite to append (#647)
    Co-authored-by: Jonas Michelsen <jvm@energinet.dk>
    Co-authored-by: Jonas Michelsen <39729843+johevemi@users.noreply.github.com>

--- source/geh_calculated_measurements/tests/electrical_heating/electrical_heating_tests/infrastructure/test_repository.py
+++ source/geh_calculated_measurements/tests/electrical_heating/electrical_heating_tests/infrastructure/test_repository.py
@@ -1,78 +1,145 @@
-from pathlib import Path
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import pytest
-from pyspark.sql import SparkSession
+from geh_common.domain.types import MeteringPointSubType, MeteringPointType, NetSettlementGroup
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql import functions as F
 
 from geh_calculated_measurements.electrical_heating.infrastructure import (
     ElectricityMarketRepository,
 )
-
-_TEST_FILES_FOLDER_PATH = str(Path(__file__).parent / "test_data")
-
-
-def _create_repository_with_extra_col(spark: SparkSession) -> ElectricityMarketRepository:
-    return ElectricityMarketRepository(
-        spark,
-        _TEST_FILES_FOLDER_PATH,
-        consumption_metering_point_periods_file_name="consumption_metering_point_periods_v1_extra_col.csv",
-        child_metering_points_file_name="child_metering_points_v1_extra_col.csv",
-    )
+from tests import SPARK_CATALOG_NAME
+from tests.external_data_products import ExternalDataProducts
 
 
-def _create_repository_with_missing_col(spark: SparkSession) -> ElectricityMarketRepository:
-    return ElectricityMarketRepository(
-        spark,
-        _TEST_FILES_FOLDER_PATH,
-        consumption_metering_point_periods_file_name="consumption_metering_point_periods_v1_missing_col.csv",
-        child_metering_points_file_name="child_metering_points_v1_missing_col.csv",
-    )
+@pytest.fixture(scope="module")
+def electricity_market_repository(spark: SparkSession) -> ElectricityMarketRepository:
+    return ElectricityMarketRepository(spark, SPARK_CATALOG_NAME)
 
 
-def _create_repository(spark: SparkSession) -> ElectricityMarketRepository:
-    return ElectricityMarketRepository(spark, _TEST_FILES_FOLDER_PATH)
+class TestReadConsumptionMeteringPointPeriods:
+    @pytest.fixture(scope="module")
+    def valid_dataframe(self, spark: SparkSession) -> DataFrame:
+        df = spark.createDataFrame(
+            [
+                (
+                    "170000000000000201",
+                    NetSettlementGroup.NET_SETTLEMENT_GROUP_2.value,
+                    1,
+                    datetime(2023, 12, 31, 0, tzinfo=ZoneInfo("Europe/Copenhagen")),
+                    None,
+                ),
+            ],
+            schema=ExternalDataProducts.ELECTRICAL_HEATING_CONSUMPTION_METERING_POINT_PERIODS.schema,
+        )
+        assert df.schema == ExternalDataProducts.ELECTRICAL_HEATING_CONSUMPTION_METERING_POINT_PERIODS.schema
+        return df
+
+    def test__when_invalid_contract__raises_with_useful_message(
+        self,
+        valid_dataframe: DataFrame,
+        electricity_market_repository: ElectricityMarketRepository,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        # Arrange
+        invalid_df = valid_dataframe.drop(F.col("metering_point_id"))
+
+        def mock_read_table(*args, **kwargs):
+            return invalid_df
+
+        monkeypatch.setattr(electricity_market_repository, "_read_table", mock_read_table)
+
+        # Assert
+        with pytest.raises(
+            Exception,
+            match=r"The data source does not comply with the contract.*",
+        ):
+            # Act
+            electricity_market_repository.read_consumption_metering_point_periods()
+
+    def test__when_source_contains_unexpected_columns__returns_data_without_unexpected_column(
+        self,
+        valid_dataframe: DataFrame,
+        electricity_market_repository: ElectricityMarketRepository,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        """Test that the table can handle columns being added as it is defined to _not_ be a breaking change.
+        The repository should return the data without the unexpected column."""
+        # Arrange
+        valid_df_with_extra_col = valid_dataframe.withColumn("extra_col", F.lit("extra_value"))
+
+        def mock_read_table(*args, **kwargs):
+            return valid_df_with_extra_col
+
+        monkeypatch.setattr(electricity_market_repository, "_read_table", mock_read_table)
+
+        # Act
+        actual = electricity_market_repository.read_consumption_metering_point_periods()
+
+        # Assert
+        assert actual.schema == ExternalDataProducts.ELECTRICAL_HEATING_CONSUMPTION_METERING_POINT_PERIODS.schema
 
 
-def test__when_consumption_missing_expected_column_raises_exception(spark: SparkSession, monkeypatch) -> None:
-    with pytest.raises(ValueError, match=r"Column settlement_month not found in CSV"):
-        _create_repository_with_missing_col(spark).read_consumption_metering_point_periods()
+class TestReadChildMeteringPoints:
+    @pytest.fixture(scope="module")
+    def valid_dataframe(self, spark: SparkSession) -> DataFrame:
+        df = spark.createDataFrame(
+            [
+                (
+                    "140000000000170201",
+                    MeteringPointType.ELECTRICAL_HEATING,
+                    MeteringPointSubType.CALCULATED,
+                    "170000000000000201",
+                    datetime(2023, 12, 31, 0, tzinfo=ZoneInfo("Europe/Copenhagen")),
+                    None,
+                ),
+            ],
+            schema=ExternalDataProducts.ELECTRICAL_HEATING_CHILD_METERING_POINTS.schema,
+        )
+        assert df.schema == ExternalDataProducts.ELECTRICAL_HEATING_CHILD_METERING_POINTS.schema
+        return df
 
+    def test__when_invalid_contract__raises_with_useful_message(
+        self,
+        valid_dataframe: DataFrame,
+        electricity_market_repository: ElectricityMarketRepository,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        # Arrange
+        invalid_df = valid_dataframe.drop(F.col("metering_point_id"))
 
-def test__when_child_missing_expected_column_raises_exception(spark: SparkSession, monkeypatch) -> None:
-    with pytest.raises(ValueError, match=r"Column metering_point_sub_type not found in CSV"):
-        _create_repository_with_missing_col(spark).read_child_metering_points()
+        def mock_read_table(*args, **kwargs):
+            return invalid_df
 
+        monkeypatch.setattr(electricity_market_repository, "_read_table", mock_read_table)
 
-def test__when_consumption_source_contains_unexpected_columns_returns_data_without_unexpected_column(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    consumption = _create_repository(spark).read_consumption_metering_point_periods()
+        # Assert
+        with pytest.raises(
+            Exception,
+            match=r"The data source does not comply with the contract.*",
+        ):
+            # Act
+            electricity_market_repository.read_child_metering_points()
 
-    # Act
-    consumption_with_extra_input_col = _create_repository_with_extra_col(
-        spark
-    ).read_consumption_metering_point_periods()
+    def test__when_source_contains_unexpected_columns__returns_data_without_unexpected_column(
+        self,
+        valid_dataframe: DataFrame,
+        electricity_market_repository: ElectricityMarketRepository,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        """Test that the table can handle columns being added as it is defined to _not_ be a breaking change.
+        The repository should return the data without the unexpected column."""
+        # Arrange
+        valid_df_with_extra_col = valid_dataframe.withColumn("extra_col", F.lit("extra_value"))
 
-    # Assert
-    assert consumption_with_extra_input_col.df.columns == consumption.df.columns
+        def mock_read_table(*args, **kwargs):
+            return valid_df_with_extra_col
 
+        monkeypatch.setattr(electricity_market_repository, "_read_table", mock_read_table)
 
-def test__when_child_source_contains_unexpected_columns_returns_data_without_unexpected_column(
-    spark: SparkSession,
-) -> None:
-    # Arrange
-    child = _create_repository(spark).read_child_metering_points()
+        # Act
+        actual = electricity_market_repository.read_child_metering_points()
 
-    # Act
-    child_with_extra_input_col = _create_repository_with_extra_col(spark).read_child_metering_points()
-
-    # Assert
-    assert child_with_extra_input_col.df.columns == child.df.columns
-
-
-# should be use if this repository reads from a source that contains data type e.g. delta table
-@pytest.mark.skip(reason="csv file does not contain data type")
-def test__when_source_contains_wrong_data_type_raises_exception(
-    electricity_market_repository_wrong_data_type: ElectricityMarketRepository,
-) -> None:
-    pass
+        # Assert
+        assert actual.schema == ExternalDataProducts.ELECTRICAL_HEATING_CHILD_METERING_POINTS.schema
