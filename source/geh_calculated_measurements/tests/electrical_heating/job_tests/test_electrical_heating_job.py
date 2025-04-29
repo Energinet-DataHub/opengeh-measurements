733d1301c908e3e4a701b2a795ad0dd22f02d75b
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-29 14:41:29 +0200

    move seed_current_measurements to 'testing' folder

--- source/geh_calculated_measurements/tests/electrical_heating/job_tests/test_electrical_heating_job.py
+++ source/geh_calculated_measurements/tests/electrical_heating/job_tests/test_electrical_heating_job.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import uuid
+from datetime import datetime
 from typing import Any
 
+from geh_common.domain.types import MeteringPointType
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
@@ -10,9 +12,12 @@ from geh_calculated_measurements.common.infrastructure import (
     CalculatedMeasurementsInternalDatabaseDefinition,
 )
 from geh_calculated_measurements.electrical_heating.entry_point import execute
+from geh_calculated_measurements.testing import seed_current_measurements
 from tests import create_job_environment_variables
 from tests.electrical_heating.job_tests import get_test_files_folder_path
-from tests.electrical_heating.job_tests.seeding import seed_electricity_market, seed_gold
+from tests.electrical_heating.job_tests.seeding import seed_electricity_market
+
+_PARENT_METERING_POINT_ID = "170000000000000202"
 
 
 def test_execute(
@@ -27,7 +32,12 @@ def test_execute(
     monkeypatch.setattr(sys, "argv", ["dummy_script_name", "--orchestration-instance-id", orchestration_instance_id])
     monkeypatch.setattr(os, "environ", create_job_environment_variables(get_test_files_folder_path()))
 
-    seed_gold(spark)
+    seed_current_measurements(
+        spark=spark,
+        metering_point_id=_PARENT_METERING_POINT_ID,
+        metering_point_type=MeteringPointType.CONSUMPTION,
+        observation_time=datetime(2024, 1, 1, 22),
+    )
     seed_electricity_market(spark)
 
     # Act
