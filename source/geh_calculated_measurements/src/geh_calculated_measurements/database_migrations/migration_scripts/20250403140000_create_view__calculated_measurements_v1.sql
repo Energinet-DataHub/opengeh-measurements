a184f32c09a593d5fe5f510ce19b65a245003fea
Jonas Michelsen <jvm@energinet.dk>
2025-04-29 13:58:26 +0200

    .

---
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/database_migrations/migration_scripts/20250403140000_create_view__calculated_measurements_v1.sql
@@ -0,0 +1,17 @@
+DROP VIEW IF EXISTS {catalog_name}.{calculated_measurements_database}.hourly_calculated_measurements_v1
+GO
+
+CREATE VIEW {catalog_name}.{calculated_measurements_database}.calculated_measurements_v1 AS
+SELECT 
+  orchestration_type,
+  orchestration_instance_id,
+  transaction_id,
+  transaction_creation_datetime,
+  metering_point_id,
+  metering_point_type,
+  observation_time,
+  quantity,
+  "kWh" as quantity_unit,
+  "calculated" as quantity_quality,
+  "PT1H" as resolution
+FROM {catalog_name}.{calculated_measurements_internal_database}.calculated_measurements 
