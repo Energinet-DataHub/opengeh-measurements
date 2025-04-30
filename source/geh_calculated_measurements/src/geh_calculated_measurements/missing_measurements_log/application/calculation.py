7c3fbdb79fe0bc4c7f3946f9caf382532cfda267
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-30 11:53:05 +0200

    Update calculation.py

--- source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/missing_measurements_log/application/calculation.py
@@ -6,17 +6,20 @@ from geh_calculated_measurements.missing_measurements_log.application.missing_me
     MissingMeasurementsLogArgs,
 )
 from geh_calculated_measurements.missing_measurements_log.domain import execute
-from geh_calculated_measurements.missing_measurements_log.infrastructure import ElectricityMarketRepository
+from geh_calculated_measurements.missing_measurements_log.infrastructure import (
+    ElectricityMarketRepository,
+    MissingMeasurementsLogRepository,
+)
 
 
 @use_span()
 def execute_application(spark: SparkSession, args: MissingMeasurementsLogArgs) -> None:
     current_measurements_repository = CurrentMeasurementsRepository(spark, args.catalog_name)
-    missing_measurements_log_repository = ElectricityMarketRepository(spark, args.catalog_name)
+    electricity_market_repository = ElectricityMarketRepository(spark, args.catalog_name)
 
     # Read data frames
     current_measurements = current_measurements_repository.read_current_measurements()
-    metering_point_periods = missing_measurements_log_repository.read_metering_point_periods()
+    metering_point_periods = electricity_market_repository.read_metering_point_periods()
 
     missing_measurements_log = execute(
         current_measurements=current_measurements,
@@ -28,4 +31,5 @@ def execute_application(spark: SparkSession, args: MissingMeasurementsLogArgs) -
         period_end_datetime=args.period_end_datetime,
     )
 
+    missing_measurements_log_repository = MissingMeasurementsLogRepository(spark, args.catalog_name)
     missing_measurements_log_repository.write_missing_measurements_log(missing_measurements_log)
