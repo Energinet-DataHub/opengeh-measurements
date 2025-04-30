98588e8b8b5b6887de3ca33c33c32b688d8568fe
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-04-30 12:59:11 +0000

    Use constants

--- source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/seed_table.py
+++ source/geh_calculated_measurements/tests/subsystem_tests/net_consumption_group_6/seed_table.py
@@ -87,7 +87,7 @@ def electricity_market_tables_statements(catalog_name: str, database_name: str) 
     )
     VALUES (
         '{net_consumption_metering_point_id}',
-        'net_consumption',
+        '{MeteringPointType.NET_CONSUMPTION}',
         '{parent_metering_point_id}',
         '{datetime(2022, 12, 31, 23, 0, 0, tzinfo=timezone.utc)}',
         '{datetime(2025, 12, 31, 23, 0, 0, tzinfo=timezone.utc)}'
@@ -103,7 +103,7 @@ def electricity_market_tables_statements(catalog_name: str, database_name: str) 
     )
     VALUES (
         '{supply_to_grid_metering_point_id}',
-        'supply_to_grid',
+        '{MeteringPointType.SUPPLY_TO_GRID}',
         '{parent_metering_point_id}',
         '{datetime(2022, 12, 31, 23, 0, 0, tzinfo=timezone.utc)}',
         '{datetime(2025, 12, 31, 23, 0, 0, tzinfo=timezone.utc)}'
@@ -119,7 +119,7 @@ def electricity_market_tables_statements(catalog_name: str, database_name: str) 
     )
     VALUES (
         '{consumption_from_grid_metering_point_id}',
-        'consumption_from_grid',
+        '{MeteringPointType.CONSUMPTION_FROM_GRID}',
         '{parent_metering_point_id}',
         '{datetime(2022, 12, 31, 23, 0, 0, tzinfo=timezone.utc)}',
         '{datetime(2025, 12, 31, 23, 0, 0, tzinfo=timezone.utc)}'
