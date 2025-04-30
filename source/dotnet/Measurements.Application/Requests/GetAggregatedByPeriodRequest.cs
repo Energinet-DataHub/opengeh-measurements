8e41789f74bd85275edf7ab0759f91350f145a61
Gizmooa <jeppe1000@live.dk>
2025-04-30 14:15:06 +0200

    adj

--- source/dotnet/Measurements.Application/Requests/GetAggregatedByPeriodRequest.cs
+++ source/dotnet/Measurements.Application/Requests/GetAggregatedByPeriodRequest.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Energinet.DataHub.Measurements.Domain;
 
+namespace Energinet.DataHub.Measurements.Application.Requests;
+
 public record GetAggregatedByPeriodRequest(
     [property: Required] string MeteringPointIds,
     [property: Required] DateTimeOffset DateFrom,
     [property: Required] DateTimeOffset DateTo,
     [property: Required] Aggregation Aggregation)
 {
-  public List<string> GetMeteringPointIds() =>
-      MeteringPointIds.Trim().Split(',').ToList();
+    public List<string> GetMeteringPointIds() =>
+        MeteringPointIds.Trim().Split(',').ToList();
 
-  public override string ToString() =>
-      $"MeteringPointIds: {MeteringPointIds}, DateFrom (UTC): {DateFrom:O}, DateTo (UTC): {DateTo:O}, Aggregation: {Aggregation}";
+    public override string ToString() =>
+        $"MeteringPointIds: {MeteringPointIds}, DateFrom (UTC): {DateFrom:O}, DateTo (UTC): {DateTo:O}, Aggregation: {Aggregation}";
 }
