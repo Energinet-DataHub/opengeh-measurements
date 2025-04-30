b0875ae1e1a2d5cb66501a950ffdbadec3ce851a
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-30 15:17:57 +0200

    ...

--- source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/infrastructure/capacity_settlement_repository.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/capacity_settlement/infrastructure/capacity_settlement_repository.py
@@ -2,6 +2,10 @@ from pyspark.sql import DataFrame, SparkSession
 
 from geh_calculated_measurements.database_migrations import DatabaseNames
 
+DATABASE_NAME = DatabaseNames.MEASUREMENTS_CALCULATED_INTERNAL
+CALCULATIONS_TABLE_NAME = "capacity_settlement_calculations"
+TEN_LARGEST_QUANTITIES_TABLE_NAME = "capacity_settlement_ten_largest_quantities"
+
 
 class CapacitySettlementRepository:
     def __init__(
@@ -19,9 +23,9 @@ class CapacitySettlementRepository:
         return f"{database_name}.{table_name}"
 
     def write_calculations(self, df: DataFrame) -> None:
-        table_name = "capacity_settlement_calculations"
+        table_name = CALCULATIONS_TABLE_NAME
         df.write.format("delta").mode("append").saveAsTable(self._get_full_table_path(table_name))
 
     def write_ten_largest_quantities(self, df: DataFrame) -> None:
-        table_name = "capacity_settlement_ten_largest_quantities"
+        table_name = TEN_LARGEST_QUANTITIES_TABLE_NAME
         df.write.format("delta").mode("append").saveAsTable(self._get_full_table_path(table_name))
