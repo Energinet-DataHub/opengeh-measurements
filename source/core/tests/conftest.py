7316a408524359be7b8ca5092bb8c286e10e6f90
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-05-01 12:48:21 +0200

    core: Add pytest-xdist (#671)

--- source/core/tests/conftest.py
+++ source/core/tests/conftest.py
@@ -10,22 +10,28 @@ from pytest_mock import MockerFixture
 import core.gold.infrastructure.config.spark as gold_spark
 import core.utility.shared_helpers as shared_helpers
 
+extra_packages = [
+    "org.apache.spark:spark-protobuf_2.12:3.5.4",
+    "org.apache.hadoop:hadoop-azure:3.3.2",
+    "org.apache.hadoop:hadoop-common:3.3.2",
+    "io.delta:delta-spark_2.12:3.1.0",
+    "io.delta:delta-core_2.12:2.3.0",
+]
+# pytest-xdist plugin does not work with SparkSession as a fixture. The session scope is not supported.
+# Therefore, we need to create a global variable to store the Spark session and data directory.
+# This is a workaround to avoid creating a new Spark session for each test.
+_spark, data_dir = get_spark_test_session(extra_packages=extra_packages)
+
 
 @pytest.fixture(scope="session")
-def spark(session_mocker: MockerFixture) -> Generator[SparkSession, None, None]:
-    extra_packages = [
-        "org.apache.spark:spark-protobuf_2.12:3.5.4",
-        "org.apache.hadoop:hadoop-azure:3.3.2",
-        "org.apache.hadoop:hadoop-common:3.3.2",
-        "io.delta:delta-spark_2.12:3.1.0",
-        "io.delta:delta-core_2.12:2.3.0",
-    ]
-    session, _ = get_spark_test_session(extra_packages=extra_packages)
-    session_mocker.patch(f"{gold_spark.__name__}.initialize_spark", return_value=session)
+def spark(tmp_path_factory, worker_id, session_mocker: MockerFixture) -> Generator[SparkSession, None, None]:
+    """
+    Create a Spark session with Delta Lake enabled.
+    """
+    session_mocker.patch(f"{gold_spark.__name__}.initialize_spark", return_value=_spark)
 
-    yield session
-
-    session.stop()
+    yield _spark
+    _spark.stop()
 
 
 @pytest.fixture
