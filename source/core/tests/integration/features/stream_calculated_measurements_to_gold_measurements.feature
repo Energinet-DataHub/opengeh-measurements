22b40e0f314fb1eddd9bf6a32680111b1fadc7d8
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-05-01 10:39:18 +0200

    feat: Add integration tests (#716)

--- source/core/tests/integration/features/stream_calculated_measurements_to_gold_measurements.feature
+++ source/core/tests/integration/features/stream_calculated_measurements_to_gold_measurements.feature
@@ -4,3 +4,13 @@ Feature: Streaming Calculated Measurements to Gold Measurements
     Given calculated measurements inserted into the calculated measurements table
     When streaming calculated measurements to the Gold layer
     Then measurements are available in the gold measurements table
+
+  Scenario: Processing duplicated calculated measurements to Gold
+    Given duplicated calculated measurements inserted into the calculated measurements table
+    When streaming calculated measurements to the Gold layer
+    Then measurements are available in the gold measurements table
+
+  Scenario: Processing duplicate rows to Gold ensures only one row exists
+    Given calculated measurements inserted into the calculated measurements table and the same calculated measurements inserted into the gold table
+    When streaming calculated measurements to the Gold layer
+    Then measurements are available in the gold measurements table    
--- source/core/tests/integration/step_definitions/test_stream_submitted_silver_measurements_to_gold_measurements.py
+++ source/core/tests/integration/step_definitions/test_stream_submitted_silver_measurements_to_gold_measurements.py
@@ -1,7 +1,9 @@
+from geh_common.domain.types.orchestration_type import OrchestrationType
 from pyspark.sql import SparkSession
 from pytest_bdd import given, scenarios, then, when
 
 import core.gold.application.streams.gold_measurements_stream as sut_gold
+import tests.helpers.datetime_helper as datetime_helper
 import tests.helpers.identifier_helper as identifier_helper
 import tests.helpers.table_helper as table_helper
 from core.gold.infrastructure.config import GoldTableNames
@@ -10,6 +12,7 @@ from core.settings.core_internal_settings import CoreInternalSettings
 from core.settings.gold_settings import GoldSettings
 from core.settings.silver_settings import SilverSettings
 from core.silver.infrastructure.config.table_names import TableNames as SilverTableNames
+from tests.helpers.builders.gold_builder import GoldMeasurementsBuilder
 from tests.helpers.builders.silver_measurements_builder import SilverMeasurementsBuilder
 
 scenarios("../features/stream_submitted_silver_measurements_to_gold_measurements.feature")
@@ -47,6 +50,62 @@ def _(spark: SparkSession, mock_checkpoint_path):
         silver_measurements, SilverSettings().silver_database_name, SilverTableNames.silver_measurements
     )
     return orchestration_instance_id
+
+
+@given(
+    "duplicated valid measurements inserted into the silver measurements table",
+    target_fixture="expected_metering_point_id",
+)
+def _(spark: SparkSession, mock_checkpoint_path):
+    metering_point_id = identifier_helper.create_random_metering_point_id()
+    silver_measurements = SilverMeasurementsBuilder(spark).add_row(metering_point_id=metering_point_id).build()
+    table_helper.append_to_table(
+        silver_measurements, SilverSettings().silver_database_name, SilverTableNames.silver_measurements
+    )
+    table_helper.append_to_table(
+        silver_measurements, SilverSettings().silver_database_name, SilverTableNames.silver_measurements
+    )
+    return metering_point_id
+
+
+@given(
+    "valid measurements inserted into the silver measurements table and the same calculated measurements inserted into the gold table",
+    target_fixture="expected_metering_point_id",
+)
+def _(spark: SparkSession, mock_checkpoint_path):
+    metering_point_id = identifier_helper.create_random_metering_point_id()
+    start_time = datetime_helper.random_datetime()
+
+    # Silver measurements
+    silver_measurements = (
+        SilverMeasurementsBuilder(spark)
+        .add_row(
+            metering_point_id=metering_point_id,
+            start_datetime=start_time,
+            orchestration_type=OrchestrationType.SUBMITTED.value,
+            transaction_creation_datetime=start_time,
+            transaction_id="",
+        )
+        .build()
+    )
+    table_helper.append_to_table(
+        silver_measurements, SilverSettings().silver_database_name, SilverTableNames.silver_measurements
+    )
+
+    # Gold measurements
+    gold_measurements = (
+        GoldMeasurementsBuilder(spark)
+        .add_24_hours_rows(
+            metering_point_id=metering_point_id,
+            start_time=start_time,
+            orchestration_type=OrchestrationType.SUBMITTED.value,
+            transaction_creation_datetime=start_time,
+            transaction_id="",
+        )
+        .build()
+    )
+    table_helper.append_to_table(gold_measurements, GoldSettings().gold_database_name, GoldTableNames.gold_measurements)
+    return metering_point_id
 
 
 # When steps
