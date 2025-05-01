e3ae7edfd1d8b743ec3ba58c2988a06fc49e7df1
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2025-05-01 09:41:38 +0200

    [MeasurementsApi] Refactor routes (#718)

--- source/dotnet/Measurements.WebApi.IntegrationTests/Controllers/MeasurementsControllerTests.cs
+++ source/dotnet/Measurements.WebApi.IntegrationTests/Controllers/MeasurementsControllerTests.cs
@@ -90,13 +90,13 @@ public class MeasurementsControllerTests(WebApiFixture fixture) : IClassFixture<
     }
 
     [Fact]
-    public async Task GetAggregatedByMonthAsync_WhenMeteringPointExists_ReturnsValidAggregatedMeasurements()
+    public async Task GetAggregatedByMonthAsyncV2_WhenMeteringPointExists_ReturnsValidAggregatedMeasurements()
     {
         // Arrange
         const string expectedMeteringPointId = "1234567890";
         var expectedDate = Instant.FromUtc(2021, 2, 1, 23, 0, 0).ToDateOnly();
         var yearMonth = new YearMonth(2021, 2);
-        var url = CreateGetAggregatedMeasurementsByMonthUrl(expectedMeteringPointId, yearMonth);
+        var url = CreateGetAggregatedMeasurementsByMonthV2Url(expectedMeteringPointId, yearMonth);
 
         // Act
         var actualResponse = await fixture.Client.GetAsync(url);
@@ -117,12 +117,12 @@ public class MeasurementsControllerTests(WebApiFixture fixture) : IClassFixture<
     }
 
     [Fact]
-    public async Task GetAggregatedByMonthAsync_WhenMeteringPointDoesNotExist_ReturnNotFoundStatus()
+    public async Task GetAggregatedByMonthAsyncV2_WhenMeteringPointDoesNotExist_ReturnNotFoundStatus()
     {
         // Arrange
         const string expectedMeteringPointId = "not existing id";
         var yearMonth = new YearMonth(2022, 1);
-        var url = CreateGetAggregatedMeasurementsByMonthUrl(expectedMeteringPointId, yearMonth);
+        var url = CreateGetAggregatedMeasurementsByMonthV2Url(expectedMeteringPointId, yearMonth);
 
         // Act
         var actualResponse = await fixture.Client.GetAsync(url);
@@ -132,13 +132,13 @@ public class MeasurementsControllerTests(WebApiFixture fixture) : IClassFixture<
     }
 
     [Fact]
-    public async Task GetAggregatedByYearAsync_WhenMeteringPointExists_ReturnsValidAggregatedMeasurements()
+    public async Task GetAggregatedByYearAsyncV2_WhenMeteringPointExists_ReturnsValidAggregatedMeasurements()
     {
         // Arrange
         const string expectedMeteringPointId = "1234567890";
         var expectedDate = Instant.FromUtc(2021, 2, 1, 23, 0, 0).ToDateOnly();
         var expectedYearMonth = new YearMonth(expectedDate.Year, expectedDate.Month);
-        var url = CreateGetAggregatedMeasurementsByYearUrl(expectedMeteringPointId, new Year(expectedYearMonth.Year));
+        var url = CreateGetAggregatedMeasurementsByYearV2Url(expectedMeteringPointId, new Year(expectedYearMonth.Year));
 
         // Act
         var actualResponse = await fixture.Client.GetAsync(url);
@@ -156,16 +156,113 @@ public class MeasurementsControllerTests(WebApiFixture fixture) : IClassFixture<
         }
     }
 
+    [Fact]
+    public async Task GetAggregatedByYearAsyncV2_WhenMeteringPointDoesNotExist_ReturnNotFoundStatus()
+    {
+        // Arrange
+        const string expectedMeteringPointId = "not existing id";
+        var yearMonth = new YearMonth(2022, 1);
+        var url = CreateGetAggregatedMeasurementsByYearV2Url(expectedMeteringPointId, new Year(yearMonth.Year));
+
+        // Act
+        var actualResponse = await fixture.Client.GetAsync(url);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, actualResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetAggregatedByDateAsync_WhenMeteringPointExists_ReturnsValidAggregatedMeasurements()
+    {
+        // Arrange
+        const string expectedMeteringPointId = "1234567890";
+        var expectedDate = Instant.FromUtc(2021, 2, 1, 23, 0, 0).ToDateOnly();
+        var yearMonth = new YearMonth(2021, 2);
+        var url = CreateGetAggregatedMeasurementsByDateUrl(expectedMeteringPointId, yearMonth);
+
+        // Act
+        var actualResponse = await fixture.Client.GetAsync(url);
+        var actual = await ParseResponseAsync<MeasurementsAggregatedByDateResponse>(actualResponse);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, actualResponse.StatusCode);
+        Assert.Equal(2, actual.MeasurementAggregations.Count);
+        Assert.Equal(expectedDate, actual.MeasurementAggregations.First().Date);
+        foreach (var measurementAggregation in actual.MeasurementAggregations)
+        {
+            Assert.Equal(Quality.Measured, measurementAggregation.Quality);
+            Assert.Equal(285.6M, measurementAggregation.Quantity);
+            Assert.False(measurementAggregation.MissingValues);
+            Assert.Equal(Unit.kWh, measurementAggregation.Unit);
+            Assert.False(measurementAggregation.ContainsUpdatedValues);
+        }
+    }
+
+    [Fact]
+    public async Task GetAggregatedByDateAsync_WhenMeteringPointDoesNotExist_ReturnNotFoundStatus()
+    {
+        // Arrange
+        const string expectedMeteringPointId = "not existing id";
+        var yearMonth = new YearMonth(2022, 1);
+        var url = CreateGetAggregatedMeasurementsByDateUrl(expectedMeteringPointId, yearMonth);
+
+        // Act
+        var actualResponse = await fixture.Client.GetAsync(url);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, actualResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetAggregatedByMonthAsync_WhenMeteringPointExists_ReturnsValidAggregatedMeasurements()
+    {
+        // Arrange
+        const string expectedMeteringPointId = "1234567890";
+        var expectedDate = Instant.FromUtc(2021, 2, 1, 23, 0, 0).ToDateOnly();
+        var expectedYearMonth = new YearMonth(expectedDate.Year, expectedDate.Month);
+        var url = CreateGetAggregatedMeasurementsByMonthUrl(expectedMeteringPointId, new Year(expectedYearMonth.Year));
+
+        // Act
+        var actualResponse = await fixture.Client.GetAsync(url);
+        var actual = await ParseResponseAsync<MeasurementsAggregatedByMonthResponse>(actualResponse);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, actualResponse.StatusCode);
+        Assert.Single(actual.MeasurementAggregations);
+        Assert.Equal(expectedYearMonth, actual.MeasurementAggregations.First().YearMonth);
+        foreach (var measurementAggregation in actual.MeasurementAggregations)
+        {
+            Assert.Equal(Quality.Measured, measurementAggregation.Quality);
+            Assert.Equal(571.2M, measurementAggregation.Quantity);
+            Assert.Equal(Unit.kWh, measurementAggregation.Unit);
+        }
+    }
+
+    [Fact]
+    public async Task GetAggregatedByMonthAsync_WhenMeteringPointDoesNotExist_ReturnNotFoundStatus()
+    {
+        // Arrange
+        const string expectedMeteringPointId = "not existing id";
+        var yearMonth = new YearMonth(2022, 1);
+        var url = CreateGetAggregatedMeasurementsByMonthUrl(expectedMeteringPointId, new Year(yearMonth.Year));
+
+        // Act
+        var actualResponse = await fixture.Client.GetAsync(url);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, actualResponse.StatusCode);
+    }
+
     [Theory]
     [InlineData("")]
     [InlineData("v1")]
-    [InlineData("v3")]
-    public async Task GetAggregatedMeasurementsAsync_WhenTargettingUnsupportedVersion_ReturnsNotFound(string version)
+    [InlineData("v4")]
+    public async Task GetAggregatedMeasurementsAsync_WhenTargetingUnsupportedVersion_ReturnsNotFound(string version)
     {
         // Arrange
         const string expectedMeteringPointId = "1234567890";
         var yearMonth = new YearMonth(2021, 2);
-        var url = CreateGetAggregatedMeasurementsByMonthUrl(expectedMeteringPointId, yearMonth, version);
+        var url = CreateGetAggregatedMeasurementsByMonthV2Url(expectedMeteringPointId, yearMonth, version);
 
         // Act
         var actual = await fixture.Client.GetAsync(url);
@@ -174,19 +271,29 @@ public class MeasurementsControllerTests(WebApiFixture fixture) : IClassFixture<
         Assert.Equal(HttpStatusCode.NotFound, actual.StatusCode);
     }
 
-    private static string CreateGetMeasurementsForPeriodUrl(string expectedMeteringPointId, string startDate, string endDate, string versionPrefix = "v2")
+    private static string CreateGetMeasurementsForPeriodUrl(string expectedMeteringPointId, string startDate, string endDate, string versionPrefix = "v3")
     {
         return $"{versionPrefix}/measurements/forPeriod?meteringPointId={expectedMeteringPointId}&startDate={startDate}&endDate={endDate}";
     }
 
-    private static string CreateGetAggregatedMeasurementsByMonthUrl(string expectedMeteringPointId, YearMonth yearMonth, string versionPrefix = "v2")
+    private static string CreateGetAggregatedMeasurementsByMonthV2Url(string expectedMeteringPointId, YearMonth yearMonth, string versionPrefix = "v2")
     {
         return $"{versionPrefix}/measurements/aggregatedByMonth?meteringPointId={expectedMeteringPointId}&year={yearMonth.Year}&month={yearMonth.Month}";
     }
 
-    private static string CreateGetAggregatedMeasurementsByYearUrl(string expectedMeteringPointId, Year year, string versionPrefix = "v2")
+    private static string CreateGetAggregatedMeasurementsByYearV2Url(string expectedMeteringPointId, Year year, string versionPrefix = "v2")
     {
         return $"{versionPrefix}/measurements/aggregatedByYear?meteringPointId={expectedMeteringPointId}&year={year.GetYear()}";
+    }
+
+    private static string CreateGetAggregatedMeasurementsByDateUrl(string expectedMeteringPointId, YearMonth yearMonth, string versionPrefix = "v3")
+    {
+        return $"{versionPrefix}/measurements/aggregatedByDate?meteringPointId={expectedMeteringPointId}&year={yearMonth.Year}&month={yearMonth.Month}";
+    }
+
+    private static string CreateGetAggregatedMeasurementsByMonthUrl(string expectedMeteringPointId, Year year, string versionPrefix = "v3")
+    {
+        return $"{versionPrefix}/measurements/aggregatedByMonth?meteringPointId={expectedMeteringPointId}&year={year.GetYear()}";
     }
 
     private async Task<T> ParseResponseAsync<T>(HttpResponseMessage response)
