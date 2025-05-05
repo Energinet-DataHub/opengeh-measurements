5e27c368f9c96c72bc432b5338078cc8d54391fb
henrik-t-caroe <henrik.t.caroe@twoday.com>
2025-05-05 12:41:14 +0000

    update

--- source/geh_calculated_measurements/src/geh_calculated_measurements/measurements_report/entry_point.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/measurements_report/entry_point.py
@@ -4,24 +4,22 @@ from geh_common.telemetry.logging_configuration import LoggingSettings, configur
 
 from geh_calculated_measurements.common.infrastructure.spark_initializor import initialize_spark
 from geh_calculated_measurements.measurements_report.application import execute_application
-from geh_calculated_measurements.measurements_report.application.measurement_report_args import (
+from geh_calculated_measurements.measurements_report.application.measurements_report_args import (
     MeasurementsReportArgs,
 )
 
 
 # cenc
 def execute() -> None:
-    measurement_report_args = MeasurementsReportArgs()  # Retrieve calculation oriented settings / job arguments
-    logging_settings = configure_logging(subsystem="measurements", cloud_role_name="dbr-net-consumption-group-6")
-    orchestrate_business_logic(measurement_report_args, logging_settings=logging_settings)
+    measurements_report_args = MeasurementsReportArgs()  # Retrieve calculation oriented settings / job arguments
+    logging_settings = configure_logging(subsystem="measurements", cloud_role_name="dbr-measurements-report")
+    orchestrate_business_logic(measurements_report_args, logging_settings=logging_settings)
 
 
 @start_trace()
 def orchestrate_business_logic(job_arguments: MeasurementsReportArgs, logging_settings: LoggingSettings) -> None:
     logger = Logger(__name__)
     logger.info(f"Command line arguments / env variables retrieved for Logging Settings: {logging_settings}")
-    logger.info(
-        f"Command line arguments retrieved for net consumption group 6 job Oriented Parameters: {job_arguments}"
-    )
+    logger.info(f"Command line arguments retrieved for measurements report job Oriented Parameters: {job_arguments}")
     spark = initialize_spark()
     execute_application(spark, args=job_arguments)
