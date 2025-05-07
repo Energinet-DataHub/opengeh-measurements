5f264b925981a9ed346506345d91bfab8df0d3fc
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-07 14:09:49 +0200

    test: adds tests for required table properties
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_calculated_measurements/tests/common/test_table_properties.py
+++ source/geh_calculated_measurements/tests/common/test_table_properties.py
@@ -1,66 +1,61 @@
-import pytest
+import logging
+from typing import Callable
+
 from pyspark.sql import SparkSession
+from pyspark.sql.catalog import Table
 
-from tests import SPARK_CATALOG_NAME
-from tests.testsession_configuration import TestSessionConfiguration
-
-###
-# tables = spark.catalog.listTables()
-# for table in tables:
-#    print(f"{table.database}.{table.name}" if table.database else table.name)
-###
+from geh_calculated_measurements.database_migrations.migrations_runner import _migrate
+from tests import REQUIRED_DATABASES, SPARK_CATALOG_NAME
 
 
-@pytest.mark.parametrize(
-    ("fqn", "cluster_columns"),
-    [
-        # (f"{SPARK_CATALOG_NAME}.measurements_calculated_internal.executed_migrations", ["col1", "col2"]),
-        (
-            f"{SPARK_CATALOG_NAME}.measurements_calculated_internal.capacity_settlement_ten_largest_quantities",
-            ("orchestration_instance_id", "metering_point_id", "observation_time"),
-        ),
-        (
-            f"{SPARK_CATALOG_NAME}.measurements_calculated_internal.capacity_settlement_calculations",
-            ["orchestration_instance_id", "execution_time"],
-        ),
-        (
-            f"{SPARK_CATALOG_NAME}.measurements_calculated_internal.calculated_measurements",
-            ["orchestration_instance_id", "transaction_id", "metering_point_id", "transaction_creation_datetime"],
-        ),
-        # (
-        #    f"{SPARK_CATALOG_NAME}.measurements_calculated_internal.missing_measurements_log",
-        #    ["col1", "col2"],
-        # ),
-    ],
-)
-def test_clustering(
-    spark: SparkSession, migrations_executed: TestSessionConfiguration, fqn: str, cluster_columns: list[str]
-):
+def test_table_properties(spark):
     """
-    Test that all tables have liquad clustering, is managed and has a 30 days retention policy.
+    Test the properties of tables in the specified databases after migration.
+    This function creates the databases, runs the migration, and checks the properties of each table.
     """
-
-    # arrange
-
-    details_df = spark.sql(f"DESCRIBE DETAIL {fqn}")
-    details = details_df.collect()[0].asDict()  # .get("type")
-
-    location = details.get("location", "")
-
-    actual_clustering_cols = details.get("clusteringColumns", [])
-    expected_cluster_cols = [col.strip() for col in cluster_columns]
-
-    # Check if liquid clustering is enabled
-    assert actual_clustering_cols, f"Table {fqn} does not have liquid clustering enabled"
-
-    # Check if cluster columns are set correct
-    assert sorted(actual_clustering_cols) == sorted(expected_cluster_cols), (
-        f"Table {fqn} clustering columns mismatch.\nExpected: {cluster_columns}\nActual: {actual_clustering_cols}"
+    _assert_migrations(
+        spark=spark,
+        migration_func=_migrate,
+        databases=REQUIRED_DATABASES,
+        spark_catalog_name=SPARK_CATALOG_NAME,
     )
 
-    # check if retention policy is 30 days
-    retention = details["properties"].get("delta.deletedFileRetentionDuration")
-    assert retention == "interval 30 days", f"Expected 30-day retention, but found {retention}"
 
-    # check if table is managed
-    assert "spark-warehouse" in location, f"table is not managed and is located at: {location}"
+# TODO: Move the geh_common
+def _assert_migrations(
+    spark: SparkSession,
+    migration_func: Callable[[str], None],
+    databases: list[str],
+    spark_catalog_name: str = "spark_catalog",
+):
+    for db in databases:
+        spark.sql(f"CREATE DATABASE IF NOT EXISTS {db}")
+    migration_func(spark_catalog_name)
+    tables: list[Table] = []
+    for db in databases:
+        tables += spark.catalog.listTables(db)
+    for table in tables:
+        if table.tableType == "VIEW":
+            continue
+        fqn = f"{spark_catalog_name}.{table.database}.{table.name}"
+        assert table.tableType == "MANAGED", f"Table {fqn}: expected table type to be 'MANAGED', got {table.tableType}"
+        logging.info(f"Testing Table: {fqn}")
+        _test_table(spark, fqn)
+
+
+# TODO: Move the geh_common
+def _test_table(spark: SparkSession, fqn: str):
+    details = spark.sql(f"DESCRIBE DETAIL {fqn}").collect()[0].asDict()
+    table_format = details.get("format", "")
+    table_location = details.get("location", "")
+    table_clustering_columns = details.get("clusteringColumns", [])
+    table_properties = details.get("properties", {})
+    table_retention = table_properties.get("delta.deletedFileRetentionDuration", "")
+    assert table_format == "delta", f"Table {fqn}: expected format to be 'delta', got {table_format}"
+    assert table_location != "", f"Table {fqn}: expected location to be set, got {table_location}"
+    assert table_retention == "interval 30 days", (
+        f"Table {fqn}: expected retention to be 30 days, got {table_retention}"
+    )
+    assert len(table_clustering_columns) > 0, (
+        f"Table {fqn}: expected clustering columns to be set, got {table_clustering_columns}"
+    )
