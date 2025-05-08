4bce91436cb66ab908b47170d9d7ee664d908e73
Gizmooa <jeppe1000@live.dk>
2025-05-08 14:32:21 +0200

    adj

--- source/dotnet/Measurements.Infrastructure/Persistence/Queries/GetAggregatedByPeriodQuery.cs
+++ source/dotnet/Measurements.Infrastructure/Persistence/Queries/GetAggregatedByPeriodQuery.cs
@@ -11,11 +11,12 @@ public class GetAggregatedByPeriodQuery(string meteringPointIds, Instant from, I
 {
     protected override string GetSqlStatement()
     {
-        return AggregateSqlStatement.GetAggregateSqlStatement(
+        return AggregateSqlStatement.GetAggregatedByPeriodSqlStatement(
             databricksSchemaOptions.CatalogName,
             databricksSchemaOptions.SchemaName,
-            CreateGroupByStatement(),
-            GroupByMeteringPointAndObservationTime(aggregation));
+            CreateWhereStatement(),
+            CreateGroupKeyStatement(aggregation),
+            CreateGroupByStatement(aggregation));
     }
 
     protected override IReadOnlyCollection<QueryParameter> GetParameters()
@@ -29,14 +30,28 @@ public class GetAggregatedByPeriodQuery(string meteringPointIds, Instant from, I
         return parameters;
     }
 
-    private static string CreateGroupByStatement()
+    private static string CreateGroupKeyStatement(Aggregation aggregation)
     {
-        return $"where {MeasurementsGoldConstants.MeteringPointIdColumnName} in (:{QueryParameterConstants.MeteringPointIdParameter}) " +
+        return aggregation switch
+        {
+            Aggregation.Quarter or Aggregation.Hour or Aggregation.Day =>
+                $"cast(date(from_utc_timestamp(cast({AggregatedQueryConstants.MinObservationTime} as timestamp), '{TimeZoneConstants.EuropeCopenhagenTimeZone}')) as string)",
+            Aggregation.Month =>
+                $"substring(date(from_utc_timestamp(cast({AggregatedQueryConstants.MinObservationTime} as timestamp), '{TimeZoneConstants.EuropeCopenhagenTimeZone}')), 0, 7)",
+            Aggregation.Year =>
+                $"substring(date(from_utc_timestamp(cast({AggregatedQueryConstants.MinObservationTime} as timestamp), '{TimeZoneConstants.EuropeCopenhagenTimeZone}')), 0, 4)",
+            _ => throw new ArgumentOutOfRangeException(aggregation.ToString()),
+        };
+    }
+
+    private static string CreateWhereStatement()
+    {
+        return $"where {MeasurementsGoldConstants.MeteringPointIdColumnName} in (:{QueryParameterConstants.MeteringPointIdsParameter}) " +
                $"and {MeasurementsGoldConstants.ObservationTimeColumnName} >= :{QueryParameterConstants.ObservationTimeFromParameter} " +
                $"and {MeasurementsGoldConstants.ObservationTimeColumnName} < :{QueryParameterConstants.ObservationTimeToParameter}";
     }
 
-    private static string GroupByMeteringPointAndObservationTime(Aggregation aggregation)
+    private static string CreateGroupByStatement(Aggregation aggregation)
     {
         var windowTimeStatement = aggregation switch
         {
