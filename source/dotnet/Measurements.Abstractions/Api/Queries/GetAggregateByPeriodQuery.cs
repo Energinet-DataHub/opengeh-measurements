95751fe23c5c827a631f6577f03c59854e034bba
HenrikSommer <sommerh@gmail.com>
2025-05-13 14:09:36 +0200

    Remove aggregate query

--- source/dotnet/Measurements.Abstractions/Api/Queries/GetAggregateByPeriodQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using NodaTime;
-
-namespace Energinet.DataHub.Measurements.Abstractions.Api.Queries;
-
-/// <summary>
-/// Query for getting measurements aggregated for a period for a metering point.
-/// </summary>
-public sealed record GetAggregateByPeriodQuery(string MeteringPointId, Instant From, Instant To);
