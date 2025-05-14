3c6af129503044449ed847ee9d711eeb111ee861
jesper knudsen <jmk@halfspace.io>
2025-05-14 12:47:37 +0000

    passes given_nsg6_cnc_standard_scenario test now

--- source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/logic/cnc.py
+++ source/geh_calculated_measurements/src/geh_calculated_measurements/net_consumption_group_6/domain/logic/cnc.py
@@ -227,7 +227,7 @@ def _filter_periods_by_cut_off(parent_child_joined: DataFrame) -> DataFrame:
                 -THREE_YEARS_IN_MONTHS,
             ).alias("cut_off_date"),
         )
-        .where(F.col(ContractColumnNames.period_to_date) <= F.col("cut_off_date"))
+        .where(F.col(ContractColumnNames.period_to_date) >= F.col("cut_off_date"))
         .select(
             F.col(ContractColumnNames.metering_point_id),
             F.col(ContractColumnNames.metering_point_type),
