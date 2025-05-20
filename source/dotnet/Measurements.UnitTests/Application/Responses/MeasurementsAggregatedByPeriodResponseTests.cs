e7ca4abb2aae7ea40ea1d954572ddf1244dfa9c7
Gizmooa <jeppe1000@live.dk>
2025-05-20 08:30:03 +0200

    add more tests

--- source/dotnet/Measurements.UnitTests/Application/Responses/MeasurementsAggregatedByPeriodResponseTests.cs
+++ source/dotnet/Measurements.UnitTests/Application/Responses/MeasurementsAggregatedByPeriodResponseTests.cs
@@ -50,6 +50,145 @@ public class MeasurementsAggregatedByPeriodResponseTests
         Assert.Equal(Resolution.Hourly, firstAggregation.PointAggregationGroups.First().Value.Resolution);
     }
 
+    [Fact]
+    public void Create_WhenMultipleQualities_ThenLowestQualityIsReturned()
+    {
+        // Arrange
+        const string meteringPoints = "123456789012345678";
+        var minObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow);
+        var maxObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow.AddHours(23));
+        const decimal aggregatedQuantity = 100.0m;
+        var qualities = new[] { "measured", "estimated" };
+        const string resolution = "PT1H";
+        const string aggregationGroupKey = "12345678901234567890";
+
+        var aggregatedMeasurements = new List<AggregatedByPeriodMeasurementsResult>
+        {
+            new(CreateRaw(
+                meteringPoints,
+                minObservationTime,
+                maxObservationTime,
+                aggregatedQuantity,
+                qualities,
+                resolution,
+                aggregationGroupKey)),
+        };
+
+        // Act
+        var actual = MeasurementsAggregatedByPeriodResponse.Create(aggregatedMeasurements);
+
+        // Assert
+        var firstAggregation = actual.MeasurementAggregations.First();
+        Assert.Equal(Quality.Estimated, firstAggregation.PointAggregationGroups.First().Value.PointAggregations.First().Quality);
+    }
+
+    [Fact]
+    public void Create_WhenDataContainsInvalidQualities_ThenThrowException()
+    {
+        // Arrange
+        const string meteringPoints = "123456789012345678";
+        var minObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow);
+        var maxObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow.AddHours(23));
+        const decimal aggregatedQuantity = 100.0m;
+        var qualities = new[] { "invalid_quality" };
+        const string resolution = "PT1H";
+        const string aggregationGroupKey = "12345678901234567890";
+
+        var aggregatedMeasurements = new List<AggregatedByPeriodMeasurementsResult>
+        {
+            new(CreateRaw(
+                meteringPoints,
+                minObservationTime,
+                maxObservationTime,
+                aggregatedQuantity,
+                qualities,
+                resolution,
+                aggregationGroupKey)),
+        };
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => MeasurementsAggregatedByPeriodResponse.Create(aggregatedMeasurements));
+    }
+
+    [Fact]
+    public void Create_WhenDataContainsMultipleRows_MultiplePointAggregationsIsReturned()
+    {
+        // Arrange
+        const string meteringPoints = "123456789012345678";
+        var minObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow);
+        var maxObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow.AddHours(23));
+        const decimal aggregatedQuantity = 100.0m;
+        var qualities = new[] { "measured" };
+        const string resolution = "PT1H";
+        const string aggregationGroupKey = "12345678901234567890";
+
+        var aggregatedMeasurements = new List<AggregatedByPeriodMeasurementsResult>
+        {
+            new(CreateRaw(
+                meteringPoints,
+                minObservationTime,
+                maxObservationTime,
+                aggregatedQuantity,
+                qualities,
+                resolution,
+                aggregationGroupKey)),
+            new(CreateRaw(
+                meteringPoints,
+                minObservationTime.Plus(Duration.FromDays(1)),
+                maxObservationTime.Plus(Duration.FromDays(1)),
+                aggregatedQuantity * 2,
+                qualities,
+                resolution,
+                aggregationGroupKey)),
+        };
+
+        // Act
+        var actual = MeasurementsAggregatedByPeriodResponse.Create(aggregatedMeasurements);
+
+        // Assert
+        Assert.Equal(2, actual.MeasurementAggregations.First().PointAggregationGroups.First().Value.PointAggregations.Count);
+    }
+
+    [Fact]
+    public void Create_WhenDataContainsMultipleRowsWithDifferentResolution_MultiplePointAggregationGroupsIsReturned()
+    {
+        // Arrange
+        const string meteringPoints = "123456789012345678";
+        var minObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow);
+        var maxObservationTime = Instant.FromDateTimeOffset(DateTimeOffset.UtcNow.AddHours(23));
+        const decimal aggregatedQuantity = 100.0m;
+        var qualities = new[] { "measured" };
+        const string resolution1 = "PT1H";
+        const string resolution2 = "PT15M";
+        const string aggregationGroupKey = "12345678901234567890";
+
+        var aggregatedMeasurements = new List<AggregatedByPeriodMeasurementsResult>
+        {
+            new(CreateRaw(
+                meteringPoints,
+                minObservationTime,
+                maxObservationTime,
+                aggregatedQuantity,
+                qualities,
+                resolution1,
+                aggregationGroupKey)),
+            new(CreateRaw(
+                meteringPoints,
+                minObservationTime.Plus(Duration.FromDays(1)),
+                maxObservationTime.Plus(Duration.FromDays(1)),
+                aggregatedQuantity * 2,
+                qualities,
+                resolution2,
+                aggregationGroupKey)),
+        };
+
+        // Act
+        var actual = MeasurementsAggregatedByPeriodResponse.Create(aggregatedMeasurements);
+
+        // Assert
+        Assert.Equal(2, actual.MeasurementAggregations.First().PointAggregationGroups.Count);
+    }
+
     private static ExpandoObject CreateRaw(
         string meteringPoints,
         Instant minObservationTime,
