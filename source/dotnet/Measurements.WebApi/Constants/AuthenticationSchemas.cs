20f180db8cb9f135762bd22c4b708c0f4131fdb8
Jeppe M <jeppe1000@live.dk>
2025-05-23 13:22:43 +0200

    Update source/dotnet/Measurements.WebApi/Constants/AuthenticationSchemas.cs
    Co-authored-by: Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>

--- source/dotnet/Measurements.WebApi/Constants/AuthenticationSchemas.cs
+++ source/dotnet/Measurements.WebApi/Constants/AuthenticationSchemas.cs
@@ -2,7 +2,7 @@
 
 public class AuthenticationSchemas
 {
-    public const string Default = "Bearer";
+    public const string Default = JwtBearerDefaults.AuthenticationScheme;
 
     public const string B2C = "B2C";
 }
