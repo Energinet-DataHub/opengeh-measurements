fa110ac8b164e2035dcf0af0dafa2d31057082a5
jonasdmoeller <66109423+jonasdmoeller@users.noreply.github.com>
2025-05-26 09:09:27 +0200

    Cleanup

--- source/dotnet/Measurements.Abstractions/Api/Models/MeasurementAggregationByMonthDto.cs
+++ source/dotnet/Measurements.Abstractions/Api/Models/MeasurementAggregationByMonthDto.cs
@@ -11,4 +11,3 @@ namespace Energinet.DataHub.Measurements.Abstractions.Api.Models;
 /// <param name="Unit">Unit of aggregated measurement.</param>
 /// <param name="MissingValues">Indicates whether aggregate contains missing values during month of aggregation.</param>
 public sealed record MeasurementAggregationByMonthDto(YearMonth YearMonth, decimal? Quantity, Quality Quality, Unit Unit, bool MissingValues);
-
