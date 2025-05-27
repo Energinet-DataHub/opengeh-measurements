d5cbce5959d5ed7f0fbad46235d746bf6a231210
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-05-27 13:38:42 +0200

    Update test_series_sap_transformations.py

--- source/core/tests/unit_tests/gold/domain/transformations/test_series_sap_transformations.py
+++ source/core/tests/unit_tests/gold/domain/transformations/test_series_sap_transformations.py
@@ -27,4 +27,4 @@ def test__transform__should_return_serie_seq_no_column_with_offset(spark: SparkS
 
     # Assert
     actual_row = actual.collect()[0]
-    assert actual_row["serie_seq_no"] > expected_offset
+    assert actual_row["serie_seq_no"] >= expected_offset
