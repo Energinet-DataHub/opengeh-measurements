bbbcbffe2a5ad351d8f8eaf756c608ae819eb54d
Jan Duelund <jdd@energinet.dk>
2025-05-28 08:38:47 +0200

    rename

--- source/core/tests/integration/step_definitions/test_gold_sap_delta_view.py
+++ source/core/tests/integration/step_definitions/test_gold_sap_delta_view.py
@@ -7,9 +7,8 @@ from pytest_bdd import scenarios, then, when
 
 from core.gold.infrastructure.config import GoldViewNames
 from core.settings.gold_settings import GoldSettings
-from tests.helpers.builders.gold_builder import GoldMeasurementsBuilder
 
-scenarios("../features/gold_current_view.feature")
+scenarios("../features/gold_sap_delta_view.feature")
 
 
 @when("accessing the sap_delta_v1 gold view", target_fixture="actual_schema")
@@ -17,6 +16,6 @@ def _(spark):
     return spark.table(f"{GoldSettings().gold_database_name}.{GoldViewNames.sap_delta_v1}").schema
 
 
-@then("the table schema should match the expected current_v1 schema")
+@then("the table schema should match the expected sap_delta_v1 schema")
 def _(actual_schema):
     assert_schemas.assert_schema(actual=actual_schema, expected=sap_delta_v1_schema, ignore_nullability=True)
