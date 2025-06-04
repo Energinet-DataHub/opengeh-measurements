d26e25ba3503b333fba5f09cd0cd5b494abe9935
Jan Duelund <jdd@energinet.dk>
2025-06-04 15:26:35 +0200

    changed view and table

--- source/core/src/core/databases/migrations/migration_scripts/20250528073800_add_gold_measurements_sap_delta_view.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW {gold_database}.{gold_sap_delta_v1} AS
-SELECT
-    serie_no, metering_point_id, transaction_id, transaction_creation_datetime,
-    start_time, end_time, unit, resolution, is_cancelled, created
-FROM {gold_database}.{gold_measurements}
