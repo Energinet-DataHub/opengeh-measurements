2e09dc0a55e22fd0b198103d73a5360c7fc3977f
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-06-11 12:29:52 +0200

    feat: Add migrated to SAP series stream (#814)

--- source/core/src/core/gold/domain/constants/streaming/checkpoint_names.py
+++ source/core/src/core/gold/domain/constants/streaming/checkpoint_names.py
@@ -5,3 +5,4 @@ class CheckpointNames(Enum):
     SILVER_TO_GOLD = "measurements"
     CALCULATED_TO_GOLD = "measurements_calculated"
     MIGRATIONS_TO_GOLD = "measurements_migrations"
+    MIGRATIONS_TO_GOLD_SAP_SERIES = "measurements_migrations_sap_series"
