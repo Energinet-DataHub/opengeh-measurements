96eaf508ed9d51ebd3a818ccda60971b5f5f8082
HenrikSommer <sommerh@gmail.com>
2025-06-11 16:42:30 +0200

    Add client test for AggregatedByPeriod (unrelated to PR)

--- source/dotnet/Measurements.Client.UnitTests/MeasurementsClientTests.cs
+++ source/dotnet/Measurements.Client.UnitTests/MeasurementsClientTests.cs
@@ -20,8 +20,7 @@ public class MeasurementsClientTests
     public async Task GetByDayAsync_WhenCalledWithValidQuery_ReturnsMeasurement(
         Mock<IMeasurementsForDateResponseParser> measurementsForDayResponseParser,
         Mock<IMeasurementsForPeriodResponseParser> measurementsForPeriodResponseParser,
-        MeasurementDto expectedMeasurementDto,
-        ReadOnlyCollection<MeasurementPointDto> expectedMeasurementPoints)
+        MeasurementDto expectedMeasurementDto)
     {
         // Arrange
         var query = new GetByDayQuery("1234567890123", new LocalDate(1, 2, 3));
@@ -31,9 +30,6 @@ public class MeasurementsClientTests
         measurementsForDayResponseParser
             .Setup(x => x.ParseResponseMessage(response, CancellationToken.None))
             .ReturnsAsync(expectedMeasurementDto);
-        measurementsForPeriodResponseParser
-            .Setup(x => x.ParseResponseMessage(response, CancellationToken.None))
-            .ReturnsAsync(expectedMeasurementPoints);
         var sut = new MeasurementsClient(
             httpClientFactoryMock.Object, measurementsForDayResponseParser.Object, measurementsForPeriodResponseParser.Object);
 
@@ -61,6 +57,57 @@ public class MeasurementsClientTests
 
         // Act
         var actual = (await sut.GetByDayAsync(query, CancellationToken.None)).MeasurementPositions;
+
+        // Assert
+        Assert.Empty(actual);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetCurrentByPeriodAsync_WhenCalledWithValidQuery_ReturnsMeasurement(
+        Mock<IMeasurementsForDateResponseParser> measurementsForDayResponseParser,
+        Mock<IMeasurementsForPeriodResponseParser> measurementsForPeriodResponseParser,
+        ReadOnlyCollection<MeasurementPointDto> expectedMeasurementPoints)
+    {
+        // Arrange
+        var from = Instant.FromDateTimeOffset(new DateTimeOffset(2025, 1, 1, 23, 0, 0, TimeSpan.FromHours(1)));
+        var to = Instant.FromDateTimeOffset(new DateTimeOffset(2025, 1, 2, 23, 0, 0, TimeSpan.FromHours(1)));
+        var query = new GetByPeriodQuery("1234567890123", from, to);
+        var response = CreateResponse(HttpStatusCode.OK, TestAssets.MeasurementsForSingleDay);
+        var httpClient = CreateHttpClient(response);
+        var httpClientFactoryMock = CreateHttpClientFactoryMock(httpClient);
+        measurementsForPeriodResponseParser
+            .Setup(x => x.ParseResponseMessage(response, CancellationToken.None))
+            .ReturnsAsync(expectedMeasurementPoints);
+        var sut = new MeasurementsClient(
+            httpClientFactoryMock.Object, measurementsForDayResponseParser.Object, measurementsForPeriodResponseParser.Object);
+
+        // Act
+        var actual = await sut.GetCurrentByPeriodAsync(query, CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Equal(expectedMeasurementPoints, actual);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetCurrentByPeriodAsync_WhenCalledWithQueryWithNoMeasurements_ReturnsEmptyList(
+        Mock<IMeasurementsForDateResponseParser> measurementsForDayResponseParser,
+        Mock<IMeasurementsForPeriodResponseParser> measurementsForPeriodResponseParser)
+    {
+        // Arrange
+        var from = Instant.FromDateTimeOffset(new DateTimeOffset(2025, 1, 1, 23, 0, 0, TimeSpan.FromHours(1)));
+        var to = Instant.FromDateTimeOffset(new DateTimeOffset(2025, 1, 2, 23, 0, 0, TimeSpan.FromHours(1)));
+        var query = new GetByPeriodQuery("1234567890123", from, to);
+        var response = CreateResponse(HttpStatusCode.NotFound, string.Empty);
+        var httpClient = CreateHttpClient(response);
+        var httpClientFactoryMock = CreateHttpClientFactoryMock(httpClient);
+        var sut = new MeasurementsClient(
+            httpClientFactoryMock.Object, measurementsForDayResponseParser.Object, measurementsForPeriodResponseParser.Object);
+
+        // Act
+        var actual = await sut.GetCurrentByPeriodAsync(query, CancellationToken.None);
 
         // Assert
         Assert.Empty(actual);
@@ -167,6 +214,42 @@ public class MeasurementsClientTests
         Assert.Equal(2021, actual.First().Year);
         Assert.Equal(2025, actual.Last().Year);
         Assert.All(actual, p => Assert.Equal(Unit.kWh, p.Unit));
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAggregatedByPeriod_WhenCalledForMeasuredMeteringPoint_ReturnsListOfMeasurementAggregations(
+        Mock<IMeasurementsForDateResponseParser> measurementsForDayResponseParser,
+        Mock<IMeasurementsForPeriodResponseParser> measurementsForPeriodResponseParser)
+    {
+        // Arrange
+        var from = Instant.FromDateTimeOffset(new DateTimeOffset(2025, 3, 1, 0, 0, 0, TimeSpan.FromHours(1)));
+        var to = Instant.FromDateTimeOffset(new DateTimeOffset(2025, 4, 1, 0, 0, 0, TimeSpan.FromHours(2)));
+        var query = new GetAggregateByPeriodQuery(["123456789123456789"], from, to, Aggregation.Day);
+        var response = CreateResponse(HttpStatusCode.OK, TestAssets.MeasurementsAggregatedByPeriod);
+        var httpClient = CreateHttpClient(response);
+        var httpClientFactoryMock = CreateHttpClientFactoryMock(httpClient);
+        var sut = new MeasurementsClient(
+            httpClientFactoryMock.Object, measurementsForDayResponseParser.Object, measurementsForPeriodResponseParser.Object);
+
+        // Act
+        var actual = (await sut.GetAggregatedByPeriodAsync(query, CancellationToken.None)).ToList();
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Single(actual);
+        var actualFirst = actual.First();
+        Assert.Equal("123456789123456789", actualFirst.MeteringPoint.Id);
+        Assert.Single(actualFirst.PointAggregationGroups);
+        Assert.Equal(Resolution.Hourly, actualFirst.PointAggregationGroups.First().Value.Resolution);
+        Assert.Equal(from, actualFirst.PointAggregationGroups.First().Value.From);
+        Assert.Equal(to, actualFirst.PointAggregationGroups.First().Value.To);
+        Assert.Equal(31, actualFirst.PointAggregationGroups.First().Value.PointAggregations.Count);
+        Assert.All(actualFirst.PointAggregationGroups.First().Value.PointAggregations, p =>
+        {
+            Assert.Equal(Quality.Measured, p.Quality);
+            Assert.Equal(1, p.Quantity);
+        });
     }
 
     private static Mock<IHttpClientFactory> CreateHttpClientFactoryMock(HttpClient httpClient)
