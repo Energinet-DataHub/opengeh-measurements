312ca23dd6c1258189723dc0eae93eea6d9b8e4a
Morten Kjærgård <36844419+ShaBloze@users.noreply.github.com>
2025-06-12 09:22:50 +0200

    add stream from calculated to sap series

--- source/core/tests/unit_tests/gold/application/streams/test_calculated_measurements_stream.py
+++ source/core/tests/unit_tests/gold/application/streams/test_calculated_measurements_stream.py
@@ -7,6 +7,7 @@ import core.gold.application.streams.calculated_measurements_stream as sut
 from core.gold.domain.constants.streaming.query_names import QueryNames
 from core.gold.infrastructure.repositories.calculated_measurements_repository import CalculatedMeasurementsRepository
 from core.gold.infrastructure.repositories.gold_measurements_repository import GoldMeasurementsRepository
+from core.gold.infrastructure.repositories.measurements_sap_series_repository import GoldMeasurementsSAPSeriesRepository
 from core.gold.infrastructure.streams.gold_measurements_stream import GoldMeasurementsStream
 
 
@@ -36,6 +37,12 @@ def test__batch_operation__calls_append_to_gold_measurements(mocker: MockerFixtu
     transform_mock = Mock()
     mocker.patch.object(sut, "GoldMeasurementsRepository", return_value=gold_repo_mock)
     mocker.patch.object(sut.transformations, "transform_calculated_to_gold", transform_mock)
+
+    sap_series_repo_mock = Mock(spec=GoldMeasurementsSAPSeriesRepository)
+    transform_sap_mock = Mock()
+    mocker.patch.object(sut, "GoldMeasurementsSAPSeriesRepository", return_value=sap_series_repo_mock)
+    mocker.patch.object(sut.sap_series_transformations, "transform_calculated", transform_sap_mock)
+
     calculated_measurements_mock = Mock()
 
     # Act
@@ -46,3 +53,6 @@ def test__batch_operation__calls_append_to_gold_measurements(mocker: MockerFixtu
     gold_repo_mock.append_if_not_exists.assert_called_once_with(
         transform_mock.return_value, query_name=QueryNames.CALCULATED_TO_GOLD
     )
+
+    transform_sap_mock.assert_called_once_with(calculated_measurements_mock)
+    sap_series_repo_mock.append_if_not_exists.assert_called_once_with(transform_sap_mock.return_value)
